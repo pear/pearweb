f290ba20e2087cd18c6d3e994be30ad5d2648184
Martin Jansen <mj@php.net>
2003-10-06 20:29:06 +0000

    * Trying to track down, at which point in cyberspace the bug mails get   lost.
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@141938 c90b9560-bf6c-de11-be94-00142212c4b1

--- public_html/bugs/report.php
+++ public_html/bugs/report.php
@@ -182,9 +182,9 @@ you can scroll down and click the submit button to really enter the details into
 			$extra_headers.= "Message-ID: <bug-$cid@bugs.php.net>";
 
             // mail to package developers
-            mail($mailto, "[PEAR-BUG] #$cid [NEW]: $sdesc", $ascii_report."1\n-- \n$dev_extra", $extra_headers);
+            mail($mailto, "[PEAR-BUG] #$cid [NEW]: $sdesc", $ascii_report."1\n-- \n$dev_extra", $extra_headers, "-fpear-sys@php.net");
             // mail to reporter
-            mail($email, "[PEAR-BUG] Bug #$cid: $sdesc", $ascii_report."2\n", "From: PHP Bug Database <$mailfrom>\nX-PHP-Bug: $cid\nMessage-ID: <bug-$cid@bugs.php.net>");
+            mail($email, "[PEAR-BUG] Bug #$cid: $sdesc", $ascii_report."2\n", "From: PHP Bug Database <$mailfrom>\nX-PHP-Bug: $cid\nMessage-ID: <bug-$cid@bugs.php.net>", "-fpear-sys@php.net");
             header("Location: bug.php?id=$cid&thanks=4");
             exit;
         }
