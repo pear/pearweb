2ea7732e93654b8b7e3544cd2f472f0f5299fa5e
Martin Jansen <mj@php.net>
2003-10-26 13:21:30 +0000

    * Adding new field
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@143122 c90b9560-bf6c-de11-be94-00142212c4b1

--- sql/maintains.sql
+++ sql/maintains.sql
@@ -2,6 +2,6 @@ CREATE TABLE maintains (
     handle      VARCHAR(20) NOT NULL REFERENCES users(handle),
     package     INTEGER NOT NULL REFERENCES packages(id),
     role        ENUM('lead', 'developer', 'contributor', 'helper') NOT NULL,
-
+    active      TINYINT(4) NOT NULL default '1',
     PRIMARY KEY(handle,package)
 );
