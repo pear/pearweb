3ae76533681cb36c7d7f5caeefd208a5b06c179a
Martin Jansen <mj@php.net>
2003-12-19 15:40:41 +0000

    * WS
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@146899 c90b9560-bf6c-de11-be94-00142212c4b1

--- cron/pepr.php
+++ cron/pepr.php
@@ -13,52 +13,50 @@
    | obtain it through the world-wide-web, please send a note to          |
    | license@php.net so we can mail you a copy immediately.               |
    +----------------------------------------------------------------------+
-   | Authors:       Tobias Schlitt <toby@php.net>                         |
+   | Authors: Tobias Schlitt <toby@php.net>                               |
    +----------------------------------------------------------------------+
    $Id$
 */
 		
-	ini_set("include_path", ini_get("include_path").":../../include");
+ini_set("include_path", ini_get("include_path").":../../include");
 
-	require_once('DB.php');
+require_once('DB.php');
 	
-	/**
-	* DSN for pear packages database
-	*/
+/**
+ * DSN for pear packages database
+ */
+$dsn = "mysql://pear:pear@localhost/pear";
+$dbh = DB::connect($dsn);
+
+if (DB::isError($db = DB::connect($dsn))) {
+    die ("Failed to connect: $dsn\n");
+}
+
+require_once 'pepr/pepr.php';
+
+// This checks if a proposal should automatically be finished
 	
-	$dsn = "mysql://pear:pear@localhost/pear";
-	$dbh = DB::connect($dsn);
-
-	if (DB::isError($db = DB::connect($dsn))) {
-		die ("Failed to connect: $dsn\n");
-	}
-
-	require_once 'pepr/pepr.php';
-
-	// This checks if a proposal should automatically be finished
+$proposals = proposal::getAll($dbh, "vote");
 	
-	$proposals = proposal::getAll($dbh, "vote");
-	
-	foreach ($proposals AS $id => $proposal) {
-		if ($proposal->getStatus() == "vote") {
-			$lastVoteDate = ($proposal->longened_date > 0) ? $proposal->longened_date : $proposal->vote_date;
-			if (($lastVoteDate + PROPOSAL_STATUS_VOTE_TIMELINE) < time()) {
-				if (ppVote::getCount($dbh, $proposal->id) > 4) {
-					$proposals[$id]->status = 'finished';
-					$proposal->sendActionEmail('change_status_finished', 'pearweb', null);
-				} else {
-					if ($proposal->longened_date > 0) {
-						$proposals[$id]->status = 'finished';
-						$proposal->sendActionEmail('change_status_finished', 'pearweb', null);
-					} else {
-						$proposals[$id]->longened_date = time();
-						$proposal->sendActionEmail('longened_timeline_sys', 'pearweb', null);
-					}
-				}
-				$proposals[$id]->getLinks($dbh);
-				$proposals[$id]->store($dbh);
-			}
-		}
-	}
-
+foreach ($proposals AS $id => $proposal) {
+    if ($proposal->getStatus() == "vote") {
+        $lastVoteDate = ($proposal->longened_date > 0) ? $proposal->longened_date : $proposal->vote_date;
+        if (($lastVoteDate + PROPOSAL_STATUS_VOTE_TIMELINE) < time()) {
+            if (ppVote::getCount($dbh, $proposal->id) > 4) {
+                $proposals[$id]->status = 'finished';
+                $proposal->sendActionEmail('change_status_finished', 'pearweb', null);
+            } else {
+                if ($proposal->longened_date > 0) {
+                    $proposals[$id]->status = 'finished';
+                    $proposal->sendActionEmail('change_status_finished', 'pearweb', null);
+                } else {
+                    $proposals[$id]->longened_date = time();
+                    $proposal->sendActionEmail('longened_timeline_sys', 'pearweb', null);
+                }
+            }
+            $proposals[$id]->getLinks($dbh);
+            $proposals[$id]->store($dbh);
+        }
+    }
+}
 ?>
