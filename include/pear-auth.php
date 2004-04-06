5ccce19f88aca3a2ac974022a8913ce19520fce1
Tobias Schlitt <toby@php.net>
2004-04-06 17:50:59 +0000

    * Insert logon reasons and account request link.
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@155406 c90b9560-bf6c-de11-be94-00142212c4b1

--- include/pear-auth.php
+++ include/pear-auth.php
@@ -67,6 +67,14 @@ function auth_reject($realm = null, $message = null)
         }
         print "\" />\n";
         print "</form>\n";
+        print "<p>Authorization is required for the following actions:</p>";
+        print "<ul>";
+        print "<li>Propose new packages.</li>";
+        print "<li>Leave comments and vote on package proposals.</li>";
+        print "<li>Upload new package releases.</li>";
+        print "</ul>";
+        print "Commenting and voting, as well as uploading releases, is only possible if you already maintain a package.</p>";
+        print "<p>If you have no PEAR Website account, yet, feel free to request one ".make_link("/account-request.php", "here").".</p>";        
     }
     response_footer();
     exit;
