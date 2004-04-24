5a5bca2ecaf675a2b1b1cd1bc9e818e213de8cee
Helgi Þormar Þorbjörnsson <dufuz@php.net>
2004-04-24 19:25:01 +0000

    width 50% -> 80%   <b> -> <strong> " -> ' Basiclly only changing this
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@157022 c90b9560-bf6c-de11-be94-00142212c4b1

--- public_html/pepr/pepr-comments-show.php
+++ public_html/pepr/pepr-comments-show.php
@@ -3,14 +3,14 @@
     require_once 'pepr/pepr.php';
     
     if (empty($_GET['id'])) {
-        localRedirect("/pepr/pepr-overview.php");
+        localRedirect('/pepr/pepr-overview.php');
     }
     
-    response_header("PEPr :: Proposal comments");
+    response_header('PEPr :: Proposal comments');
         
     $proposal = proposal::get($dbh, $_GET['id']);
     if (PEAR::isError($proposal)) {
-        PEAR::raiseError("Proposal ".$_GET['id']." not found.");
+        PEAR::raiseError('Proposal '.$_GET['id'].' not found.');
     }
 
     $comments = ppComment::getAll($proposal->id, 'package_proposal_comments');
@@ -22,27 +22,27 @@
             if (empty($userInfos[$comment->user_handle])) {
                 $userInfos[$comment->user_handle] = user::info($comment->user_handle);
             }
-            $commentsHTML .= "<b>".user_link($comment->user_handle);
-            $commentsHTML .= " [". date("Y-m-d, H:i", $comment->timestamp) ."]</b><br />";
-            $commentsHTML .= nl2br($comment->comment)."<br /><br />";
+            $commentsHTML .= '<strong>'.user_link($comment->user_handle);
+            $commentsHTML .= ' ['. date('Y-m-d, H:i', $comment->timestamp) .']</strong><br />';
+            $commentsHTML .= nl2br($comment->comment).'<br /><br />';
         }
     } else {
-        $commentsHTML = "Sorry, there are no comments available.";
+        $commentsHTML = 'Sorry, there are no comments available.';
     }
     
-    echo make_link("/pepr/pepr-proposal-show.php?id=".$_GET['id'], "Back to proposal");
+    echo make_link('/pepr/pepr-proposal-show.php?id='.$_GET['id'], 'Back to proposal');
     
-    $bb = new BorderBox("Proposal comments", "50%", "", 2, true);
+    $bb = new BorderBox('Proposal comments', '80%', '', 2, true);
     
-    $bb->horizHeadRow("Category:", $proposal->pkg_category);
-    $bb->horizHeadRow("Package:", $proposal->pkg_name);
-    $bb->horizHeadRow("Proposer:", user_link($proposal->user_handle));
+    $bb->horizHeadRow('Category:', $proposal->pkg_category);
+    $bb->horizHeadRow('Package:', $proposal->pkg_name);
+    $bb->horizHeadRow('Proposer:', user_link($proposal->user_handle));
     
     $bb->fullRow($commentsHTML);
     
     $bb->end();
     
-    echo make_link("/pepr/pepr-proposal-show.php?id=".$_GET['id'], "Back to proposal");
+    echo make_link('/pepr/pepr-proposal-show.php?id='.$_GET['id'], 'Back to proposal');
     
     response_footer();
 
