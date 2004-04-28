e26eb7ac0a478d326684bbe3dd1eb7093d39b922
Helgi Þormar Þorbjörnsson <dufuz@php.net>
2004-04-28 05:00:36 +0000

    " -> ' <b> -> <strong> some XHTML stuff, and changelog can now have BBcode (doesn't make sense that it hasn't that ;))
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@157361 c90b9560-bf6c-de11-be94-00142212c4b1

--- public_html/pepr/pepr-proposal-show.php
+++ public_html/pepr/pepr-proposal-show.php
@@ -25,7 +25,7 @@
 	require_once 'Damblan/Karma.php';
 
 	if (empty($id)) {
-		localRedirect("pepr-proposals.php");
+		localRedirect('pepr-proposals.php');
 	}
 	
 	$proposal = proposal::get($dbh, $id);
@@ -59,7 +59,7 @@
 			$voteData['user_handle'] = $_COOKIE['PEAR_USER'];
 			
 			if ($voteData['is_conditional'] && empty($voteData['comment'])) {
-				PEAR::raiseError("You have to apply a comment if your vote is conditional!");
+				PEAR::raiseError('You have to apply a comment if your vote is conditional!');
 			}
 			if ($voteData['is_conditional'] && ($voteData['value'] < 1)) {
 				PEAR::raiseError("Conditional votes have to be formulated positively! Please select '+1' and change your text to a form like 'I am +1 on this if you change...'.");
@@ -69,10 +69,10 @@
 			$proposal->addVote($dbh, new ppVote($voteData));
 			$proposal->sendActionEmail('proposal_vote', 'user', $_COOKIE['PEAR_USER']);
 			$form->removeElement('submit');
-			$form->addElement('static', '', '<b>Your vote has been registered successfully!</b>'); 
+			$form->addElement('static', '', '<strong>Your vote has been registered successfully!</strong>'); 
 			$form->freeze();
 			if (!DEVBOX) {
-				localredirect("pepr-proposal-show.php?id=".$proposal->id);
+				localredirect('pepr-proposal-show.php?id='.$proposal->id);
 			}
 		}
 		
@@ -81,8 +81,8 @@
 	if (isset($_COOKIE['PEAR_USER']) && ($proposal->getStatus() == 'proposal')) {
 		$form = new HTML_QuickForm('comment', 'post', 'pepr-proposal-show.php?id='.$id);
 		$form->addElement('textarea', 'comment', null, array('cols' => 70, 'rows' => 6));
-		$form->addElement('static', '', '', '<small>Your comment will also be sent to the <b>pear-dev</b> mailing list.<br />
-		      <b>Please do not respond to other developers comments</b>.<br />
+		$form->addElement('static', '', '', '<small>Your comment will also be sent to the <strong>pear-dev</strong> mailing list.<br />
+		      <strong>Please do not respond to other developers comments</strong>.<br />
 		      The author himself is responsible to reflect comments in an acceptable way.</small>');
 		$form->addElement('submit', 'submit', 'Add New Comment');
 		
@@ -93,9 +93,9 @@
 			$proposal->sendActionEmail('proposal_comment', 'user', $_COOKIE['PEAR_USER'], $values['comment']);
 			$proposal->addComment($values['comment'], 'package_proposal_comments');
 			$form->removeElement('submit');
-			$form->addElement('static', '', '<b>Your comment has been sent successfully!</b>'); 
+			$form->addElement('static', '', '<strong>Your comment has been sent successfully!</strong>'); 
 			$form->freeze();
-			localredirect("pepr-proposal-show.php?id=".$proposal->id."&comment=1");			
+			localredirect('pepr-proposal-show.php?id='.$proposal->id.'&comment=1');			
 		}
 	}
 	
@@ -106,31 +106,31 @@
 	$proposalVotesSum = ppVote::getSum($dbh, $proposal->id);
 	
 	if (empty($_COOKIE['PEAR_USER'])) {
-		$proposalEditRow = "<div align='right'><small>[You are the author? Login to edit!]</small></div>";
+		$proposalEditRow = '<div align="right"><small>[You are the author? Login to edit!]</small></div>';
 	} else if ($proposal->mayEdit($_COOKIE['PEAR_USER'])) {
-		$proposalEditRow = "<div align='right'>".make_link("pepr-proposal-edit.php?id=".$proposal->id, make_image("edit.gif", "Edit"))." ".make_link("pepr-proposal-delete.php?id=".$proposal->id, make_image("delete.gif", "Delete package"))."</div>";
+		$proposalEditRow = '<div align="right">'.make_link('pepr-proposal-edit.php?id='.$proposal->id, make_image('edit.gif', 'Edit')).' '.make_link('pepr-proposal-delete.php?id='.$proposal->id, make_image('delete.gif', 'Delete package')).'</div>';
 	}
 	
 	$proposer = user::info($proposal->user_handle);
 	
-	response_header("PEPr :: Proposal details");
+	response_header('PEPr :: Proposal details');
 	
 	if (isset($form)) {
 		echo $form->getValidationScript();
 	}
 	
-	echo "<h1>Proposal for ".$proposal->pkg_name." (".$proposal->getStatus(true).")</h1>";
+	echo '<h1>Proposal for '.$proposal->pkg_name.' ('.$proposal->getStatus(true).')</h1>';
 	
-	$bb = new BorderBox("Proposal details", "90%", "", 2, true);
+	$bb = new BorderBox('Proposal details', '90%', '', 2, true);
 	
 	
-	$bb->horizHeadRow("Package name:", $proposal->pkg_name);
-	$bb->horizHeadRow("Package category:", $proposal->pkg_category);
-	$bb->horizHeadRow("Package License:", $proposal->pkg_license);
+	$bb->horizHeadRow('Package name:', $proposal->pkg_name);
+	$bb->horizHeadRow('Package category:', $proposal->pkg_category);
+	$bb->horizHeadRow('Package License:', $proposal->pkg_license);
 	if (!empty($proposal->pkg_deps)) {
-		$bb->horizHeadRow("Dependencies:", nl2br($proposal->pkg_deps));
+		$bb->horizHeadRow('Dependencies:', nl2br($proposal->pkg_deps));
 	}
-	$bb->horizHeadRow("Proposer:", user_link($proposal->user_handle));
+	$bb->horizHeadRow('Proposer:', user_link($proposal->user_handle));
 	$bbparser = new HTML_BBCodeParser(array('filters' => 'Basic,Images,Links,Lists,Extended'));
 	$bb->fullRow($bbparser->qparse(nl2br($proposal->pkg_describtion)));
 	
@@ -140,13 +140,13 @@
 		if (!isset($userinfos[$comment->user_handle])) {
 			$userinfo[$comment->user_handle] = user::info($comment->user_handle);
 		}
-		$changeLogRow .= "<b>".$userinfo[$comment->user_handle]['name'];
-		$changeLogRow .= " [". date("Y-m-d, H:i", $comment->timestamp) ."]</b><br />";
-		$changeLogRow .= nl2br($comment->comment)."<br /><br />";
+		$changeLogRow .= '<strong>'.$userinfo[$comment->user_handle]['name'];
+		$changeLogRow .= ' ['. date('Y-m-d, H:i', $comment->timestamp) .']</strong><br />';
+		$changeLogRow .= $bbparser->qparse(nl2br($comment->comment)).'<br /><br />';
 	}
 	
 	if (!empty($changeLogRow)) {
-		$bb->horizHeadRow("Changelog:", $changeLogRow);
+		$bb->horizHeadRow('Changelog:', $changeLogRow);
 	}
 	
 	if (!empty($proposalEditRow)) {
@@ -157,7 +157,7 @@
 	
 	echo spacer(1, 20);
 	
-	$bb = new BorderBox("Proposal links:", "90%", "", 2, true);
+	$bb = new BorderBox('Proposal links:', '90%', '', 2, true);
 	
 	$lastLinkType = false;
 	foreach ($proposal->links as $link) {
@@ -177,14 +177,14 @@
 	
 	echo spacer(1, 20);
 	
-	$bb = new BorderBox("Timeline", "90%", "", 2, true);
+	$bb = new BorderBox('Timeline', '90%', '', 2, true);
 	
-	$bb->horizHeadRow("First draft:", date("d.m.Y",$proposal->draft_date));
+	$bb->horizHeadRow('First draft:', date('d.m.Y',$proposal->draft_date));
 	if ($proposal->proposal_date != 0) {
-		$bb->horizHeadRow("Proposal:", date("d.m.Y",$proposal->proposal_date));
+		$bb->horizHeadRow('Proposal:', date('d.m.Y',$proposal->proposal_date));
 	}
 	if ($proposal->vote_date != 0) {
-		$bb->horizHeadRow("Call for votes:", date("d.m.Y",$proposal->vote_date));
+		$bb->horizHeadRow('Call for votes:', date('d.m.Y',$proposal->vote_date));
 	}
 	
 	$bb->end();
@@ -193,31 +193,31 @@
 		if (isset($_COOKIE['PEAR_USER'])) {
 			$formArray = $form->toArray();
 			echo "<form ".$formArray['attributes'].">";
-			$bb = new BorderBox("Comment on this proposal", "90%", "", 2, true);
-			$bb->horizHeadRow("Comment:", $formArray['elements'][0]['html']);
-			$bb->horizHeadRow("", $formArray['elements'][1]['html']);
-			$bb->horizHeadRow("", $formArray['elements'][2]['html']);
+			$bb = new BorderBox('Comment on this proposal', '90%', '', 2, true);
+			$bb->horizHeadRow('Comment:', $formArray['elements'][0]['html']);
+			$bb->horizHeadRow('', $formArray['elements'][1]['html']);
+			$bb->horizHeadRow('', $formArray['elements'][2]['html']);
 			if (isset($_GET['comment']) && ($_GET['comment'] == 1)) {
-				$bb->horizHeadRow("", "Comment sent successfully.");
+				$bb->horizHeadRow('', 'Comment sent successfully.');
 			}
 			$bb->end();
-			echo "</form>";
+			echo '</form>';
 		} else {
-			$bb = new BorderBox("Comment on this proposal", "90%", "", 2, true);
-			$bb->fullRow("Please login to comment or comment directly on ".make_link("pear-dev@lists.php.net", "pear-dev@lists.php.net").".");
+			$bb = new BorderBox('Comment on this proposal', '90%', '', 2, true);
+			$bb->fullRow('Please login to comment or comment directly on '.make_link('pear-dev@lists.php.net', 'pear-dev@lists.php.net').'.');
 			$bb->end();
 		}
 	}
     
-	echo "<p align='center' width='90%'>".make_link("/pepr/pepr-comments-show.php?id=".$proposal->id, "View comments on this proposal.")."</p>";
+	echo '<p align="center" width="90%">'.make_link('/pepr/pepr-comments-show.php?id='.$proposal->id, 'View comments on this proposal.').'</p>';
 	
 	echo spacer(1, 20);
 	
-	echo "<table width='90%'>";
+	echo '<table style="width: 90%;">';
 	
-	echo "<tr><td width='50%' valign='top'>";
+	echo '<tr><td style="width: 50%; vertical-align: top;">';
 	
-	$bb = new BorderBox("Votes", "100%", "", 2, true);
+	$bb = new BorderBox('Votes', '100%', '', 2, true);
 	if (count($proposal->votes) > 0) {
 	
 		$users = array();
@@ -227,30 +227,30 @@
 			if (!isset($users[$vote->user_handle])) {
 				$users[$vote->user_handle] =& user::info($vote->user_handle);
 			}
-			$vote->value = ($vote->value > 0) ? "+".$vote->value : $vote->value;
-			$vote_line = "<b>".make_link("pepr-vote-show.php?id=".$proposal->id."&handle=".$vote->user_handle, $vote->value."</b>");
+			$vote->value = ($vote->value > 0) ? '+'.$vote->value : $vote->value;
+			$vote_line = '<strong>'.make_link('pepr-vote-show.php?id='.$proposal->id.'&amp;handle='.$vote->user_handle, $vote->value.'</strong>');
 			if ($vote->is_conditional || !empty($vote->comment)) {
 				$vote_line .= "*";
 			}
-			$vote_line .= " (".make_link("../account-info.php?handle=".$vote->user_handle, $users[$vote->user_handle]['name']).")";
-			$vote_line .= ", ".date("Y-m-d", $vote->timestamp);
+			$vote_line .= ' ('.make_link('../account-info.php?handle='.$vote->user_handle, $users[$vote->user_handle]['name']).')';
+			$vote_line .= ', '.date('Y-m-d', $vote->timestamp);
 			
 			$bb->horizHeadRow("", $vote_line);
 		
 		}
 	
-		$bb->horizHeadRow("Sum:", "<b>".$proposalVotesSum['all']."</b> <small>(".$proposalVotesSum['conditional']." conditional)</small>");
+		$bb->horizHeadRow('Sum:', '<strong>'.$proposalVotesSum['all'].'</strong> <small>('.$proposalVotesSum['conditional'].' conditional)</small>');
 	} else {
-		$bb->fullRow("No votes yet.");
+		$bb->fullRow('No votes yet.');
 	}
 
 	$bb->end();
 	
-	echo "</td><td width='50%' valign='top'>";
+	echo '</td><td style="width=: 50%; vertical-align: top";>';
 	
 	if (($proposal->status == 'vote')) {
 	
-		$bb = new BorderBox("Vote on this proposal", "100%", "", 2, true);
+		$bb = new BorderBox('Vote on this proposal', '100%', '', 2, true);
 		
 		$karma = new Damblan_Karma($dbh);
 		if (isset($_COOKIE['PEAR_USER']) && $karma->has($_COOKIE['PEAR_USER'],'pear.dev')) {
@@ -258,42 +258,42 @@
 				$formArray = $form->toArray();
 				echo "<form ".$formArray['attributes'].">";
 				
-				$bb->horizHeadRow("Vote", $formArray['elements'][0]['html']." ".$formArray['elements'][0]['label']);
-				$bb->horizHeadRow("", $formArray['elements'][1]['html']." ".$formArray['elements'][1]['label']);
-				$bb->horizHeadRow("Comment", $formArray['elements'][2]['html']);
-				$bb->horizHeadRow("Reviews", $formArray['elements'][3]['html'].$formArray['elements'][3]['label']);
-				$bb->horizHeadRow("", $formArray['elements'][4]['html']);
-				$bb->horizHeadRow("", $formArray['elements'][5]['html']);
+				$bb->horizHeadRow('Vote', $formArray['elements'][0]['html']." ".$formArray['elements'][0]['label']);
+				$bb->horizHeadRow('', $formArray['elements'][1]['html']." ".$formArray['elements'][1]['label']);
+				$bb->horizHeadRow('Comment', $formArray['elements'][2]['html']);
+				$bb->horizHeadRow('Reviews', $formArray['elements'][3]['html'].$formArray['elements'][3]['label']);
+				$bb->horizHeadRow('', $formArray['elements'][4]['html']);
+				$bb->horizHeadRow('', $formArray['elements'][5]['html']);
 				echo "</form>";
 			} else if ($proposal->isOwner($_COOKIE['PEAR_USER'])) {
-				$bb->fullRow("You can not vote.");
+				$bb->fullRow('You can not vote.');
 			} else {
-				$bb->fullRow("You voted.");
+				$bb->fullRow('You voted.');
 			}
 			
 		} else {
-			$bb->fullRow("Only logged in, full featured PEAR developers may vote.");
+			$bb->fullRow('Only logged in, full featured PEAR developers may vote.');
 		}
 		
 		$bb->end();
 	} else if (($proposal->status == 'finished')) {
-		$bb = new BorderBox("Vote on this proposal", "100%", "", 2, true);
-		$bb->fullRow("This proposal is finished.");
+		$bb = new BorderBox('Vote on this proposal', '100%', '', 2, true);
+		$bb->fullRow('This proposal is finished.');
 		$bb->horizHeadRow("Sum votes:", $proposalVotesSum['all']." (".$proposalVotesSum['conditional'].")");
 		if ($proposalVotesSum['all'] >= 5) {
-			$bb->fullRow("This proposal has been accepted.");
+			$bb->fullRow('This proposal has been accepted.');
 		} else {
-			$bb->fullRow("This proposal has not been accepted.");
+			$bb->fullRow('This proposal has not been accepted.');
 		}
 		$bb->end();
 	} else {
-		$bb = new BorderBox("Vote on this proposal", "100%", "", 2, true);
+		$bb = new BorderBox('Vote on this proposal', '100%', '', 2, true);
 		$bb->fullRow("Voting is only enabled during 'Call for votes phase'.");
 		$bb->end();
 	}
 	
-	echo "</td></tr>";
-	echo "</table>";
+	echo '</td></tr>';
+	echo '</table>';
 	
 	response_footer();
 
