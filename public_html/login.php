03693747ad6579a758ea73b25d1539d764a64ea6
Daniel Convissor <danielc@php.net>
2004-06-14 04:23:04 +0000

    Oh, move parens from debugging to normal layout.
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@161305 c90b9560-bf6c-de11-be94-00142212c4b1

--- public_html/login.php
+++ public_html/login.php
@@ -25,9 +25,8 @@
  */
 if (!isset($_COOKIE['PHPSESSID']) &&
     ((strpos(@$_SERVER['HTTP_REFERER'], @$_GET['redirect']) !== false) ||
-     (isset($_POST['PEAR_USER']) && isset($_POST['PEAR_PW']))
-    )
-) {
+     (isset($_POST['PEAR_USER']) && isset($_POST['PEAR_PW']))))
+{
     auth_reject(PEAR_AUTH_REALM, 'Cookies must be enabled to log in.');
 }
 
