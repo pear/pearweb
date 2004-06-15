74eaae3ab029fba3ef7707a80756528723123088
Daniel Convissor <danielc@php.net>
2004-06-15 16:23:37 +0000

    Add/enhance page-level docblock.
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@161415 c90b9560-bf6c-de11-be94-00142212c4b1

--- public_html/bugs/bug.php
+++ public_html/bugs/bug.php
@@ -1,14 +1,42 @@
 <?php /* vim: set noet ts=4 sw=4: : */
 
-// $Id$
+/**
+ * User interface for viewing and editing bug details
+ *
+ * This source file is subject to version 3.0 of the PHP license,
+ * that is bundled with this package in the file LICENSE, and is
+ * available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_0.txt.
+ * If you did not receive a copy of the PHP license and are unable to
+ * obtain it through the world-wide-web, please send a note to
+ * license@php.net so we can mail you a copy immediately.
+ *
+ * @category  pearweb
+ * @package   Bugs
+ * @copyright Copyright (c) 1997-2004 The PHP Group
+ * @license   http://www.php.net/license/3_0.txt  PHP License
+ * @version   $Id$
+ */
 
-require_once './include/prepend.inc';
-require_once './include/cvs-auth.inc';
-require_once './include/trusted-devs.inc';
 /*
  * NOTE: another require exists in the code below, so if changing
  * the include path, make sure to change it too.
  */
+
+/**
+ * Obtain common includes
+ */
+require_once './include/prepend.inc';
+
+/**
+ * Get user's CVS password
+ */
+require_once './include/cvs-auth.inc';
+
+/**
+ * Obtain a list of the trusted developers
+ */
+require_once './include/trusted-devs.inc';
 
 
 error_reporting(E_ALL ^ E_NOTICE);
--- public_html/bugs/report.php
+++ public_html/bugs/report.php
@@ -5,16 +5,31 @@
  *
  * See pearweb/sql/bugs.sql for the table layout.
  *
- * @category pearweb
- * @package  Bugs
- * @version  $Id$
+ * This source file is subject to version 3.0 of the PHP license,
+ * that is bundled with this package in the file LICENSE, and is
+ * available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_0.txt.
+ * If you did not receive a copy of the PHP license and are unable to
+ * obtain it through the world-wide-web, please send a note to
+ * license@php.net so we can mail you a copy immediately.
+ *
+ * @category  pearweb
+ * @package   Bugs
+ * @copyright Copyright (c) 1997-2004 The PHP Group
+ * @license   http://www.php.net/license/3_0.txt  PHP License
+ * @version   $Id$
  */
 
 /**
- * Obtain needed files
+ * Obtain common includes
  */
 require_once './include/prepend.inc';
+
+/**
+ * Get user's CVS password
+ */
 require_once './include/cvs-auth.inc';
+
 
 error_reporting(E_ALL ^ E_NOTICE);
 
