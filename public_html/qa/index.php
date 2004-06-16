c95b5c930bfcb6fdca54888ba2ef2fdc57298598
Martin Jansen <mj@php.net>
2004-06-16 07:43:03 +0000

    * Added some information on the QA initiative.
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@161463 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ public_html/qa/index.php
@@ -0,0 +1,52 @@
+<?php
+/*
+   +----------------------------------------------------------------------+
+   | PEAR Web site version 1.0                                            |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 2004 The PEAR Group                                    |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 2.02 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available at through the world-wide-web at                           |
+   | http://www.php.net/license/2_02.txt.                                 |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Authors: Martin Jansen <mj@php.net>                                  |
+   +----------------------------------------------------------------------+
+   $Id$
+*/
+response_header("Quality Assurance Initiative");
+?>
+
+<h1>The PEAR Quality Assurance Initiative</h1>
+
+<p>The PEAR Quality Assurance Initiative is still in its early stages,
+but some basic information about how the <acronym title="Quality Assurance">QA</acronym>
+team works can be found in the <a href="/pepr/pepr-proposal-show.php?id=60">appendant RfC</a>.
+</p>
+
+<p>As a first step seven people were nominated to form the 
+<acronym title="Quality Assurance">QA</acronym> Core Group:</p>
+
+<ul>
+  <li>David Costa</li>
+  <li>Klaus Guenther</li>
+  <li>Arnaud Limbourg</li>
+  <li>Stefan Neufeind</li>
+  <li>Tobias Schlitt</li>
+  <li>Stephan Schmidt</li>
+  <li>Davey Shafik</li>
+  <li>Lukas Smith</li>
+</ul>
+
+<p>If you are interested in helping out, or if you have questions 
+concerning the <acronym title="Quality Assurance">QA</acronym> 
+initiative, you can contact the team using the mailing list
+<a href="mailto:pear-qa@lists.php.net">pear-qa@lists.php.net</a>.
+(<a href="/support.php">Subscription information</a>.)
+<?php
+response_footer();
+?>
+
