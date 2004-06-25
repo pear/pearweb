281c0f0beb0ce2a00169d21c31c770a959521d0b
Tobias Schlitt <toby@php.net>
2004-06-25 15:48:36 +0000

    * This just echo's the current "open-bug-count" for inclusion into PEARBot.
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@162067 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ public_html/bugs/lstat.php
@@ -0,0 +1,10 @@
+<?php
+
+    require_once 'pear-database.php';
+    $res = $dbh->getOne('SELECT count( id ) AS count FROM bugdb WHERE STATUS = "Open"');
+    if (DB::isError($res)) {
+        die;
+    }
+        echo $res;
+
+?>
