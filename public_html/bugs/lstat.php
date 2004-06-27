3b0832d59d7e936dd77ba535fcc1d14b22fb3533
Helgi Þormar Þorbjörnsson <dufuz@php.net>
2004-06-27 22:54:28 +0000

    Finding only bugs, not feature requests, and also only finding pear bugs.
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@162195 c90b9560-bf6c-de11-be94-00142212c4b1

--- public_html/bugs/lstat.php
+++ public_html/bugs/lstat.php
@@ -1,7 +1,10 @@
 <?php
 
     require_once 'pear-database.php';
-    $res = $dbh->getOne('SELECT count( id ) AS count FROM bugdb WHERE status = "Open" OR status = "Assigned"');
+    $res = $dbh->getOne('SELECT count( bugdb.id ) AS count FROM bugdb
+                        LEFT JOIN packages ON packages.name = bugdb.package_name
+                        WHERE bugdb.status = "Open" OR bugdb.status = "Assigned" 
+                        AND bugdb.bug_type = "Bug" AND packages.package_type = "pear"');
     if (DB::isError($res)) {
         die;
     }
