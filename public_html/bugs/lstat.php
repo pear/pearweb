10fe2e238c07711456e7eeb6ff590ed0f6a753f7
Helgi Þormar Þorbjörnsson <dufuz@php.net>
2004-06-28 01:26:35 +0000

    Added more states so it would be in sync with the search page.
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@162208 c90b9560-bf6c-de11-be94-00142212c4b1

--- public_html/bugs/lstat.php
+++ public_html/bugs/lstat.php
@@ -1,9 +1,10 @@
 <?php
 
     require_once 'pear-database.php';
-    $res = $dbh->getOne('SELECT count( bugdb.id ) AS count FROM bugdb
+    $res = $dbh->getOne('SELECT count(bugdb.id) AS count FROM bugdb
                         LEFT JOIN packages ON packages.name = bugdb.package_name
-                        WHERE bugdb.status = "Open" OR bugdb.status = "Assigned" 
+                        WHERE bugdb.status IN ("Open", "Assigned", 
+                                                "Analyzed", "Critical", "Verified")
                         AND bugdb.bug_type = "Bug" AND packages.package_type = "pear"');
     if (DB::isError($res)) {
         die;
