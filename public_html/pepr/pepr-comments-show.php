266c1168070550f702f42a96daf72128cf37963e
Daniel Convissor <danielc@php.net>
2004-07-05 23:37:50 +0000

    * Tabs to spaces.
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@162722 c90b9560-bf6c-de11-be94-00142212c4b1

--- public_html/pepr/pepr-comments-show.php
+++ public_html/pepr/pepr-comments-show.php
@@ -2,37 +2,37 @@
 
 require_once 'pepr/pepr.php';
 
-if (empty($_GET['id']))	{
-	localRedirect('/pepr/pepr-overview.php');
+if (empty($_GET['id'])) {
+    localRedirect('/pepr/pepr-overview.php');
 }
 
 response_header('PEPr :: Proposal comments');
 
-$proposal =	proposal::get($dbh,	$_GET['id']);
+$proposal = proposal::get($dbh, $_GET['id']);
 if (PEAR::isError($proposal)) {
-	PEAR::raiseError('Proposal '.$_GET['id'].' not found.');
+    PEAR::raiseError('Proposal '.$_GET['id'].' not found.');
 }
 
-$comments =	ppComment::getAll($proposal->id, 'package_proposal_comments');
+$comments = ppComment::getAll($proposal->id, 'package_proposal_comments');
 $userInfos = array();
 
-$commentsHTML =	"";
-if (is_array($comments)	&& (count($comments) > 0)) {
-	foreach	($comments as $comment)	{
-		if (empty($userInfos[$comment->user_handle])) {
-			$userInfos[$comment->user_handle] =	user::info($comment->user_handle);
-		}
-		$commentsHTML .= '<strong>'.user_link($comment->user_handle);
-		$commentsHTML .= ' ['. date('Y-m-d,	H:i	T',	$comment->timestamp) .']</strong><br />';
-		$commentsHTML .= nl2br(htmlentities($comment->comment)).'<br /><br />';
-	}
+$commentsHTML = "";
+if (is_array($comments) && (count($comments) > 0)) {
+    foreach ($comments as $comment) {
+        if (empty($userInfos[$comment->user_handle])) {
+            $userInfos[$comment->user_handle] = user::info($comment->user_handle);
+        }
+        $commentsHTML .= '<strong>'.user_link($comment->user_handle);
+        $commentsHTML .= ' ['. date('Y-m-d, H:i T', $comment->timestamp) .']</strong><br />';
+        $commentsHTML .= nl2br(htmlentities($comment->comment)).'<br /><br />';
+    }
 } else {
-	$commentsHTML =	'Sorry,	there are no comments available.';
+    $commentsHTML = 'Sorry, there are no comments available.';
 }
 
-echo make_link('/pepr/pepr-proposal-show.php?id='.$_GET['id'], 'Back to	proposal');
+echo make_link('/pepr/pepr-proposal-show.php?id='.$_GET['id'], 'Back to proposal');
 
-$bb	= new BorderBox('Proposal comments', '80%',	'',	2, true);
+$bb = new BorderBox('Proposal comments', '80%', '', 2, true);
 
 $bb->horizHeadRow('Category:', $proposal->pkg_category);
 $bb->horizHeadRow('Package:', $proposal->pkg_name);
@@ -42,7 +42,7 @@ $bb->fullRow($commentsHTML);
 
 $bb->end();
 
-echo make_link('/pepr/pepr-proposal-show.php?id='.$_GET['id'], 'Back to	proposal');
+echo make_link('/pepr/pepr-proposal-show.php?id='.$_GET['id'], 'Back to proposal');
 
 response_footer();
 
