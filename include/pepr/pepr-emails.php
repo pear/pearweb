2f44a4390637ca1e699563196deee2b339a82c76
Daniel Convissor <danielc@php.net>
2004-07-15 02:14:08 +0000

    * Alter {owner_email} so mail() won't choke if owner email is blank. * Add page-level docblocks. * Bit of coding standards.
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@163337 c90b9560-bf6c-de11-be94-00142212c4b1

--- include/pepr/pepr-emails.php
+++ include/pepr/pepr-emails.php
@@ -24,8 +24,8 @@ $proposalEmailTexts = array(
     // $proposal->sendActionEmail("proposal_delete", $_COOKIE["PEAR_USER"], $proposal);
     'proposal_delete' => array(
         'to'        => array (
-            'admin'   => '{email_pear_dev}, {email_pear_group}, {owner_name} <{owner_email}>',
-            'user'    => '{email_pear_dev}, {owner_name} <{owner_email}>'
+            'admin'   => '{email_pear_dev}, {email_pear_group}, {owner_name} {owner_email}',
+            'user'    => '{email_pear_dev}, {owner_name} {owner_email}'
         ),
         'subject'   => '{pkg_category}::{pkg_name} deleted',
         'text'      => "{actor_name} ({actor_link}) has deleted the proposal for {pkg_category}::{pkg_name}."
@@ -34,8 +34,8 @@ $proposalEmailTexts = array(
     // $proposal->sendActionEmail("change_status_proposal", $_COOKIE["PEAR_USER"], $proposal);
     'change_status_proposal' => array(
         'to'        => array (
-            'admin'   => '{email_pear_dev}, {email_pear_group}, {owner_name} <{owner_email}>',
-            'user'    => '{email_pear_dev}, {owner_name} <{owner_email}>'
+            'admin'   => '{email_pear_dev}, {email_pear_group}, {owner_name} {owner_email}',
+            'user'    => '{email_pear_dev}, {owner_name} {owner_email}'
         ),
         'subject'   => 'Proposal for {pkg_category}::{pkg_name}',
         'text'      => "{actor_name} ({actor_link}) proposes {pkg_category}::{pkg_name}.\n\nYou can find more detailed information here:\n {proposal_url}"
@@ -44,8 +44,8 @@ $proposalEmailTexts = array(
     // $proposal->sendActionEmail("change_status_vote", $_COOKIE["PEAR_USER"], $proposal);
     'change_status_vote' => array(
         'to'        => array (
-            'admin'   => '{email_pear_dev}, {email_pear_group}, {owner_name} <{owner_email}>',
-            'user'    => '{email_pear_dev}, {owner_name} <{owner_email}>'
+            'admin'   => '{email_pear_dev}, {email_pear_group}, {owner_name} {owner_email}',
+            'user'    => '{email_pear_dev}, {owner_name} {owner_email}'
         ),
         'subject'   => 'Call for votes on {pkg_category}::{pkg_name}',
         'text'      => "{actor_name} ({actor_link}) has initiated the call for votes on {pkg_category}::{pkg_name}.\n\nPlease review the proposal and give your vote here:\n{proposal_url}"
@@ -54,7 +54,7 @@ $proposalEmailTexts = array(
     // $proposal->sendActionEmail("longened_timeline_sys", $_COOKIE["PEAR_USER"], $proposal);
     'longened_timeline_sys' => array(
         'to'        => array (
-            'pearweb'   => '{email_pear_dev}, {email_pear_group}, {owner_name} <{owner_email}>'
+            'pearweb'   => '{email_pear_dev}, {email_pear_group}, {owner_name} {owner_email}'
         ),
         'subject'   => 'Extended call for votes on {pkg_category}::{pkg_name}',
         'text'      => "PEPr has automatically extended the voting time on {pkg_category}::{pkg_name} until {end_voting_time} because there were not enough votes, yet.\n\nPlease review the proposal and give your vote here:\n{proposal_url}\n\nVoting time is extended only once per proposal."
@@ -63,7 +63,7 @@ $proposalEmailTexts = array(
     // $proposal->sendActionEmail("longened_timeline_admin", $_COOKIE["PEAR_USER"], $proposal);
     'longened_timeline_admin' => array(
         'to'        => array (
-            'admin'   => '{email_pear_dev}, {email_pear_group}, {owner_name} <{owner_email}>'
+            'admin'   => '{email_pear_dev}, {email_pear_group}, {owner_name} {owner_email}'
         ),
         'subject'   => 'Extended call for votes on {pkg_category}::{pkg_name}',
         'text'      => "{actor_name} ({actor_link}) has extended the voting time on {pkg_category}::{pkg_name} until {end_voting_time}.\n\nPlease review the proposal and give your vote here:\n{proposal_url}"
@@ -72,8 +72,8 @@ $proposalEmailTexts = array(
     // $proposal->sendActionEmail("change_status_finished", $_COOKIE["PEAR_USER"], $proposal);
     'change_status_finished' => array(
         'to'        => array (
-            'pearweb' => '{email_pear_dev}, {email_pear_group}, {owner_name} <{owner_email}>',
-            'user'    => '{email_pear_dev}, {owner_name} <{owner_email}>'
+            'pearweb' => '{email_pear_dev}, {email_pear_group}, {owner_name} {owner_email}',
+            'user'    => '{email_pear_dev}, {owner_name} {owner_email}'
         ),
         'subject'   => 'Proposal finished {pkg_category}::{pkg_name}',
         'text'      => "PEPr has automatically finished the proposal on {pkg_category}::{pkg_name}.\n\nFurther details on the status of the proposal and the votes can be found here:\n{proposal_url}"
@@ -82,8 +82,8 @@ $proposalEmailTexts = array(
     // $proposal->sendActionEmail("edit_proposal", $_COOKIE["PEAR_USER"], $proposal);
     'edit_proposal' => array(
         'to'        => array (
-            'admin'   => '{email_pear_dev}, {email_pear_group}, {owner_name} <{owner_email}>',
-            'user'    => '{email_pear_dev}, {owner_name} <{owner_email}>'
+            'admin'   => '{email_pear_dev}, {email_pear_group}, {owner_name} {owner_email}',
+            'user'    => '{email_pear_dev}, {owner_name} {owner_email}'
         ),
         'subject'   => 'Changes in proposal for {pkg_category}::{pkg_name}',
         'text'      => "{actor_name} ({actor_link}) has edited the proposal for {pkg_category}::{pkg_name}.\n\nChange comment:\n\n{comment}\n\nPlease re-review the proposal:\n{proposal_url}"
@@ -92,8 +92,8 @@ $proposalEmailTexts = array(
     // $proposal->sendActionEmail("proposal_vote", $_COOKIE["PEAR_USER"], $proposal);
     'proposal_vote' => array(
         'to'        => array (
-            'user'    => '{email_pear_dev}, {actor_name} <{actor_email}>, {owner_name} <{owner_email}>',
-            'admin'   => '{email_pear_dev}, {actor_name} <{actor_email}>, {owner_name} <{owner_email}>'
+            'user'    => '{email_pear_dev}, {actor_name} <{actor_email}>, {owner_name} {owner_email}',
+            'admin'   => '{email_pear_dev}, {actor_name} <{actor_email}>, {owner_name} {owner_email}'
         ),
         'subject'   => '{vote_value} for {pkg_category}::{pkg_name}',
         'text'      => "{actor_name} ({actor_link}) has voted {vote_value} on the proposal for {pkg_category}::{pkg_name}.\n\nProposal information:\n{proposal_url}\nVote information:\n{vote_url}{vote_conditional}"
@@ -102,8 +102,8 @@ $proposalEmailTexts = array(
     // $proposal->sendActionEmail("proposal_vote", $_COOKIE["PEAR_USER"], $proposal);
     'proposal_comment' => array(
         'to'        => array (
-            'user'    => '{email_pear_dev}, {actor_name} <{actor_email}>, {owner_name} <{owner_email}>',
-            'admin'   => '{email_pear_dev}, {actor_name} <{actor_email}>, {owner_name} <{owner_email}>'
+            'user'    => '{email_pear_dev}, {actor_name} <{actor_email}>, {owner_name} {owner_email}',
+            'admin'   => '{email_pear_dev}, {actor_name} <{actor_email}>, {owner_name} {owner_email}'
         ),
         'subject'   => 'Comment on {pkg_category}::{pkg_name}',
         'text'      => "{actor_name} ({actor_link}) has commented on the proposal for {pkg_category}::{pkg_name}.\n\nComment:\n\n{comment}\n\nProposal information:\n{proposal_url}"
