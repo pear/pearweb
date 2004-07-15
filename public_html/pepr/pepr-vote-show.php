8432c9c8726ccd9a9c8d75be8601d3949c1dcb20
Daniel Convissor <danielc@php.net>
2004-07-15 02:17:37 +0000

    * Foramt like rest of pearweb, including error/warning formats. * Work with register_globals off. * Change DB::quote() to DB::quoteSmart() and ditch addslashes()   and stripslashes() since that's handled by quoteSmart(). * htmlspecialchars() output. * In ppVote class, if get() produces no rows, return null.   Eliminates PHP notices from bogus requests. * Fix "undefined offset" when looping through links when saving   data in pepr-proposal-edit.php.  The loop was getting the "url"   from the next link. * Allow owners to vote on RFC's. * Votes now viewed and entered via pepr-votes-show.php instead of   pepr-proposal-show.php. * Don't try to include things more than once. * Add page-level docblocks. * Only perform validate() if form submitted. * Show dates/times in UTC.
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@163344 c90b9560-bf6c-de11-be94-00142212c4b1

--- public_html/pepr/pepr-vote-show.php
+++ public_html/pepr/pepr-vote-show.php
@@ -1,56 +1,115 @@
 <?php
 
-/*
-+----------------------------------------------------------------------+
-| PEAR Web site version 1.0                                            |
-+----------------------------------------------------------------------+
-| Copyright (c) 2001-2003 The PHP Group                                |
-+----------------------------------------------------------------------+
-| This source file is subject to version 2.02 of the PHP license,      |
-| that is bundled with this package in the file LICENSE, and is        |
-| available at through the world-wide-web at                           |
-| http://www.php.net/license/2_02.txt.                                 |
-| If you did not receive a copy of the PHP license and are unable to   |
-| obtain it through the world-wide-web, please send a note to          |
-| license@php.net so we can mail you a copy immediately.               |
-+----------------------------------------------------------------------+
-| Authors:       Tobias Schlitt <toby@php.net>                         |
-+----------------------------------------------------------------------+
-$Id$
-*/
+/**
+ * Displays details about a specific vote.
+ *
+ * This source file is subject to version 3.0 of the PHP license,
+ * that is bundled with this package in the file LICENSE, and is
+ * available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_0.txt.
+ * If you did not receive a copy of the PHP license and are unable to
+ * obtain it through the world-wide-web, please send a note to
+ * license@php.net so we can mail you a copy immediately.
+ *
+ * @category  pearweb
+ * @package   PEPr
+ * @author    Tobias Schlitt <toby@php.net>
+ * @author    Daniel Convissor <danielc@php.net>
+ * @copyright Copyright (c) 1997-2004 The PHP Group
+ * @license   http://www.php.net/license/3_0.txt  PHP License
+ * @version   $Id$
+ */
 
+/**
+ * Obtain the common functions and classes.
+ */
 require_once 'pepr/pepr.php';
 
-if (empty($id) || empty($handle)) {
-    PEAR::raiseError("No vote selected.");
+$handle = htmlspecialchars(@$_GET['handle']);
+
+if (!$proposal =& proposal::get($dbh, @$_GET['id']) || !$handle)
+{
+    response_header('PEPr :: Vote Details :: Invalid Request');
+    echo "<h1>Vote Details</h1>\n";
+    report_error('The requested proposal or user does not exist.');
+    response_footer();
+    exit;
 }
 
-$proposal = proposal::get($dbh, $id);
+response_header('PEPr :: Vote Details :: '
+                . htmlspecialchars($proposal->pkg_name) . ' :: ' . $handle);
+
+echo '<h1>Vote Details for ' . htmlspecialchars($proposal->pkg_name) . ' by ';
+echo $handle . "</h1>\n";
+
+display_pepr_nav($proposal);
+
+?>
+
+<table border="0" cellspacing="0" cellpadding="2" style="width: 100%">
+
+ <tr>
+  <th class="headrow">&raquo; Details</th>
+ </tr>
+ <tr>
+
+<?php
 
 $vote = ppVote::get($dbh, $proposal->id, $handle);
-$voteVal = ($vote->value > 0) ? '+'.$vote->value : $vote->value;
 
-$conditional = ($vote->is_conditional) ? "This vote is conditional." : "This vote is not conditional.";
+if (!$vote) {
+    echo '<td class="textcell" valign="top">';
+    echo 'This user has not voted on this proposal yet.';
+} else {
+    echo '<td class="ulcell" valign="top">';
+    echo "<ul>\n";
 
-response_header("PEPr :: Vote details");
+    echo ' <li>Voter: ';
+    echo user_link($handle);
+    echo "</li>\n";
 
-$bb = new BorderBox("Proposal Vote details", "100%", "", 2, true);
+    echo ' <li>Vote: ';
+    if ($vote->value > 0) {
+        echo '+';
+    }
+    echo $vote->value;
+    if ($vote->is_conditional) {
+        echo ' (conditional)';
+    } else {
+        echo ' (not conditional)';
+    }
+    echo "</li>\n";
 
-$bb->horizHeadRow('Package:', $proposal->pkg_category.'::'.$proposal->pkg_name);
-$bb->horizHeadRow('Voter:', user_link($handle));
-$bb->horizHeadRow('Vote:', $voteVal);
-$bb->horizHeadRow('', $conditional);
-$label = "Reviews:";
-foreach ($vote->getReviews(true) as $review) {
-    $bb->horizHeadRow($label, $review);
-    $label = "";
+    echo ' <li>Reviews: ';
+    echo htmlspecialchars(implode(', ', $vote->getReviews(true)));
+    echo "</li>\n";
+
+    ?>
+
+   </ul>
+
+  </td>
+ </tr>
+
+ <tr>
+  <th class="headrow">&raquo; Comment</th>
+ </tr>
+ <tr>
+  <td class="textcell" valign="top">
+
+    <?php
+
+    $vote->comment = trim($vote->comment);
+    if ($vote->comment) {
+        echo nl2br(htmlentities($vote->comment));
+    } else {
+        echo '&nbsp;';
+    }
 }
 
-$bb->horizHeadRow('Comment:', nl2br(htmlentities(stripslashes($vote->comment))));
-
-$bb->end();
-
-echo make_link('/pepr/pepr-proposal-show.php?id='.$id, 'Back to proposal');
+echo "  </td>\n";
+echo " </tr>\n";
+echo "</table>\n";
 
 response_footer();
 
