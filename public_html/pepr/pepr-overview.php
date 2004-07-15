ba576d153d8f64f6526a55d64c8c6fa1c91a1964
Daniel Convissor <danielc@php.net>
2004-07-15 02:14:12 +0000

    * Foramt like rest of pearweb, including error/warning formats. * Work with register_globals off. * htmlspecialchars() output. * On the overview page, provide a direct link to comments or voting page   depending on the status. * Don't try to include things more than once. * Add page-level docblocks.
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@163340 c90b9560-bf6c-de11-be94-00142212c4b1

--- public_html/pepr/pepr-overview.php
+++ public_html/pepr/pepr-overview.php
@@ -1,27 +1,34 @@
 <?php
-/*
-+----------------------------------------------------------------------+
-| PEAR Web site version 1.0                                            |
-+----------------------------------------------------------------------+
-| Copyright (c) 2001-2003 The PHP Group                                |
-+----------------------------------------------------------------------+
-| This source file is subject to version 2.02 of the PHP license,      |
-| that is bundled with this package in the file LICENSE, and is        |
-| available at through the world-wide-web at                           |
-| http://www.php.net/license/2_02.txt.                                 |
-| If you did not receive a copy of the PHP license and are unable to   |
-| obtain it through the world-wide-web, please send a note to          |
-| license@php.net so we can mail you a copy immediately.               |
-+----------------------------------------------------------------------+
-| Authors:       toby@php.net                                          |
-+----------------------------------------------------------------------+
-$Id$
-*/
 
+/**
+ * Displays a list of all proposals.
+ *
+ * The <var>$proposalStatiMap</var> array is defined in
+ * pearweb/include/pepr/pepr.php.
+ *
+ * This source file is subject to version 3.0 of the PHP license,
+ * that is bundled with this package in the file LICENSE, and is
+ * available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_0.txt.
+ * If you did not receive a copy of the PHP license and are unable to
+ * obtain it through the world-wide-web, please send a note to
+ * license@php.net so we can mail you a copy immediately.
+ *
+ * @category  pearweb
+ * @package   PEPr
+ * @author    Tobias Schlitt <toby@php.net>
+ * @author    Daniel Convissor <danielc@php.net>
+ * @copyright Copyright (c) 1997-2004 The PHP Group
+ * @license   http://www.php.net/license/3_0.txt  PHP License
+ * @version   $Id$
+ */
+
+/**
+ * Obtain the common functions and classes.
+ */
 require_once 'pepr/pepr.php';
-require_once 'HTML/QuickForm.php';
 
-$form = new HTML_QuickForm('filter_proposals', 'get');
+$form =& new HTML_QuickForm('filter_proposals', 'get');
 
 $values[''] = 'All';
 $values = array_merge($values, $proposalStatiMap);
@@ -33,7 +40,6 @@ $form->addElement('submit', 'submit', 'Filter');
 $filter_value = $filter->getValue();
 
 if ($form->validate()) {
-
     if (trim($filter_value[0]) != "") {
         $selectStatus = $filter_value[0];
     }
@@ -41,7 +47,7 @@ if ($form->validate()) {
 
 $proposals =& proposal::getAll($dbh, @$selectStatus);
 
-response_header('Package Proposals');
+response_header('PEPr :: Package Proposals');
 
 echo '<h1>Package Proposals</h1>' . "\n";
 
@@ -54,7 +60,10 @@ $last_status = false;
 foreach ($proposals as $proposal) {
     if ($proposal->getStatus() != $last_status) {
         echo "</ul>";
-        echo '<h2>&raquo; ' . $proposal->getStatus(true) . "</h2>\n";
+        echo '<h2 name="' . $proposal->getStatus() . '" id="';
+        echo $proposal->getStatus() . '">';
+        echo '&raquo; ' . htmlspecialchars($proposal->getStatus(true));
+        echo "</h2>\n";
         echo "<ul>";
         $last_status = $proposal->getStatus();
     }
@@ -62,14 +71,26 @@ foreach ($proposals as $proposal) {
         $users[$proposal->user_handle] = user::info($proposal->user_handle);
     }
     echo "<li>";
-    echo make_link('pepr-proposal-show.php?id='.$proposal->id, $proposal->pkg_category." :: ".$proposal->pkg_name);
-    echo " by ";
-    echo make_link('/user/'.$proposal->user_handle, $users[$proposal->user_handle]['name']);
-    if (in_array($proposal->getStatus(), array('vote', 'finished'))) {
-        $voteSums = ppVote::getSum($dbh, $proposal->id);
-        echo " (Vote sum: <b>".$voteSums['all']."</b><small>, ".$voteSums['conditional']." conditional</small>)";
+    print_link('pepr-proposal-show.php?id=' . $proposal->id,
+               htmlspecialchars($proposal->pkg_category) . ' :: '
+               . htmlspecialchars($proposal->pkg_name));
+    echo ' by ';
+    print_link('/user/' . htmlspecialchars($proposal->user_handle),
+               htmlspecialchars($users[$proposal->user_handle]['name']));
+    switch ($proposal->getStatus()) {
+        case 'proposal':
+            echo ' &nbsp;(<a href="pepr-comments-show.php?id=' . $proposal->id;
+            echo '">Comments</a>)';
+            break;
+        case 'vote':
+        case 'finished':
+            $voteSums = ppVote::getSum($dbh, $proposal->id);
+            echo ' &nbsp;(<a href="pepr-votes-show.php?id=' . $proposal->id;
+            echo '">Vote</a> sum: <strong>' . $voteSums['all'] . '</strong>';
+            echo '<small>, ' . $voteSums['conditional'];
+            echo ' conditional</small>)';
     }
-    echo "</li>";
+    echo "</li>\n";
 }
 
 echo "</ul>";
