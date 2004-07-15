8aee8317bdcf85d179e737577f9b43e987786b54
Daniel Convissor <danielc@php.net>
2004-07-15 02:14:13 +0000

    * Foramt like rest of pearweb, including error/warning formats. * Work with register_globals off. * htmlspecialchars() output. * Don't try to include things more than once. * Add page-level docblocks. * Eliminate $mapProposalStatus undefined error in pepr-proposal-delete.php. * Provide warning to admins when trying to edit or delete proposals after   they leave the proposal stage. * Disallow proposal owners who are admins from deleting proposals after   they leave the proposal stage.
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@163341 c90b9560-bf6c-de11-be94-00142212c4b1

--- public_html/pepr/pepr-proposal-delete.php
+++ public_html/pepr/pepr-proposal-delete.php
@@ -1,84 +1,98 @@
 <?php
 
-/*
-+----------------------------------------------------------------------+
-| PEAR Web site version 1.0                                            |
-+----------------------------------------------------------------------+
-| Copyright (c) 2001-2003 The PHP Group                                |
-+----------------------------------------------------------------------+
-| This source file is subject to version 2.02 of the PHP license,      |
-| that is bundled with this package in the file LICENSE, and is        |
-| available at through the world-wide-web at                           |
-| http://www.php.net/license/2_02.txt.                                 |
-| If you did not receive a copy of the PHP license and are unable to   |
-| obtain it through the world-wide-web, please send a note to          |
-| license@php.net so we can mail you a copy immediately.               |
-+----------------------------------------------------------------------+
-| Authors:       Tobias Schlitt <toby@php.net>                         |
-+----------------------------------------------------------------------+
-$Id$
-*/
+/**
+ * Interface for deleting a proposal.
+ *
+ * This source file is subject to version 3.0 of the PHP license,
+ * that is bundled with this package in the file LICENSE, and is
+ * available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_0.txt.
+ * If you did not receive a copy of the PHP license and are unable to
+ * obtain it through the world-wide-web, please send a note to
+ * license@php.net so we can mail you a copy immediately.
+ *
+ * @category  pearweb
+ * @package   PEPr
+ * @author    Tobias Schlitt <toby@php.net>
+ * @author    Daniel Convissor <danielc@php.net>
+ * @copyright Copyright (c) 1997-2004 The PHP Group
+ * @license   http://www.php.net/license/3_0.txt  PHP License
+ * @version   $Id$
+ */
 
+/**
+ * Obtain the common functions and classes.
+ */
 require_once 'pepr/pepr.php';
-require_once 'HTML/QuickForm.php';
 
 auth_require('pear.pepr');
 
-$karma = new Damblan_Karma($dbh);
-
-if (empty($id)) {
-    PEAR::raiseError("Proposal not found.");
-}
-
-if (!empty($isDeleted)) {
-    response_header("PEPr :: Delete proposal");
-    echo "<p>";
-    echo "<b>Proposal deleted successfully!</b>";;
-    echo "</p>";
-    echo "<p>";
-    echo make_link("/pepr/pepr-overview.php", "Back to proposal overview");
-    echo "</p>";
+if (!empty($_GET['isDeleted'])) {
+    response_header('PEPr :: Delete');
+    echo "<h1>Delete Proposal</h1>\n";
+    report_success('Proposal deleted successfully.');
+    echo '<p>';
+    print_link('/pepr/pepr-overview.php', 'Back to PEPr Home Page');
+    echo "</p>\n";
     response_footer();
     exit;
 }
 
-$proposal = proposal::get($dbh, $id);
-
-if (empty($proposal)) {
-    PEAR::raiseError("Proposal not found.");
+if (!$proposal =& proposal::get($dbh, @$_GET['id'])) {
+    response_header('PEPr :: Delete :: Invalid Request');
+    echo "<h1>Delete Proposal</h1>\n";
+    report_error('The requested proposal does not exist.');
+    response_footer();
+    exit;
 }
 
-if (DB::isError($proposal)) {
-    PEAR::raiseError($proposal);
+response_header('PEPr :: Delete :: ' . htmlspecialchars($proposal->pkg_name));
+echo '<h1>Delete Proposal ' . htmlspecialchars($proposal->pkg_name) . "</h1>\n";
+
+if (!$proposal->mayEdit($_COOKIE['PEAR_USER'])) {
+    report_error('You are not allowed to delete this proposal,'
+                 . ' probably due to it having reached the "'
+                 . $proposal->getStatus(true) . '" phase.'
+                 . ' If this MUST be deleted, contact someone ELSE'
+                 . ' who has pear.pepr.admin karma.');
+    response_footer();
+    exit;
 }
 
-if (($_COOKIE['PEAR_USER'] != $proposal->user_handle) && !$karma->has($_COOKIE['PEAR_USER'], "pear.pepr.admin")) {
-    PEAR::raiseError("You did not create this proposal. You can not delete it.");
+if ($proposal->compareStatus('>', 'proposal')) {
+    $karma =& new Damblan_Karma($dbh);
+    if ($karma->has($_COOKIE['PEAR_USER'], 'pear.pepr.admin')) {
+        report_error('This proposal has reached the "'
+                     . $proposal->getStatus(true) . '" phase.'
+                     . ' Are you SURE you want to delete it?',
+                     'warnings', 'WARNING:');
+    }
 }
 
-if ((($proposal->status == "vote") || ($proposal->status == "finished")) && !$karma->has($_COOKIE['PEAR_USER'], "pear.pepr.admin")) {
-    PEAR::raiseError("You can not delete proposals later than status '".$mapProposalStatus['proposal']."'.");
-}
+$form =& new HTML_QuickForm('delete-proposal', 'post',
+                            'pepr-proposal-delete.php?id=' . $proposal->id);
 
-$form = new HTML_QuickForm('delete-proposal', 'post', 'pepr-proposal-delete.php?id='.$id);
-
-$form->addElement('checkbox', 'delete', 'Really delete proposal for', $proposal->pkg_category."::".$proposal->pkg_name);
+$form->addElement('checkbox', 'delete', 'Really delete proposal for ',
+                  htmlspecialchars($proposal->pkg_category) . '::'
+                  . htmlspecialchars($proposal->pkg_name));
 $form->addElement('submit', 'submit', 'Do it');
 
-$form->addRule('delete', 'You have to check the box to delete!', 'required', '', 'client');
+$form->addRule('delete', 'You have to check the box to delete!', 'required',
+               '', 'client');
 
-if ($form->validate()) {
-    $proposal->delete($dbh);
-    $proposal->sendActionEmail('proposal_delete', 'mixed', $_COOKIE['PEAR_USER']);
-    $form->removeElement('submit');
-    $form->addElement('static', '', '', 'Package deleted successfully');
-    $form->freeze();
-    localRedirect("pepr-proposal-delete.php?id=".$proposal->id."&isDeleted=1");
+if (isset($_POST['submit'])) {
+    if ($form->validate()) {
+        $proposal->delete($dbh);
+        $proposal->sendActionEmail('proposal_delete', 'mixed',
+                                   $_COOKIE['PEAR_USER']);
+        localRedirect('pepr-proposal-delete.php?id=' . $proposal->id . '&isDeleted=1');
+    } else {
+        $pepr_form = $form->toArray();
+        report_error($pepr_form['errors']);
+    }
 }
 
-
-
-response_header("PEPr :: Delete proposal");
+display_pepr_nav($proposal);
 
 $form->display();
 
