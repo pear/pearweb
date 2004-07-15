2084621dcb204b1a725a4a228aee5560974d12ca
Daniel Convissor <danielc@php.net>
2004-07-15 02:14:15 +0000

    * Foramt like rest of pearweb, including error/warning formats. * Work with register_globals off. * htmlspecialchars() output. * Comments now entered via pepr-comments-show.php instead of   pepr-proposal-show.php. * Don't try to include things more than once. * Add page-level docblocks. * Only perform validate() if form submitted. * Show dates/times in UTC.
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@163343 c90b9560-bf6c-de11-be94-00142212c4b1

--- public_html/pepr/pepr-comments-show.php
+++ public_html/pepr/pepr-comments-show.php
@@ -1,48 +1,181 @@
 <?php
 
+/**
+ * Displays and accepts comments for a given proposal.
+ *
+ * This source file is subject to version 3.0 of the PHP license,
+ * that is bundled with this package in the file LICENSE, and is
+ * available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_0.txt.
+ * If you did not receive a copy of the PHP license and are unable to
+ * obtain it through the world-wide-web, please send a note to
+ * license@php.net so we can mail you a copy immediately.
+ *
+ * @category  pearweb
+ * @package   PEPr
+ * @author    Tobias Schlitt <toby@php.net>
+ * @author    Daniel Convissor <danielc@php.net>
+ * @copyright Copyright (c) 1997-2004 The PHP Group
+ * @license   http://www.php.net/license/3_0.txt  PHP License
+ * @version   $Id$
+ */
+
+/**
+ * Obtain the common functions and classes.
+ */
 require_once 'pepr/pepr.php';
 
-if (empty($_GET['id'])) {
-    localRedirect('/pepr/pepr-overview.php');
+if (!$proposal =& proposal::get($dbh, @$_GET['id'])) {
+    response_header('PEPr :: Comments :: Invalid Request');
+    echo "<h1>Comments for</h1>\n";
+    report_error('The requested proposal does not exist.');
+    response_footer();
+    exit;
 }
 
-response_header('PEPr :: Proposal comments');
+response_header('PEPr :: Comments :: ' . htmlspecialchars($proposal->pkg_name));
+echo '<h1>Comments for ' . htmlspecialchars($proposal->pkg_name) . "</h1>\n";
 
-$proposal = proposal::get($dbh, $_GET['id']);
-if (PEAR::isError($proposal)) {
-    PEAR::raiseError('Proposal '.$_GET['id'].' not found.');
+if (isset($_COOKIE['PEAR_USER']) &&
+    $proposal->getStatus() == 'proposal')
+{
+    $form =& new HTML_QuickForm('comment', 'post',
+                                'pepr-comments-show.php?id=' . $proposal->id);
+
+    $form->addElement('textarea', 'comment', null,
+                      array('cols' => 40,
+                            'rows' => 6,
+                            'id'   => 'comment_field'));
+
+    $form->addElement('static', '', '',
+            '<small>Your comment will also be sent to the'
+            . ' <strong>pear-dev</strong> mailing list.<br />'
+            . ' <strong>Please do not respond to other developers'
+            . ' comments</strong>.<br />'
+            . ' The author himself is responsible to reflect comments'
+            . ' in an acceptable way.</small>');
+
+    $form->addElement('submit', 'submit', 'Add New Comment');
+
+    $form->applyFilter('comment', 'trim');
+    $form->addRule('comment', 'A comment is required', 'required', null,
+                   'client');
+
+    if (isset($_POST['submit'])) {
+        if ($form->validate()) {
+            $values = $form->exportValues();
+            $proposal->sendActionEmail('proposal_comment', 'user',
+                                       $_COOKIE['PEAR_USER'],
+                                       $values['comment']);
+            $proposal->addComment($values['comment'],
+                                  'package_proposal_comments');
+            report_success('Your comment was successfully processed');
+        } else {
+            report_error($form->getElementError('comment'));
+        }
+    }
 }
+
+display_pepr_nav($proposal);
+
+?>
+
+<table border="0" cellspacing="0" cellpadding="2" style="width: 100%">
+
+ <tr>
+  <th class="headrow" colspan="2">&raquo; Submit Your Comment</th>
+ </tr>
+ <tr>
+  <td class="textcell" valign="top" colspan="2">
+
+<?php
+
+if ($proposal->getStatus() == 'proposal') {
+    if (isset($_COOKIE['PEAR_USER'])) {
+        $formArray = $form->toArray();
+
+        echo $form->getValidationScript();
+
+        echo '<form ' . $formArray['attributes'] . ">\n";
+        echo '<table class="form-holder" cellspacing="1">' . "\n";
+
+        echo ' <caption class="form-caption">Comment on This';
+        echo ' Proposal</caption>' . "\n";
+
+        echo " <tr>\n";
+        echo '  <th class="form-label_left">';
+        echo '   <label for="comment_field" accesskey="o">C<span';
+        echo ' class="accesskey">o</span>mment:</label>';
+        echo "  </th>\n";
+        echo '  <td class="form-input">';
+        echo $formArray['elements'][0]['html'] . "</td>\n";
+        echo " </tr>\n";
+
+        echo " <tr>\n";
+        echo '  <th class="form-label_left">&nbsp;</th>' . "\n";
+        echo '  <td class="form-input">';
+        echo $formArray['elements'][1]['html'] . "</td>\n";
+        echo " </tr>\n";
+
+        echo " <tr>\n";
+        echo '  <th class="form-label_left">&nbsp;</th>' . "\n";
+        echo '  <td class="form-input">';
+        echo $formArray['elements'][2]['html'] . "</td>\n";
+        echo " </tr>\n";
+
+        echo "</table>\n";
+        echo "</form>\n";
+    } else {
+        echo 'Please log in to enter your comment. If you are not a registered';
+        echo ' PEAR developer, you can comment by sending an email to ';
+        echo make_mailto_link('pear-dev@lists.php.net') . '.';
+    }
+} else {
+    echo 'Comments only accepted during the &quot;Proposal&quot; phase.';
+    echo ' This proposal is currently in the &quot;';
+    echo $proposal->getStatus(true) . '&quot; phase.';
+}
+
+?>
+
+  </td>
+ </tr>
+
+ <tr>
+  <th class="headrow" style="width: 100%">&raquo; Comments</th>
+ </tr>
+ <tr>
+
+<?php
 
 $comments = ppComment::getAll($proposal->id, 'package_proposal_comments');
 $userInfos = array();
 
-$commentsHTML = "";
 if (is_array($comments) && (count($comments) > 0)) {
+    echo '  <td class="ulcell" valign="top">' . "\n";
+    echo '   <ul class="spaced">' . "\n";
     foreach ($comments as $comment) {
         if (empty($userInfos[$comment->user_handle])) {
             $userInfos[$comment->user_handle] = user::info($comment->user_handle);
         }
-        $commentsHTML .= '<strong>'.user_link($comment->user_handle);
-        $commentsHTML .= ' ['. date('Y-m-d, H:i T', $comment->timestamp) .']</strong><br />';
-        $commentsHTML .= nl2br(htmlentities($comment->comment)).'<br /><br />';
+        echo '<li><p style="margin: 0em 0em 0.3em 0em;">';
+        echo user_link($comment->user_handle, true);
+        echo ' [' . make_utc_date($comment->timestamp) . ']</p>';
+        echo nl2br(htmlentities(trim($comment->comment))) . "\n</li>";
     }
+    echo "   </ul>\n";
 } else {
-    $commentsHTML = 'Sorry, there are no comments available.';
+    echo '  <td class="textcell" valign="top">';
+    echo 'There are no comments.';
 }
 
-echo make_link('/pepr/pepr-proposal-show.php?id='.$_GET['id'], 'Back to proposal');
+?>
 
-$bb = new BorderBox('Proposal comments', '80%', '', 2, true);
+  </td>
+ </tr>
+</table>
 
-$bb->horizHeadRow('Category:', $proposal->pkg_category);
-$bb->horizHeadRow('Package:', $proposal->pkg_name);
-$bb->horizHeadRow('Proposer:', user_link($proposal->user_handle));
-
-$bb->fullRow($commentsHTML);
-
-$bb->end();
-
-echo make_link('/pepr/pepr-proposal-show.php?id='.$_GET['id'], 'Back to proposal');
+<?php
 
 response_footer();
 
