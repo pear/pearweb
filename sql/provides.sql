1f541544a543584105efc64ed4a683fb38b1d1be
Daniel Convissor <danielc@php.net>
2004-07-21 18:06:11 +0000

    * Seems the "provides" table is not used so remove mentions thereof.
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@163935 c90b9560-bf6c-de11-be94-00142212c4b1

--- sql/provides.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE provides (
-       release	      INTEGER REFERENCES releases(id),
-       type	      ENUM('ext','prog','class','function','feature') NOT NULL,
-       name	      VARCHAR(200),
-       INDEX(release)
-);
