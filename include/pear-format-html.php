0902e0408486a0ddda6c47b727f8daca0c86dd1d
Daniel Convissor <danielc@php.net>
2004-07-26 21:42:05 +0000

    * Move package delete link from edit page to navigation tabs.
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@164381 c90b9560-bf6c-de11-be94-00142212c4b1

--- include/pear-format-html.php
+++ include/pear-format-html.php
@@ -937,6 +937,8 @@ function print_package_navigation($pacid, $name, $action)
                                                'title' => 'Edit this package');
         $nav_items['Edit Maintainers'] = array('url'   => '/admin/package-maintainers.php?pid='.$pacid,
                                                'title' => 'Edit the maintainers of this package');
+        $nav_items['Delete']           = array('url'   => '/package-delete.php?id='.$pacid,
+                                               'title' => 'Edit this package');
     }
 
     print '<div id="nav">';
