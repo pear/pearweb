2db2e9815f14bf303a9113c271aabe7d50f74270
Daniel Convissor <danielc@php.net>
2004-08-02 16:40:06 +0000

    * Add last_dl column.
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@164967 c90b9560-bf6c-de11-be94-00142212c4b1

--- sql/package_stats.sql
+++ sql/package_stats.sql
@@ -5,6 +5,7 @@ CREATE TABLE package_stats (
   pid int(11) NOT NULL default '0',
   rid int(11) NOT NULL default '0',
   cid int(11) NOT NULL default '0',
+  last_dl datetime NOT NULL default '0000-00-00 00:00:00',
   KEY pid (pid),
   KEY rid (rid)
 );
