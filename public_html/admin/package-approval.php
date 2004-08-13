6b2f40c67cae760b10f11d16d4020bae6c8e41cf
Martin Jansen <mj@php.net>
2004-08-13 15:29:29 +0000

    * Added possibility to reject a new package.
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@166303 c90b9560-bf6c-de11-be94-00142212c4b1

--- public_html/admin/package-approval.php
+++ public_html/admin/package-approval.php
@@ -25,12 +25,22 @@ response_header("PEAR Administration :: Package Approval");
 echo "<h1>Package Approval</h1>\n";
 
 // Approve package identified by its id
-if (!empty($_GET['approve'])) {
-    $query = "SELECT * FROM packages WHERE id = ?";
-    $row = $dbh->getRow($query, array($_GET['approve']), DB_FETCHMODE_ASSOC);
+if (!empty($_GET['approve']) || !empty($_GET['reject'])) {
 
-    $query = "UPDATE packages SET approved = 1 WHERE id = ? AND approved = 0";
-    $res = $dbh->query($query, array($_GET['approve']));
+    if (!empty($_GET['approve'])) {
+        $query = "UPDATE packages SET approved = 1 WHERE id = " . (int)$_GET['approve'] . " AND approved = 0";
+        $id = $_GET['approve'];
+        $action = "approved";
+    } elseif (!empty($_GET['reject'])) {
+        $query = "DELETE FROM packages WHERE approved = 0 AND id = " . (int)$_GET['reject'];
+        $id = $_GET['reject'];
+        $action = "rejected";
+    }
+
+    $info_query = "SELECT * FROM packages WHERE id = ?";
+    $row = $dbh->getRow($info_query, array($id), DB_FETCHMODE_ASSOC);
+
+    $res = $dbh->query($query);
 
     if (!PEAR::isError($res) && $dbh->affectedRows() > 0) {
         // {{{ Logging mechanism
@@ -41,28 +51,28 @@ if (!empty($_GET['approve'])) {
         $observer = new Damblan_Log_Mail;
         $observer->setRecipients("pear-group@php.net");
         $observer->setHeader("In-Reply-To", "<approve-request-" . $row['id'] . "@pear.php.net>");
-        $observer->setHeader("Subject", "[PEAR Group] Package " . $row['name'] . " has been approved");
+        $observer->setHeader("Subject", "[PEAR Group] Package " . $row['name'] . " has been " . $action);
 
         $logger->attach($observer);
-        $logger->log($auth_user->handle . " approved " . $row['name']);
+        $logger->log($auth_user->handle . " " . $action . " " . $row['name']);
         // }}}
 
-        $mailtext = "Your package \"" . $row['name'] . "\" has been approved by the PEAR Group.";
+        $mailtext = "Your package \"" . $row['name'] . "\" has been " . $action . " by the PEAR Group.";
         $mailtext = wordwrap($mailtext, 72);
 
         $query = "SELECT u.email FROM users u, maintains m WHERE m.package = ? AND u.handle = m.handle";
-        $rows = $dbh->getAll($query, array($_GET['approve']), DB_FETCHMODE_ASSOC);
+        $rows = $dbh->getAll($query, array($id), DB_FETCHMODE_ASSOC);
         foreach ($rows as $u_row) {
-            mail($u_row['email'], "PEAR Package approved", $mailtext, "From: \"PEAR Package Approval System\" <pear-group@php.net>", "-f pear-sys@php.net");
+            mail($u_row['email'], "PEAR Package " . $action, $mailtext, "From: \"PEAR Package Approval System\" <pear-group@php.net>", "-f pear-sys@php.net");
         }
 
-        echo "Successfully <b>approved package</b>.<br /><br />";
+        echo "Successfully <b>" . $action . " package</b>.<br /><br />";
     } else {
         echo "There have been problems: Either an error occured while "
             . "updating the database or the package has already been "
-            . "approved by someone else.<br /><br />";
+            . $action . " by someone else.<br /><br />";
     }
-}
+}    
 
 $query = "SELECT * FROM packages WHERE approved = 0 AND package_type = 'pear'";
 $rows = $dbh->getAll($query, null, DB_FETCHMODE_ASSOC);
@@ -73,7 +83,11 @@ if (count($rows) == 0) {
     $bb = new BorderBox("Unapproved packages", "90%", "", 2, true);
 
     foreach ($rows as $row) {
-        $bb->plainRow($row['name'], make_link($_SERVER['PHP_SELF'] . "?approve=" . $row['id'], "Approve"));
+        $bb->plainRow($row['name'], 
+                      make_link($_SERVER['PHP_SELF'] . "?approve=" . $row['id'], "Approve") .
+                      delim() .
+                      make_link($_SERVER['PHP_SELF'] . "?reject=" . $row['id'], "Reject")
+                      );
     }
 
     $bb->end();
