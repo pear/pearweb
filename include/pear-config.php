e418d045bc91de3cc73c64677fa4d3f6fa2cd040
Greg Beaver <cellog@php.net>
2004-11-24 05:08:24 +0000

    it's not possible to set $_SERVER values, use $_ENV instead
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@173429 c90b9560-bf6c-de11-be94-00142212c4b1

--- include/pear-config.php
+++ include/pear-config.php
@@ -17,19 +17,19 @@
  * @version   $Id$
  */
 
-if (isset($_SERVER['PEAR_TMPDIR'])) {
+if (isset($_ENV['PEAR_TMPDIR'])) {
     /**
      * @ignore
      */
-    define('PEAR_TMPDIR', $_SERVER['PEAR_TMPDIR']);
+    define('PEAR_TMPDIR', $_ENV['PEAR_TMPDIR']);
     /**
      * @ignore
      */
-    define('PEAR_CVS_TMPDIR', $_SERVER['PEAR_TMPDIR'].'/cvs');
+    define('PEAR_CVS_TMPDIR', $_ENV['PEAR_TMPDIR'].'/cvs');
     /**
      * @ignore
      */
-    define('PEAR_UPLOAD_TMPDIR', $_SERVER['PEAR_TMPDIR'].'/uploads');
+    define('PEAR_UPLOAD_TMPDIR', $_ENV['PEAR_TMPDIR'].'/uploads');
 } else {
     /**
      * Where pearweb's temporary files should be stored
@@ -45,106 +45,106 @@ if (isset($_SERVER['PEAR_TMPDIR'])) {
     define('PEAR_UPLOAD_TMPDIR', '/var/tmp/pear/uploads');
 }
 
-if (isset($_SERVER['PEAR_DATABASE_DSN'])) {
+if (isset($_ENV['PEAR_DATABASE_DSN'])) {
     /**
      * @ignore
      */
-    define('PEAR_DATABASE_DSN', $_SERVER['PEAR_DATABASE_DSN']);
+    define('PEAR_DATABASE_DSN', $_ENV['PEAR_DATABASE_DSN']);
 } else {
     /**
      * The PEAR::DB DSN connection string
      *
-     * To override default, set the value in $_SERVER['PEAR_DATABASE_DSN']
+     * To override default, set the value in $_ENV['PEAR_DATABASE_DSN']
      * before this file is included.
      */
     define('PEAR_DATABASE_DSN', 'mysql://pear:pear@localhost/pear'); 
 }
 
-if (isset($_SERVER['PEAR_AUTH_REALM'])) {
-    define('PEAR_AUTH_REALM', $_SERVER['PEAR_AUTH_REALM']);
+if (isset($_ENV['PEAR_AUTH_REALM'])) {
+    define('PEAR_AUTH_REALM', $_ENV['PEAR_AUTH_REALM']);
 } else {
     /**
      * The authorization realm
      *
      * Gets output to a "WWW-authenticate" header upon authentication failures
      *
-     * To override default, set the value in $_SERVER['PEAR_AUTH_REALM']
+     * To override default, set the value in $_ENV['PEAR_AUTH_REALM']
      * before this file is included.
      */
     define('PEAR_AUTH_REALM', 'PEAR');
 }
 
-if (isset($_SERVER['PEAR_TARBALL_DIR'])) {
+if (isset($_ENV['PEAR_TARBALL_DIR'])) {
     /**
      * @ignore
      */
-    define('PEAR_TARBALL_DIR', $_SERVER['PEAR_TARBALL_DIR']);
+    define('PEAR_TARBALL_DIR', $_ENV['PEAR_TARBALL_DIR']);
 } else {
     /**
      * Where package tarballs can be found
      *
-     * To override default, set the value in $_SERVER['PEAR_TARBALL_DIR']
+     * To override default, set the value in $_ENV['PEAR_TARBALL_DIR']
      * before this file is included.
      */
     define('PEAR_TARBALL_DIR', '/var/lib/pear'); 
 }
 
-if (isset($_SERVER['PEAR_CHM_DIR'])) {
+if (isset($_ENV['PEAR_CHM_DIR'])) {
     /**
      * @ignore
      */
-    define('PEAR_CHM_DIR', $_SERVER['PEAR_CHM_DIR']);
+    define('PEAR_CHM_DIR', $_ENV['PEAR_CHM_DIR']);
 } else {
     /**
      * Where the CHM builds of the manual are located
      *
-     * To override default, set the value in $_SERVER['PEAR_CHM_DIR']
+     * To override default, set the value in $_ENV['PEAR_CHM_DIR']
      * before this file is included.
      */
     define('PEAR_CHM_DIR', '/var/lib/pear/chm/'); 
 }
 
-if (isset($_SERVER['PEAR_APIDOC_DIR'])) {
+if (isset($_ENV['PEAR_APIDOC_DIR'])) {
     /**
      * @ignore
      */
-    define('PEAR_APIDOC_DIR', $_SERVER['PEAR_APIDOC_DIR']);
+    define('PEAR_APIDOC_DIR', $_ENV['PEAR_APIDOC_DIR']);
 } else {
     /**
      * Location of the documentation automatically generated for each
      * package by phpDocumentor
      *
-     * To override default, set the value in $_SERVER['PEAR_APIDOC_DIR']
+     * To override default, set the value in $_ENV['PEAR_APIDOC_DIR']
      * before this file is included.
      */
     define('PEAR_APIDOC_DIR', '/var/lib/pear/apidoc/'); 
 }
 
-if (isset($_SERVER['PEAR_PATCHES'])) {
+if (isset($_ENV['PEAR_PATCHES'])) {
     /**
      * @ignore
      */
-    define('PEAR_PATCHES', $_SERVER['PEAR_PATCHES']);
+    define('PEAR_PATCHES', $_ENV['PEAR_PATCHES']);
 } else {
     /**
      * Where patches can be found
      *
-     * To override default, set the value in $_SERVER['PEAR_PATCHES']
+     * To override default, set the value in $_ENV['PEAR_PATCHES']
      * before this file is included.
      */
     define('PEAR_PATCHES', '/var/lib/pear/patches/');
 }
 
-if (isset($_SERVER['PEAR_CVS'])) {
+if (isset($_ENV['PEAR_CVS'])) {
     /**
      * @ignore
      */
-    define('PEAR_CVS', $_SERVER['PEAR_CVS']);
+    define('PEAR_CVS', $_ENV['PEAR_CVS']);
 } else {
     /**
      * Where proposed patches reside
      *
-     * To override default, set the value in $_SERVER['PEAR_CVS']
+     * To override default, set the value in $_ENV['PEAR_CVS']
      * before this file is included.
      */
     define('PEAR_CVS', '/var/lib/pear/patches/cvs/');
@@ -183,50 +183,50 @@ define('PROPOSAL_STATUS_PROPOSAL_TIMELINE', (60 * 60 * 24 * 7)); // 1 week
  */
 define('PROPOSAL_STATUS_VOTE_TIMELINE', (60 * 60 * 24 * 7)); // 1 week
 
-if (isset($_SERVER['PROPOSAL_MAIL_PEAR_DEV'])) {
+if (isset($_ENV['PROPOSAL_MAIL_PEAR_DEV'])) {
     /**
      * @ignore
      */
-    define('PROPOSAL_MAIL_PEAR_DEV', $_SERVER['PROPOSAL_MAIL_PEAR_DEV']);
+    define('PROPOSAL_MAIL_PEAR_DEV', $_ENV['PROPOSAL_MAIL_PEAR_DEV']);
 } else {
     /**
      * PEPr: the address of the PEAR Developer email list
      *
      * Notices of changes will be sent to this address.
      *
-     * To override default, set the value in $_SERVER['PROPOSAL_MAIL_PEAR_DEV']
+     * To override default, set the value in $_ENV['PROPOSAL_MAIL_PEAR_DEV']
      * before this file is included.
      */
     define('PROPOSAL_MAIL_PEAR_DEV', 'PEAR developer mailinglist <pear-dev@lists.php.net>');
 }
 
-if (isset($_SERVER['PROPOSAL_MAIL_PEAR_GROUP'])) {
+if (isset($_ENV['PROPOSAL_MAIL_PEAR_GROUP'])) {
     /**
      * @ignore
      */
-    define('PROPOSAL_MAIL_PEAR_GROUP', $_SERVER['PROPOSAL_MAIL_PEAR_GROUP']);
+    define('PROPOSAL_MAIL_PEAR_GROUP', $_ENV['PROPOSAL_MAIL_PEAR_GROUP']);
 } else {
     /**
      * PEPr: the address of the PEAR Group email list
      *
      * Notices of some changes get sent to this address.
      *
-     * To override default, set the value in $_SERVER['PROPOSAL_MAIL_PEAR_GROUP']
+     * To override default, set the value in $_ENV['PROPOSAL_MAIL_PEAR_GROUP']
      * before this file is included.
      */
     define('PROPOSAL_MAIL_PEAR_GROUP', 'PEAR group <pear-group@php.net>');
 }
 
-if (isset($_SERVER['PROPOSAL_MAIL_FROM'])) {
+if (isset($_ENV['PROPOSAL_MAIL_FROM'])) {
     /**
      * @ignore
      */
-    define('PROPOSAL_MAIL_FROM', $_SERVER['PROPOSAL_MAIL_FROM']);
+    define('PROPOSAL_MAIL_FROM', $_ENV['PROPOSAL_MAIL_FROM']);
 } else {
     /**
      * PEPr: the email address used as the From header
      *
-     * To override default, set the value in $_SERVER['PROPOSAL_MAIL_FROM']
+     * To override default, set the value in $_ENV['PROPOSAL_MAIL_FROM']
      * before this file is included.
      */
     define('PROPOSAL_MAIL_FROM', 'PEPr <pear-sys@php.net>');
