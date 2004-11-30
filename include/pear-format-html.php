692b8879c110a53b6f152c34a1993a73ae94707d
Helgi Þormar Þorbjörnsson <dufuz@php.net>
2004-11-30 06:14:52 +0000

    PHP bugs are allowed skip the #, if any issues come out of that, we'll just remove the PHP bugs reconicion
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@173805 c90b9560-bf6c-de11-be94-00142212c4b1

--- include/pear-format-html.php
+++ include/pear-format-html.php
@@ -1055,7 +1055,7 @@ function validate_captcha($max_age = 300)
  */
 function make_ticket_links($text)
 {
-    $text = preg_replace('/(?<=php)\s*(bug|request)\s*#([0-9]+)/i',
+    $text = preg_replace('/(?<=php)\s*(bug|request)\s*#?([0-9]+)/i',
                          ' <a href="http://bugs.php.net/\\2">\\1 \\2</a>',
                          $text);
     $text = preg_replace('/(?<!>)(bug|request)\s*#([0-9]+)/i',
