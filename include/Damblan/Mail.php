fcbfd9b7c1658627855a7b795e8d43da92d49842
Tobias Schlitt <toby@php.net>
2004-12-13 11:53:26 +0000

    * Added lots of docs. * Refactored code. * Added default headers to avoid errors.
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@174600 c90b9560-bf6c-de11-be94-00142212c4b1

--- include/Damblan/Mail.php
+++ include/Damblan/Mail.php
@@ -9,28 +9,51 @@
     class Damblan_Mail {
 
         /**
-         * The template data 
+         * The template data.
+         * A template consists of an array with several keys. The main key is "Body",
+         * which contains the text of the body. The rest of the keys represent email
+         * headers like "To", "From", "Reply-To" or "X-Mailer". Except the body, every
+         * key may contain an array of strings, the body itself may only be a string.
+         * If one of the header keys is an array, the elements of this array will be
+         * concated using the string ", ". For example templates, see the 
+         * include/Damblan/Mail/ directory, where all templates are stored.
          *  
-         * @var string
+         * @var array
          * @since  
          */
         private $_template = '';
 
         /**
-         * The data to replace in the template 
+         * The data to replace in the template.
+         * This is an array of keys which represent variables (without the %-sign
+         * used in the template), associated with the data to replace the variables.
          *  
          * @var array
-         * @since  
+         * @since
          */
         private $_data = array(); 
 
         /**
-         * Private constructor, use factory methods
+         * Default headers.
+         * These default headers will be used, if one of those is neither set through the
+         * template nor through the additional headers submitted to the send() method.
+         *  
+         * @var array
+         * @since
+         */
+        private $_defaultHeaders = array(
+            'To'        => 'pear-webmaster@lists.php.net',
+            'From'      => 'pear-sys@php.net'
+            'Reply-To'  => 'pear-webmaster@lists.php.net',
+            'X-Mailer'  => 'PEARWeb - http://pear.php.net'
+        ); 
+
+        /**
+         * Private constructor, use factory methods!
          *
          *  
-         * @since  
+         * @since
          * @access private
-         * @param  
          * @return void
          */
         private function __construct ()
@@ -38,15 +61,30 @@
         }
 
         /**
-         * Create a new mail from a template 
-         *  
-         *  
+         * Create a new mail from a template.
+         * This static method will create a new Damblan_Mail instance for you. The
+         * given parameters represent the $_template and $_data variables of this class.
+         * See their documentation for further information.
+         *
          * @since  
          * @access public
-         * @param array $template The template to load (see example templates for further info)
-         * @param array $data The data to set in the template (in the formate 'variable' => 'value')
+         *
+         * @param array $template The template. This can either be a name of a template or the
+         *                        the template itself (the array). If you choose to use the template's
+         *                        name, the template file will automatically be included. The template
+         *                        file has to reside in the include/Damblan/Mail/ directory and follow
+         *                        the following naming conventions: <name_of_the_template>.tpl.php.
+         *
+         * @param array $data     The data to set in the template (in the formate 'variable' => 'value').
+         *
+         * @see Damblan_Mail::$_data
+         * @see Damblan_Mail::$_template
+         *
          * @static
-         * @return Damblan_Mailer The created mailer object
+         *
+         * @return Damblan_Mailer The created mailer object.
+         * @throws Exception If the template file chosen does not exist.
+         * @throws Exception If the data submited is not an array.
          */
         public static function create ($template, $data)
         {
@@ -64,27 +102,27 @@
         }
         
         /**
-         * Send the mail
-         *  
+         * Send the mail.
+         * This method is used to send a generated email. When calling this method,
+         * the template itself is compiled. Afterwards, the existing headers are merged
+         * with the additional header submited to this method.
          *  
          * @since
          * @access public
+         * @param array $headers Additional headers to use when sending an email. Note, that
+                                 none of these headers will overwrite existing headers, set in
+                                 the template. If a header already exists in the template
          * @return void
          */
         public function send ( $headers )
         {
+            // Compile the template
             $data = $this->_compile();
             // Merge additional header information to the generated data
-            foreach ($headers as $headerName => $header) {
-                if (is_array($header)) {
-                    foreach ($header as $element) {
-                        $data[$headerName][] = $element;
-                    }
-                } else {
-                    $data[$headerName][] = $header;
-                }
-            }
-            // Restructure data for use with PEAR::Mail
+            $data = $this->_mergeHeaders($data, $headers);
+            // Check sanity of the email headers
+            $data = $this->_sanitize($data);
+            // Restructure data for use with PEAR::Mail (To-header and body are submitted directly)
             foreach ($data as $field => $content) {
                 switch (strtolower($field)) {
                 case 'to':
@@ -104,6 +142,9 @@
             }
             // Attempt to send mail:
             $mail = Mail::factory('mail');
+            if (PEAR::isError($mail)) {
+                throw new Exception('Could not create Mail instance. '.$mail->getMessage());
+            }
             $res = $mail->send($to, $data, $body);
             if (PEAR::isError($res)) {
                 throw new Exception('Unable to send mail. '.$res->getMessage());
@@ -112,12 +153,78 @@
         }
 
         /**
-         * Compile the data into the template 
-         *  
+         * Sanitize headers. 
+         * Sanity checks for the headers of the mail. If a header is missing, it's set to a
+         * default value, provided from Damblan_Mail::$_defaultHeaders.
          *  
          * @since  
          * @access private
-         * @return void
+         * @return array $data The sanitized template data.
+         */
+        private function _sanitize ($data)
+        {
+            foreach ($this->_defaultHeaders as $headerName => $header) {
+                if (!isset($data[$headerName])) {
+                    $data[$headerName] = $header;
+                }
+            }
+            return $data;
+        }
+
+        /**
+         * Merge new headers into the template.
+         * This method merges the newly submited headers into those set by the template.
+         * The merge is done in the following ways:
+         *  - If no old header was set, the new one is added.
+         *  - If the new header is an array and the old one a string, the old one is
+         *    converted to an array and then merged with the new (without overwriting).
+         *  - If both (old and new header) are an array, they are merged (without
+         *    overwriting).
+         *  - If the old header is an array and the new one is a string, the new header
+         *    is added to the array.
+         *  - If both (old and new header) are strings, the new header _overwrites_ the
+         *    old one!!
+         *  
+         * @since  
+         * @access private
+         * @param array $data The compiled template data.
+         * @param array $headers The new header data to merge.
+         * @return array $data The merged template data.
+         */
+        private function _mergeHeaders ( $data, $headers )
+        {
+            foreach ($headers as $headerName => $header) {
+                // The new header is an array
+                if (is_array($header)) {
+                    // If the header set in the template is not an array, convert it.
+                    if (isset($data[$headerName]) && (!is_array($data[$headerName]))) {
+                        $data[$headerName] = array($data[$headerName]);
+                    }
+                    // Add the new headers
+                    foreach ($header as $element) {
+                        $data[$headerName][] = $element;
+                    }
+                // The new header is a string
+                } else {
+                    // The old header is an array, merge the new one
+                    if (isset($data[$headerName]) && is_array($data[$headerName])) {
+                        $data[$headerName][] = $header;
+                    // The old header is a string, overwrite it
+                    } else {
+                        $data[$headerName] = $header;
+                    }
+                }
+            }
+            return $data;
+        }
+
+        /**
+         * Compile the data into the template.
+         * The compilation is processed through all data elements (headers and body).
+         *  
+         * @since  
+         * @access private
+         * @return array $data The compiled data.
          */
         private function _compile ()
         {
@@ -146,5 +253,4 @@
             return $res;
         }
     }
-
 ?>
