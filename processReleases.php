4b97c1dd5da340904bd612675de714f60bf1c976
Greg Beaver <cellog@php.net>
2004-12-27 07:03:31 +0000

    this file can be used to update the files table for all existing releases
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@175647 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ processReleases.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'DB.php';
+require_once 'Archive/Tar.php';
+// before running this file, please run
+// ALTER TABLE files ADD packagexml TEXT NOT NULL DEFAULT "";
+
+$dbh = &DB::connect('mysql://pear:pear@localhost/pear');
+$rows = $dbh->getAssoc("SELECT fullpath, release, id FROM files", false, null, DB_FETCHMODE_ASSOC);
+foreach ($rows as $file => $info) {
+    $tar = &new Archive_Tar($file);
+    if ($packagexml = $tar->extractInString('package.xml')) {
+        $dbh->query('UPDATE files SET packagexml = ? WHERE release = ? AND id = ?', array($packagexml,
+            $info['release'], $info['id']));
+    }
+}
+?>
