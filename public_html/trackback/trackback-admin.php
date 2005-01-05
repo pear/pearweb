8f678f263d86405fa75c70389d89c4eee3c06688
Tobias Schlitt <toby@php.net>
2005-01-05 22:59:22 +0000

    * Renamed Damblan_Mail -> Damblan_Mailer. * Switched to $trackback->getMaintainer(). * Activated mails for delete and approve. * Updated authors field.
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@176542 c90b9560-bf6c-de11-be94-00142212c4b1

--- public_html/trackback/trackback-admin.php
+++ public_html/trackback/trackback-admin.php
@@ -1,6 +1,27 @@
 <?php
 
+/*
+   +----------------------------------------------------------------------+
+   | PEAR Web site version 1.0                                            |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 2001-2004 The PHP Group                                |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 2.02 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available at through the world-wide-web at                           |
+   | http://www.php.net/license/2_02.txt.                                 |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Authors: Tobias Schlitt <toby@php.net>                               |
+   |                                                                      |
+   +----------------------------------------------------------------------+
+   $Id$
+*/
+
 require_once 'Damblan/Trackback.php';
+require_once 'Damblan/Mailer.php';
 
 auth_require('pear.dev');
 
@@ -17,24 +38,33 @@ if (!empty($action)) {
     $res = $trackback->load($dbh);
 }
 
+$mailData = array(
+    'id' => $trackback->id,
+    'blog_name' => $trackback->blog_name,
+    'title' => $trackback->title,
+    'url' => $trackback->url,
+    'excerpt' => $trackback->excerpt,
+    'date' => make_utc_date($trackback->timestamp),
+    'timestamp' => $trackback->timestamp,
+    'user' => $_COOKIE['PEAR_USER'],
+);
+
 switch ($action) {
 case 'approve':
-    try {
-        $trackback->approve($dbh);
-    } catch (Exception $e) {
-        PEAR::raiseError('Unable to approve trackback.');
-    }
+    $trackback->approve($dbh);
+    $mailer = Damblan_Mailer::create('Trackback_Approve', $mailData);
+    $additionalHeaders['To'] = $trackback->getMaintainers();
+    $mailer->send($additionalHeaders);
     $msg = '<div class="success">Trackback successfully approved.</div>';
     break;
 case 'delete':
     $msg = '<div class="warnings">Really <a href="/trackback/trackback-admin.php?action=delete_verified&id='.$trackback->id.'&timestamp='.$trackback->timestamp.'">delete</a> trackback '.$timestamp.' for '.$id.'?</div>';
     break;
 case 'delete_verified':
-    try {
-        $trackback->delete($dbh);
-    } catch (Exception $e) {
-        PEAR::raiseError('Unable to verify trackback.');
-    }
+    $trackback->delete($dbh);
+    $mailer = Damblan_Mailer::create('Trackback_Delete', $mailData);
+    $additionalHeaders['To'] = $trackback->getMaintainers();
+    $mailer->send($additionalHeaders);
     $msg = '<div class="success">RIP trackback.</div>';
     break;
 }
