88316f6a1d4ba3a21e8cf46c924fc58ad7c7e276
Martin Jansen <mj@php.net>
2005-01-20 20:37:11 +0000

    * Added documentation for the XML-RPC cache.
    # No functional changes in this commit.
    
    
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@177790 c90b9560-bf6c-de11-be94-00142212c4b1

--- include/xmlrpc-cache.php
+++ include/xmlrpc-cache.php
@@ -22,10 +22,28 @@ require_once "Cache.php";
 
 error_reporting(0);
 
+/**
+ * Class to cache XML_RPC requests
+ *
+ * This class employs the Cache package in order to cache the result of 
+ * calls to the XML-RPC service.
+ *
+ * @author Christian Dickmann <dickmann@php.net>
+ * @version $Revision$
+ */
 class XMLRPC_Cache
 {
     var $cache;
-        
+
+    /**
+     * Attempts to return a reference to a concrete XMLRPC_Cache instance
+     *
+     * This method will only create a new XMLRPC_Cache instance if no
+     * instance already exists.
+     *
+     * @access public
+     * @return object XMLRPC_Cache Concrete XMLRPC_Cache reference
+     */
     function &singleton()
     {
         static $instance;
@@ -37,16 +55,30 @@ class XMLRPC_Cache
         return $instance;
     }
 
+    /**
+     * Constructor
+     *
+     * This method creates an instance of Cache_Lite for later use.
+     */
     function XMLRPC_Cache()
     {
         $this->cache = new Cache('file', 
                                  array(
-                                       'cache_dir'       => PEAR_TMPDIR.'/cache/',
+                                       'cache_dir'       => PEAR_TMPDIR . '/cache/',
                                        'filename_prefix' => 'cache_xmlrpc_',
                                        )
                                  );
     }
-        
+
+    /**
+     * Get cached result of a previous method call
+     *
+     * @access public
+     * @param  string Name of the method
+     * @param  array  Arguments for the method
+     * @param  int    Age in secondsprevious to which the cache content will be discarded.
+     * @return string Result of the method call
+     */
     function get($method, $args, $maxAge = null)
     {
         $id = $this->cache->generateID(array($method, $args));
@@ -65,13 +97,28 @@ class XMLRPC_Cache
         return $this->cache->get($id);
     }
 
+    /**
+     * Caches a method call
+     *
+     * @access public
+     * @param  string Name of the method
+     * @param  array  Arguments for the method
+     * @param  string Return value of the method call
+     */
     function save($method, $args, $value)
     {
         $id = $this->cache->generateID(array($method, $args));
 
         return $this->cache->save($id, $value);
     }
-        
+
+    /**
+     * Removes a method call result from the cache
+     *
+     * @access public
+     * @param  string Name of the method
+     * @param  array  Arguments for the method
+     */
     function remove($method, $args)
     {
         $id = $this->cache->generateID(array($method, $args));
