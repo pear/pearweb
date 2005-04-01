d7f626d3d07d43671755e33f118b352a98bbffcd
Tobias Schlitt <toby@php.net>
2005-04-01 19:40:58 +0000

    * Adding slides from yesterdays PEAR talk.
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@183354 c90b9560-bf6c-de11-be94-00142212c4b1

--- public_html/support/slides.php
+++ public_html/support/slides.php
@@ -52,6 +52,17 @@ that have been held about PEAR.</p>
 
 <ul>
  <li>
+  <p><cite><a href="http://www.schlitt.info/download/PHP_Quebec_2005_Session_toby.pdf">
+  PEAR</cite></a></p>
+  <ul>
+    <li>Date: 2005-03-31</li>
+    <li>Presenter: <?php echo user_link("toby", true); ?></li>
+    <li>Venue: PEAR Introduction Session, PHP Conference Quebec 2005, 
+    Montreal, Canada</li>
+    <li>Language: English</li>
+  </ul>
+ </li>
+ <li>
   <p><cite><a href="http://www.schlitt.info/download/PEAR_Workshop_Quebec_2005_toby.tar.gz">
   PEAR</cite></a></p>
   <ul>
