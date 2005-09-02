da2ce06d376cfd6cbde4992acc6b926d0ff16cf8
Martin Jansen <mj@php.net>
2005-09-02 09:54:27 +0000

    * Added pear.php-tools.net.
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@194904 c90b9560-bf6c-de11-be94-00142212c4b1

--- public_html/channels/index.php
+++ public_html/channels/index.php
@@ -43,6 +43,7 @@ on the individual pages.</p>
 <ul>
   <li><a href="http://pear.horde.org/">Horde</a></li>
   <li><a href="http://pearified.com/">Pearified</a></li>
+  <li><a href="http://pear.php-tools.net/">PHP Application Tools</a></li>
   <li><a href="http://seagull.phpkitchen.com/docs/wakka.php?wakka=UsingThePearPackageManager&v=18nk">Seagull</a></li>
   <li><a href="http://solarphp.com/home/index.php?area=Main&page=DownloadInstall#toc1">Solar</a></li>
 </ul>
