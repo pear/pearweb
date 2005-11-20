bb356b44a95aa7c2a00db93da071421183d756ab
Arnaud Limbourg <arnaud@php.net>
2005-11-20 20:22:06 +0000

    list orphan packages. This is a qa restricted page because the load on the server is high enough as it is.
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@200927 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ public_html/qa/packages_orphan.php
@@ -0,0 +1,47 @@
+<?php
+/*
+   +----------------------------------------------------------------------+
+   | PEAR Web site version 1.0                                            |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 2001-2005 The PHP Group                                |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 2.02 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available at through the world-wide-web at                           |
+   | http://www.php.net/license/2_02.txt.                                 |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Authors: Arnaud Limbourg <arnaud@limbourg.com>                       |
+   +----------------------------------------------------------------------+
+   $Id$
+*/
+
+/**
+ * List orphan packages
+ */
+auth_require('pear.qa');
+
+response_header('Quality Assurance Initiative - Orphan packages',
+    false, $extra_header);
+
+$packages = $dbh->getAssoc("SELECT p.name " .
+    " FROM packages p ".
+    "WHERE p.unmaintained=1 AND p.package_type = 'pear' AND p.approved = 1 ".
+    "ORDER BY p.name", false, null, DB_FETCHMODE_ASSOC);
+
+echo "<h3>List of orphan packages</h3>\n";
+
+echo "<ul>\n";
+foreach ($packages as $pck => $info) {
+
+    $link = make_link('/package/' . $info['name'], 
+        $package, '', 'title="' . $info['name'] . '"');
+
+    echo '<li>' . $link . "</li>\n";
+}
+echo "</ul>\n";
+
+response_footer();
+?>
