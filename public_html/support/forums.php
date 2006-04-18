004a33c6bf0a6f63b902225e2a746ded9a6e6c0c
Pierre-Alain Joye <pajoye@php.net>
2006-04-18 16:24:46 +0000

    - forum page
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@211629 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ public_html/support/forums.php
@@ -0,0 +1,55 @@
+<?php
+/*
+   +----------------------------------------------------------------------+
+   | PEAR Web site version 1.0                                            |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 2004-2005 The PEAR Group                               |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 2.02 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available at through the world-wide-web at                           |
+   | http://www.php.net/license/2_02.txt.                                 |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Authors: Martin Jansen <mj@php.net>                                  |
+   +----------------------------------------------------------------------+
+   $Id$
+*/
+
+response_header("Support - Tutorials");
+?>
+
+<h1>Support</h1>
+
+<?php
+include './tabs_list.php';
+?>
+
+<h2>&raquo; Forums</h2>
+
+<p>The following is a list of pointers to external forums about
+PEAR packages.</p>
+
+<ul class="spaced">
+ <li>
+  <a href="http://codewalkers.com/forum/index.php?forum=pear">Codewalker</a>: PEAR Forum</a>.
+ </li>
+ <li>
+  <cite><a href="http://www.pear-forum.org/">pear-forum.org</a>: forum dedicated to PEAR </a></cite></li>
+ <li>
+  <cite><a href="http://www.pear-forum.de">pear-forum.de</a></cite>: German PEAR forum.
+ </li>
+</ul>
+
+<p><a href="/support/">&laquo; Back to the Support overview</a></p>
+
+<?php echo hdelim(); ?>
+
+<p>Do you know of other forums that should be added here? 
+Please let the <a href="mailto:pear-webmaster@lists.php.net">webmaster 
+team</a> know about them.</p>
+
+<?php
+response_footer();
