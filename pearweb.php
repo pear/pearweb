008f9f66374a2af7f2a64c5942fd1e27afbdd983
Greg Beaver <cellog@php.net>
2006-09-27 04:36:51 +0000

    add skeleton for post-install script to make it easier to install a debug copy of pearweb
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@220591 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ pearweb.php
@@ -0,0 +1,14 @@
+<?php
+class pearweb_postinstall
+{
+    function init(&$config, &$pkg, $lastversion)
+    {
+        
+    }
+
+    function run($answers, $phase)
+    {
+        return true;
+    }
+}
+?>
