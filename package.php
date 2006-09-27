03908cbb5d04b161966feb2b2e20ada87b9d7105
Greg Beaver <cellog@php.net>
2006-09-27 04:48:53 +0000

    add beginnings of package.xml generation script for pearweb
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@220592 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ package.php
@@ -0,0 +1,47 @@
+<?php
+require_once 'PEAR/PackageFileManager2.php';
+PEAR::setErrorHandling(PEAR_ERROR_DIE);
+$a = PEAR_PackageFileManager2::importOptions(dirname(__FILE__) . '/package.xml',
+    array(
+        'baseinstalldir' => '/',
+        'filelistgenerator' => 'cvs',
+        'roles' => array('*' => 'web'),
+        'exceptions' => array('pearweb.php' => 'php'),
+        'simpleoutput' => true,
+    ));
+$a->setReleaseVersion('0.2.0');
+$a->setNotes('use web role, add post-install script');
+$a->resetUsesrole();
+$a->addUsesRole('web', 'Role_Web', 'pearified.com');
+$a->clearDeps();
+$a->setPhpDep('4.3.0');
+$a->setPearInstallerDep('1.4.11');
+$a->addPackageDepWithChannel('required', 'PEAR', 'pear.php.net', '1.4.11');
+$a->addPackageDepWithChannel('required', 'Archive_Tar', 'pear.php.net', '1.3.1');
+$a->addPackageDepWithChannel('required', 'XML_RPC', 'pear.php.net', '1.5.0');
+$a->addPackageDepWithChannel('required', 'HTTP', 'pear.php.net', '1.3.1');
+$a->addPackageDepWithChannel('required', 'HTTP_Request', 'pear.php.net', '1.2.2');
+$a->addPackageDepWithChannel('required', 'VFS', 'pear.php.net', '1.2');
+$a->addPackageDepWithChannel('required', 'DB', 'pear.php.net', '1.6.5');
+$a->addPackageDepWithChannel('required', 'Cache', 'pear.php.net', '1.2');
+$a->addPackageDepWithChannel('required', 'HTML_Form', 'pear.php.net', '1.3.0');
+$a->addPackageDepWithChannel('required', 'HTML_Table', 'pear.php.net', '1.5');
+$a->addPackageDepWithChannel('required', 'Pager', 'pear.php.net', '2.2.0');
+$a->addPackageDepWithChannel('required', 'Net_URL', 'pear.php.net', '1.0.14');
+$a->addPackageDepWithChannel('required', 'HTTP_Upload', 'pear.php.net', '0.8.1');
+$a->addPackageDepWithChannel('required', 'DB_Pager', 'pear.php.net', '0.7');
+$a->addPackageDepWithChannel('required', 'Log', 'pear.php.net', '1.8.4');
+$a->addPackageDepWithChannel('required', 'Mail', 'pear.php.net', '2.0b2');
+$a->addPackageDepWithChannel('required', 'Services_Trackback', 'pear.php.net', '0.4.0');
+$a->addPackageDepWithChannel('required', 'HTML_BBCodeParser', 'pear.php.net', '1.0');
+$a->addPackageDepWithChannel('required', 'HTML_QuickForm', 'pear.php.net', '3.2.3');
+$a->addExtensionDep('required', 'pcre');
+$a->addExtensionDep('optional', 'mysql');
+$a->addExtensionDep('optional', 'mysqli');
+$a->addPackageDepWithChannel('required', 'Role_Web', 'pearified.com');
+$script = &$a->initPostinstallScript('pearweb.php');
+$script->addParamGroup('start', false, 'test');
+$a->addPostinstallTask($script, 'pearweb.php');
+$a->generateContents();
+$a->writePackageFile();
+?>
