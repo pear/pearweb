37a7dbfae9a11fa56595908947f8ef51da16644c
Greg Beaver <cellog@php.net>
2006-09-30 22:24:21 +0000

    extensive changes to package.xml
    1) fix index in MDB2_Schema xml file
    2) implement post-installation script to create database
    3) fix dependencies on weird/non-existent versions
    4) use pearified/Role_Web (this will be controversial)
    
    However, now, pearweb can be installed using package.xml
    
    All one needs to do is:
    
    1) install pearified.com/Role_Web and run post-install script
    2) run pear upgrade -o package.xml
    3) run pear run-scripts pearweb
    4) read docs/apache-setup.txt and configure apache .conf file
    
    the next step is making the post-install script smart enough to diff the installed database with the distributed one.
    
    
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@220835 c90b9560-bf6c-de11-be94-00142212c4b1

--- package.php
+++ package.php
@@ -19,7 +19,8 @@ $a->setPearInstallerDep('1.4.11');
 $a->addPackageDepWithChannel('required', 'PEAR', 'pear.php.net', '1.4.11');
 $a->addPackageDepWithChannel('required', 'Archive_Tar', 'pear.php.net', '1.3.1');
 $a->addPackageDepWithChannel('required', 'HTTP_Request', 'pear.php.net', '1.2.2');
-$a->addPackageDepWithChannel('required', 'VFS', 'pear.php.net', '1.2');
+// used only in cron jobs
+$a->addPackageDepWithChannel('required', 'VFS', 'pear.php.net', '0.1.0');
 $a->addPackageDepWithChannel('required', 'DB', 'pear.php.net', '1.6.5');
 $a->addPackageDepWithChannel('required', 'Cache', 'pear.php.net', '1.2');
 $a->addPackageDepWithChannel('required', 'HTML_Form', 'pear.php.net', '1.3.0');
@@ -27,9 +28,10 @@ $a->addPackageDepWithChannel('required', 'HTML_Table', 'pear.php.net', '1.5');
 $a->addPackageDepWithChannel('required', 'Pager', 'pear.php.net', '2.2.0');
 $a->addPackageDepWithChannel('required', 'Net_URL', 'pear.php.net', '1.0.14');
 $a->addPackageDepWithChannel('required', 'HTTP_Upload', 'pear.php.net', '0.8.1');
+$a->addPackageDepWithChannel('required', 'MDB2_Schema', 'pear.php.net', '0.6.0');
 $a->addPackageDepWithChannel('required', 'DB_Pager', 'pear.php.net', '0.7');
 $a->addPackageDepWithChannel('required', 'Log', 'pear.php.net', '1.8.4');
-$a->addPackageDepWithChannel('required', 'Mail', 'pear.php.net', '2.0b2');
+$a->addPackageDepWithChannel('required', 'Mail', 'pear.php.net', '1.1.13');
 $a->addPackageDepWithChannel('required', 'Services_Trackback', 'pear.php.net', '0.4.0');
 $a->addPackageDepWithChannel('required', 'HTML_BBCodeParser', 'pear.php.net', '1.0');
 $a->addPackageDepWithChannel('required', 'HTML_QuickForm', 'pear.php.net', '3.2.3');
@@ -38,8 +40,18 @@ $a->addExtensionDep('optional', 'mysql');
 $a->addExtensionDep('optional', 'mysqli');
 $a->addPackageDepWithChannel('required', 'Role_Web', 'pearified.com');
 $script = &$a->initPostinstallScript('pearweb.php');
-$script->addParamGroup('start', false, 'test');
+$script->addParamGroup(
+    'init',
+    array(
+        $script->getParam('driver', 'Database driver', 'string', 'mysqli'),
+        $script->getParam('user', 'Database User name', 'string', 'pear'),
+        $script->getParam('password', 'Database password', 'password', 'pear'),
+        $script->getParam('host', 'Database host', 'string', 'localhost'),
+        $script->getParam('database', 'Database name', 'string', 'pear'),
+    ));
+
 $a->addPostinstallTask($script, 'pearweb.php');
+$a->addReplacement('pearweb.php', 'pear-config', '@web-dir@', 'web_dir');
 $a->generateContents();
 $a->writePackageFile();
 ?>
--- pearweb.php
+++ pearweb.php
@@ -1,14 +1,57 @@
 <?php
+require_once 'MDB2/Schema.php';
 class pearweb_postinstall
 {
+    var $lastversion;
+    var $dsn;
+    /**
+     * Frontend object
+     * @var PEAR_Frontend
+     * @access private
+     */
+    var $_ui;
+
     function init(&$config, &$pkg, $lastversion)
     {
-        
+        $this->_ui = &PEAR_Frontend::singleton();
+        $this->lastversion = $lastversion;
+        return true;
     }
 
     function run($answers, $phase)
     {
+        switch ($phase) {
+            case 'init' :
+                return $this->initializeDatabase($answers);
+                break;
+        }
+        return true;
+    }
+
+    function initializeDatabase($answers)
+    {
+        $this->dsn = array(
+            'phptype' => $answers['driver'],
+            'username' => $answers['user'],
+            'password' => $answers['password'],
+            'hostspec' => $answers['host'],
+            'database' => $answers['database']);
+        $a = MDB2_Schema::factory($this->dsn,
+            array('idxname_format' => '%s',
+                  'seqname_format' => 'id',
+                  'quote_identifier' => true));
+        $c = $a->parseDatabaseDefinitionFile(
+            realpath('@web-dir@/sql/pearweb_mdb2schema.xml'));
+        $c['name'] = $answers['database'];
+        $c['create'] = 1;
+        PEAR::staticPushErrorHandling(PEAR_ERROR_RETURN);
+        $err = $a->createDatabase($c);
+        PEAR::staticPopErrorHandling();
+        if (PEAR::isError($err)) {
+            $this->_ui->outputData($err->getUserInfo());
+            $this->_ui->outputData($err->getMessage());
+            return false;
+        }
         return true;
     }
 }
-?>
