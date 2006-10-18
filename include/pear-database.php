f5b3ebf517998a4aa46e28b12487a407b44c97d1
Greg Beaver <cellog@php.net>
2006-10-18 00:42:01 +0000

    store downloads directly in aggregated_package_stats table
      # step 5 is update database and pear-database.php, make sure it works
      # step 6 is delete old downloads from downloads,
                         run update script
                         truncate downloads
    
    
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@221931 c90b9560-bf6c-de11-be94-00142212c4b1

--- include/pear-database.php
+++ include/pear-database.php
@@ -13,9 +13,10 @@
    | obtain it through the world-wide-web, please send a note to          |
    | license@php.net so we can mail you a copy immediately.               |
    +----------------------------------------------------------------------+
-   | Authors: Stig Sæther Bakken <ssb@fast.no>                            |
+   | Authors: Stig Bakken <ssb@fast.no>                                   |
    |          Tomas V.V.Cox <cox@php.net>                                 |
    |          Martin Jansen <mj@php.net>                                  |
+   |          Gregory Beaver <cellog@php.net>                             |
    +----------------------------------------------------------------------+
    $Id$
 */
@@ -2108,16 +2109,47 @@ class release
     {
         global $dbh;
 
-        $id = $dbh->nextId("downloads");
+        $dbh->query('UPDATE aggregated_package_stats
+            SET downloads=downloads+1
+            WHERE
+                package_id=? AND
+                release_id=? AND
+                yearmonth="' . date('Y-m-01') . '"',
+            array($package, $release_id));
+        if ($dbh->affectedRows() == 0) {
+    		$dbh->query('INSERT INTO aggregated_package_stats
+    			(package_id, release_id, yearmonth, downloads)
+    			VALUES(?,?,?,1)',
+    			array($package, $release_id, date('Y-m-01')));
+        }
 
-        $query = "INSERT INTO downloads (id, file, package, release, dl_when, dl_who) VALUES (?, ?, ?, ?, ?, ?)";
-
-        $err = $dbh->query($query, array($id, (int)$file, $package,
-                                         $release_id, date("Y-m-d H:i:s"),
-                                         $_SERVER['REMOTE_ADDR']
-                                         ));
+//      This method can be used when we have MySQL 4.1,
+//      30% efficiency gain at least over previous method
+//		$dbh->query('INSERT INTO aggregated_package_stats
+//			(package_id, release_id, yearmonth, downloads)
+//			VALUES(?,?,?,1)
+//			ON DUPLICATE KEY UPDATE downloads=downloads+1',
+//			array($package, $release_id, date('Y-m-01')));
 
         // {{{ Update package_stats table
+
+//      This method can be used when we have MySQL 4.1,
+//      30% efficiency gain at least over previous method
+//        $query = 'INSERT INTO package_stats
+//        			 (dl_number, package, release, pid, rid, cid, last_dl)
+//               	     VALUES (1, ?, ?, ?, ?, ?, ?)
+//               	     ON DUPLICATE KEY UPDATE
+//               	     	dl_number=dl_number+1,
+//               	     	last_dl = "' . date('Y-m-d H:i:s') . '"';
+//
+//        $dbh->query($query, array($pkg_info['name'],
+//                                  $version,
+//                                  $package,
+//                                  $release_id,
+//                                  $pkg_info['categoryid'],
+//                                  date('Y-m-d H:i:s')
+//                                  )
+//                    );
 
         $query = 'UPDATE package_stats '
             . ' SET dl_number = dl_number + 1,'
