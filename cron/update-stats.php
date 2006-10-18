57278d5a948b4856e3ed04d3f2c9e84f21b3b2b1
Greg Beaver <cellog@php.net>
2006-10-18 01:36:53 +0000

    we won't need the extra stats_updated table, updates are put directly into aggregated_package_data now
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@221938 c90b9560-bf6c-de11-be94-00142212c4b1

--- cron/update-stats.php
+++ cron/update-stats.php
@@ -8,18 +8,10 @@ $options = array(
 $dbh =& DB::connect(PEAR_DATABASE_DSN, $options);
 $releases = $dbh->getAll('SELECT id from releases');
 foreach ($releases as $release) {
-    $lastupdate = $dbh->getOne('SELECT lastupdate FROM stats_updated WHERE release_id = ' . $release[0]);
-    if (PEAR::isError($lastupdate) || !$lastupdate) {
-        $lastupdate = '0000-00-00 00:00:00';
-        $dbh->query('INSERT INTO stats_updated (release_id, lastupdate) VALUES(' . $release[0] .
-            ', "' . $lastupdate . '")');
-    }
-    $curtime = $dbh->getOne('SELECT NOW()');
     $all = $dbh->getAll($q = 'SELECT r.package, r.id, DATE_FORMAT(d.dl_when, "%Y-%m-01"), COUNT(*)
                           FROM releases r, downloads d
                           WHERE r.id = ' . $release[0] . '
                             AND d.release = r.id
-                            AND d.dl_when > "' . $lastupdate . '"
                             AND d.package = r.package
                           GROUP BY YEAR(d.dl_when), MONTH(d.dl_when)
                           ORDER BY YEAR(d.dl_when) DESC, MONTH(d.dl_when) DESC');
@@ -29,7 +21,4 @@ foreach ($releases as $release) {
             VALUES(' . $data[0] . ',' . $data[1] . ',"' . $data[2] . '",' . $data[3] . ')
             ON DUPLICATE KEY UPDATE downloads=downloads+' . $data[3]);
     }
-    $dbh->query('UPDATE stats_updated SET lastupdate="' . $curtime . '" WHERE release_id=' . $release[0]);
-    // remove old download entries
-    $dbh->query('DELETE FROM downloads WHERE dl_when < "' . $curtime . '" AND release=' . $release[0]);
 }
