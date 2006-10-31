a5d6bd8c29e2fd8a5ace373629371382c22f9e36
Martin Jansen <mj@php.net>
2006-10-31 09:20:17 +0000

    * Download the Japanese version of the CHM manual, too.
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@222574 c90b9560-bf6c-de11-be94-00142212c4b1

--- cron/load-chm.sh
+++ cron/load-chm.sh
@@ -3,8 +3,9 @@
 cd /var/lib/pear/chm/
 
 /usr/bin/env curl -s "http://pear.markwiesemann.eu/manual/pear_manual_{de,en,fr}.chm" -o "pear_manual_#1.chm.NEW"
+/usr/bin/env curl -s "http://peardoc.m-takagi.org/chm/pear_manual_ja.chm" -o "pear_manual_ja.chm.NEW"
 
-for LANG in de en fr;
+for LANG in de en fr ja;
 do
     mv "pear_manual_${LANG}.chm.NEW" "pear_manual_${LANG}.chm"
     chmod 0644 "pear_manual_${LANG}.chm"
