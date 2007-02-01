9f41a86847000703fe7a90ce1180be1ce6050b0e
Greg Beaver <cellog@php.net>
2007-02-01 04:22:33 +0000

    remove PEAR_PFM dep, decided not to go that route, forgot to remove it
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@228618 c90b9560-bf6c-de11-be94-00142212c4b1

--- package.php
+++ package.php
@@ -39,7 +39,7 @@ $a->addPackageDepWithChannel('required', 'Cache', 'pear.php.net', '1.2');
 $a->addPackageDepWithChannel('required', 'HTML_Form', 'pear.php.net', '1.3.0');
 $a->addPackageDepWithChannel('required', 'HTML_Table', 'pear.php.net', '1.5');
 $a->addPackageDepWithChannel('required', 'Pager', 'pear.php.net', '2.2.0');
-$a->addPackageDepWithChannel('required', 'PEAR_PackageFileManager', 'pear.php.net', '1.6.0');
+//$a->addPackageDepWithChannel('required', 'PEAR_PackageFileManager', 'pear.php.net', '1.6.0');
 $a->addPackageDepWithChannel('required', 'Net_URL', 'pear.php.net', '1.0.14');
 $a->addPackageDepWithChannel('required', 'HTTP_Upload', 'pear.php.net', '0.8.1');
 $a->addPackageDepWithChannel('required', 'MDB2_Schema', 'pear.php.net', '0.6.0');
