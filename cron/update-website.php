d7d0dedd9ea8d5ecde6b9c5b589a7c23148f8c0f
Greg Beaver <cellog@php.net>
2007-02-13 03:13:01 +0000

    fixup to display errors when testing
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@229687 c90b9560-bf6c-de11-be94-00142212c4b1

--- cron/update-website.php
+++ cron/update-website.php
@@ -9,24 +9,33 @@
  * @version   $Id: trackback-cleanup.php,v 1.3 2006/02/07 18:14:38 mj Exp $
  */
 require_once 'PEAR/PackageUpdate.php';
-$pearweb = &PEAR_PackageUpdate::factory('CLI', 'pearweb', 'pear.php.net');
+require_once 'PEAR/PackageUpdate/Cli.php';
+$pearweb = &PEAR_PackageUpdate::factory('Cli', 'pearweb', 'pear.php.net');
 if ($pearweb) {
     $pearweb->setMinimumState(PEAR_PACKAGEUPDATE_STATE_STABLE);
     if ($pearweb->checkUpdate()) {
         if (!$pearweb->update()) {
             if ($pearweb->hasErrors()) {
+                $errors = PEAR_ErrorStack::staticGetErrors();
                 echo "errors\n";
+                foreach ($errors['PEAR_PackageUpdate_Cli'] as $err) {
+                    echo $err['message'] . "\n";
+                }
             }
         }
     }
 }
-$pearweb_phars = &PEAR_PackageUpdate::factory('CLI', 'pearweb_phars', 'pear.php.net');
+$pearweb_phars = &PEAR_PackageUpdate::factory('Cli', 'pearweb_phars', 'pear.php.net');
 if ($pearweb_phars) {
     $pearweb_phars->setMinimumState(PEAR_PACKAGEUPDATE_STATE_STABLE);
     if ($pearweb_phars->checkUpdate()) {
         if (!$pearweb_phars->update()) {
             if ($pearweb_phars->hasErrors()) {
+                $errors = PEAR_ErrorStack::staticGetErrors();
                 echo "errors\n";
+                foreach ($errors['PEAR_PackageUpdate_Cli'] as $err) {
+                    echo $err['message'] . "\n";
+                }
             }
         }
     }
