c35952c616d5ac2cebad76577990926c663245b1
David Coallier <davidc@php.net>
2007-03-12 05:58:32 +0000

    sql for manual_notes
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@231711 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ sql/manual_notes.sql
@@ -0,0 +1,18 @@
+/**
+ * Create tables for user comments
+ */
+
+CREATE TABLE manual_notes
+(
+    note_id int(11) NOT NULL AUTO_INCREMENT,
+    page_url varchar(100) NOT NULL,
+    user_name varchar(100) NOT NULL,
+    note_text text NOT NULL,
+    note_time timestamp NOT NULL,
+    note_approved varchar(6) NOT NULL DEFAULT 'pending',
+    note_approved_by int(11) NULL,
+    note_deleted tinyint(1) NULL DEFAULT '0',
+    PRIMARY KEY (note_id),
+    INDEX idx_page_url (page_url),
+    INDEX idx_note_time (note_time)
+);
