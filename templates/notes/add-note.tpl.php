f32cd212f5284e2105247097804b3f9c6b1f0bca
David Coallier <davidc@php.net>
2007-03-21 23:27:08 +0000

    - Latest version of add-note template. This will also be fixed later on. I will   split the forms into one file so we dont duplicate code more.
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@232276 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ templates/notes/add-note.tpl.php
@@ -0,0 +1,60 @@
+<?php response_header('UserNote'); ?>
+<h1>Note addition</h1>
+<?php 
+    // {{{ isset($error)
+    if (!empty($errors)) {
+?>
+
+<div class="errors">
+<?php
+foreach ($errors as $error) {
+    echo $error . '<br />'; 
+}
+?>
+</div>
+
+<form action="/notes/add-note.php" method="post">
+<input type="hidden" name="noteUrl" value="<?=$noteUrl?>" />
+<tbody>
+ <tr>
+  <td colspan="2"></td>
+ </tr>
+ <tr>
+  <!-- We will care after about finding the user, this is quick fix. -->
+  <th class="subr">Your email address (or name):</th>
+  <td>
+   <input name="user" size="60" maxlength="40" value="user@example.com" type="text" />
+  </td>
+ </tr>
+ <tr>
+  <th class="subr">Your notes:</th>
+  <td><textarea name="note" rows="20" cols="60" wrap="virtual"></textarea>
+   <br />
+  </td>
+ </tr>
+ <tr>
+  <th class="subr"><?=$spamQuestion?><br/></th>
+  <td><input name="answer" size="60" maxlength="10" type="text"></td>
+ </tr>
+ <tr>
+  <th colspan="2">
+   <!-- I'll add the preview soon -->
+   <input name="action" value="Add Note" type="submit">
+  </th>
+ </tr>
+</tbody>
+</form>
+<?php
+} else {
+?>
+    <div>
+    Note added, it will be reviewed soon and you will be contacted if there's any problem.
+    Go back to the previous url <a href="http://pear.php.net/<?=$noteUrl?>" 
+                                   title="Previous Note">http://<?=htmlentities($_SERVER['SERVER_NAME'], ENT_QUOTES, 'UTF-8');?><?=$noteUrl?></a>
+    </div>
+<?php 
+
+}
+response_footer(); 
+
+?>
