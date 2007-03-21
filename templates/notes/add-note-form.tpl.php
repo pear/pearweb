4131a66ad1136708f53faad3e29a89c4484b923e
David Coallier <davidc@php.net>
2007-03-21 22:29:26 +0000

    - Good form (For alpha version)
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@232265 c90b9560-bf6c-de11-be94-00142212c4b1

--- templates/notes/add-note-form.tpl.php
+++ templates/notes/add-note-form.tpl.php
@@ -1,40 +1,53 @@
 <?php response_header('UserNote'); ?>
 <h1>Add User Notes</h1>
-<form action="/notes/add-note.php?url=<?=$noteUrl?>">
+<?php 
+    // {{{ isset($error)
+    if (isset($error)) {
+?>
+
+<div class="errors"><?php echo $error; ?></div>
+
+<?php 
+    /**
+     * If there's no uri then let's just quit..
+     */
+    response_footer(); 
+    exit;
+    
+    }
+    // }}}
+?>
+
+<form action="/notes/add-note.php" method="post">
+<input type="hidden" name="noteUrl" value="<?=$noteUrl?>" />
 <tbody>
  <tr>
-   <td colspan="2">
-  </td>
-  </tr>
-  <tr>
+  <td colspan="2"></td>
+ </tr>
+ <tr>
   <!-- We will care after about finding the user, this is quick fix. -->
-   <th class="subr">Your email address (or name):</th>
-   <td><input name="user" size="60" maxlength="40" value="user@example.com" type="text"></td>
-  </tr>
-  <tr>
-   <th class="subr">Your notes:</th>
-
-   <td><textarea name="note" rows="20" cols="60" wrap="virtual"></textarea>
-   <br>
+  <th class="subr">Your email address (or name):</th>
+  <td>
+   <input name="user" size="60" maxlength="40" value="user@example.com" type="text" />
   </td>
-  </tr>
-  <tr>
-   <th class="subr"><?=$spamQuestion?><br>
-   </th>
-
-   <td><input name="answer" size="60" maxlength="10" type="text"></td>
-  
-  </tr>
-  <tr>
-   <th colspan="2">
-    <input name="action" value="Preview" type="submit">
-    <input name="action" value="Add Note" type="submit">
-   </th>
-
-  </tr>
- </tbody> 
+ </tr>
+ <tr>
+  <th class="subr">Your notes:</th>
+  <td><textarea name="note" rows="20" cols="60" wrap="virtual"></textarea>
+   <br />
+  </td>
+ </tr>
+ <tr>
+  <th class="subr"><?=$spamQuestion?><br/></th>
+  <td><input name="answer" size="60" maxlength="10" type="text"></td>
+ </tr>
+ <tr>
+  <th colspan="2">
+   <!-- I'll add the preview soon -->
+   <input name="action" value="Add Note" type="submit">
+  </th>
+ </tr>
+</tbody>
 </form>
-<?php if (isset($error)): ?>
-<div class="errors"><?php echo $error; ?></div>
-<?php endif; // if (isset($error)): ?>
+
 <?php response_footer(); ?>
