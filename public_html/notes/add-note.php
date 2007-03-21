a976d893a97324848e46e9923e869c67ee0821b7
David Coallier <davidc@php.net>
2007-03-21 23:24:45 +0000

    - added more up-to-date version
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@232275 c90b9560-bf6c-de11-be94-00142212c4b1

--- public_html/notes/add-note.php
+++ public_html/notes/add-note.php
@@ -4,33 +4,58 @@ $post = $_POST;
 
 unset($_POST);
 
+/**
+ * These are the keys that have to be set
+ * in order to get no errors. If someone is
+ * missing one of them, then something is 
+ * totally wrong.
+ */
 $keys = array(
-    'noteUrl' => 'Note Address', 
-    'user'    => 'Username/Email', 
-    'note'    => 'User comment/note', 
-    'answer'  => 'Captcha Answer');
+        'noteUrl' => 'Note Address', 
+        'user'    => 'Username/Email', 
+        'note'    => 'User comment/note', 
+        'answer'  => 'Captcha Answer');
 
 
 $errors = array();
 
+/**
+ * Check if the keys are set, if not
+ * then set an error..
+ */
 foreach ($keys as $key => $message) {
     if (!isset($post[$key])) {
         $errors[] = 'Error occured, missing: ' . $message;
     }
 }
 
-require_once 'notes/ManualNotes.class.php';
+if (empty($errors)) {
+    require_once 'notes/ManualNotes.class.php';
 
-$manualNote = new ManualNotes;
-/**
- * @todo Check the captcha here.
- * @todo Check akismet here aswell ?
- */
+    $manualNote = new ManualNotes;
+    /**
+     * @todo Check the captcha here.
+     * @todo Check akismet here aswell ?
+     */
 
-$added = $manualNote->addComment($post['noteUrl'], $post['user'], $post['note']);
+    $added = $manualNote->addComment($post['noteUrl'], $post['user'], $post['note']);
 
-if (PEAR::isError($added)) {
-    $errors[] = $added->getMessage();
+    if (PEAR::isError($added)) {
+        if (isset($post['noteUrl'])) {
+            /**
+             * If someone tries to access this page
+             * without a noteUrl then it's his problem
+             * to get the comment template without 
+             * a noteUrl.. this is recursivly not
+             * going to be working however this check
+             * should not have to be done because
+             * in order to get to this point.. you need
+             * to have.
+             */
+            $noteUrl = strip_tags($post['noteUrl']);
+        }
+        $errors[] = $added->getMessage() . ' please contact <a mailto="pear-webaster@lists.php.net">Webmaster</a> , Thanks';
+    }
 }
 
-require dirname(dirname(dirname(__FILE__))) . '/templates/notes/add-note.php';
+require dirname(dirname(dirname(__FILE__))) . '/templates/notes/add-note.tpl.php';
