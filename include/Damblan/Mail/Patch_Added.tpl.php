307323e0bf876b529e06162d3a5f8a11755de77e
David Coallier <davidc@php.net>
2007-03-22 20:32:06 +0000

    Added templates adjustments.
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@232355 c90b9560-bf6c-de11-be94-00142212c4b1

--- include/Damblan/Mail/Patch_Added.tpl.php
+++ include/Damblan/Mail/Patch_Added.tpl.php
@@ -5,8 +5,8 @@
      * 
      * %id%             The patch ID
      * %url%            The URL of the patch entry
+     * %packageUrl%     The package url
      * %date%           The date of the patch in human readable format
-     * %user%           The user who sent the patch
      */
 
     $tpl = array(
@@ -14,13 +14,13 @@
         'Subject' => '[Patch] Patch Added/Updated for bug %id%',
         'Body' => 'Dear maintainer,
 
-The following patch has been added/updated for bug %id%:
+The following patch has been added/updated for bug #%id%:
 
 URL:        %url%
 Date:       %date%
 
-Executor:   %user%
-
+View package at : %packageUrl%
+'
      );
 
 ?>
