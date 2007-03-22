e7ba416e64f647cb617e48541e5d6653197d49b2
David Coallier <davidc@php.net>
2007-03-22 04:51:24 +0000

    - Added the manager's first version.
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@232300 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ templates/notes/note-manage-admin.tpl.php
@@ -0,0 +1,31 @@
+<?php response_header('UserNote'); ?>
+<h1>Note Management Area</h1>
+
+<?php 
+// {{{ isset($error)
+if (strlen(trim($error)) > 0):
+?>
+<div class="errors"><?php echo $error; ?></div>
+<?php endif; ?>
+
+
+<form action="/notes/admin/trans.php" method="post">
+<input type="hidden" name="action" value="updateMass" />
+<tbody>
+ <tr>
+  <th>Approve</th>
+  <td>Comment</td>
+  <td>Name/Email</td>
+ </tr>
+<?php
+foreach ($pendingComments as $pendingComment):
+?>
+<tr>
+ <th><input type="checkbox" name="noteIds[]" value="<?php echo $pendingComment['note_id']; ?>" /></th>
+ <td><?php echo substr(htmlspecialchars($pendingComment['note_text']), 0, 200) . '...'; ?></td>
+ <td><?php echo htmlspecialchars($pendingComment['user_name']); ?></td>
+</tr>
+<?php endforeach; ?>
+</tbody>
+</form>
+<?php response_footer(); ?>
