7e6c5f001e356ba025090fb75bd8237a2a866ba4
Greg Beaver <cellog@php.net>
2007-03-22 18:58:39 +0000

    use relative path for return URL so we can test on dev box
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@232349 c90b9560-bf6c-de11-be94-00142212c4b1

--- templates/notes/add-note.tpl.php
+++ templates/notes/add-note.tpl.php
@@ -2,8 +2,8 @@
 <h1>Note addition</h1>
     <div>
     Note added, it will be reviewed soon and you will be contacted if there's any problem.
-    Go back to the previous url <a href="http://pear.php.net<?php echo htmlspecialchars($redirect) ?>" 
-                                   title="Previous Note">http://pear.php.net<?php echo htmlspecialchars($redirect) ?></a>
+    Go back to the previous url <a href="<?php echo htmlspecialchars($redirect) ?>" 
+                                   title="Previous Note">http://<?php echo PEAR_CHANNELNAME . htmlspecialchars($redirect) ?></a>
     </div>
 <?php 
 
