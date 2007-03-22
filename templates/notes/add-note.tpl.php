a917fcca66f96458c7d5be3c439e5cbfd8285032
Greg Beaver <cellog@php.net>
2007-03-22 04:42:56 +0000

    no cryptic titles needed
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@232299 c90b9560-bf6c-de11-be94-00142212c4b1

--- templates/notes/add-note.tpl.php
+++ templates/notes/add-note.tpl.php
@@ -1,4 +1,4 @@
-<?php response_header('UserNote'); ?>
+<?php response_header('User Note Added'); ?>
 <h1>Note addition</h1>
     <div>
     Note added, it will be reviewed soon and you will be contacted if there's any problem.
