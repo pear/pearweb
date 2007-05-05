486c41d159aafb82b35ecf279a2d23b8f052e481
David Coallier <davidc@php.net>
2007-05-05 00:50:40 +0000

    - New group new
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@234986 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ public_html/news/newgroup-2007.php
@@ -0,0 +1,55 @@
+<?php
+/*
+   +----------------------------------------------------------------------+
+   | PEAR Web site version 1.0                                            |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 2001-2005 The PHP Group                                |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 2.02 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available at through the world-wide-web at                           |
+   | http://www.php.net/license/2_02.txt.                                 |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Authors: Martin Jansen <mj@php.net>                                  |
+   +----------------------------------------------------------------------+
+   $Id$
+*/
+
+response_header("Own website for PECL");
+?>
+
+<h1>New PEAR Group has been elected</h1>
+
+<div style="margin-left:2em;margin-right:2em">
+<p>
+PEAR now has a new PEAR Group, the new members have been elected by 
+the PEAR developers and they have chosen:
+ <ul>
+  <li>
+    Martin Jansen
+  </li>
+  <li>
+    David Coallier
+  </li>
+  <li>
+    Arnaud Limbourg
+  </li>
+  <li>
+    Joshua Eichorn
+  </li>    
+  <li>
+    Christian Weiske
+  </li>
+  <li>
+    Helgi &THORN;ormar
+  </li>
+ </ul>
+  <br />
+You can see more informations about the PEAR Group by clicking <a href="/group/" title="PEAR Group">here</a>
+</p>
+</div>
+
+<?php response_footer(); ?>
