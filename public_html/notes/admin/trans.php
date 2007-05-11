dd4487e181aeb65dddfe3dc73978cc2fea73ac39
Mark Wiesemann <wiesemann@php.net>
2007-05-11 21:57:10 +0000

    - *now* it works as expected (and the error messages are synced)
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@235467 c90b9560-bf6c-de11-be94-00142212c4b1

--- public_html/notes/admin/trans.php
+++ public_html/notes/admin/trans.php
@@ -147,18 +147,24 @@ switch ($action) {
                 $message .= isset($_POST['approve']) ? 'approved' : 'deleted';
             }
             $_GET = $_POST;
-            if (isset($_POST['undelete'])) {
+            if (isset($_POST['approve'])) {
+                $_GET['status'] = 'approved';
+            } elseif (isset($_POST['delete'])) {
+                $_GET['status'] = 'deleted';
+            } elseif (isset($_POST['undelete'])) {
                 $_GET['status'] = 'deleted';
             }
             
             include dirname(__FILE__) . '/index.php';
             exit;
         } else {
-            $error = 'Neither delete nor approve was selected';
+            $error = 'Neither delete nor un-delete nor approve was selected';
             $_GET = $_POST;
             if (isset($_POST['approve'])) {
                 $_GET['status'] = 'approved';
             } elseif (isset($_POST['delete'])) {
+                $_GET['status'] = 'deleted';
+            } elseif (isset($_POST['undelete'])) {
                 $_GET['status'] = 'deleted';
             }
             
