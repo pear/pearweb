86845632f69f39e3b61c64d507a7b275e8a0664f
Helgi Þormar Þorbjörnsson <dufuz@php.net>
2007-06-09 22:36:43 +0000

    update changelog
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@237308 c90b9560-bf6c-de11-be94-00142212c4b1

--- package.php
+++ package.php
@@ -38,6 +38,7 @@ $a->setNotes('
  * Fix Bug #11192 Bug comments create wrong links to other bugs
    Now a bug link has to have # to be valid and thus parsed [dufuz]
  * Fix Bug #11200  Add html anchors for letters in proposal list [dufuz]
+ * Fix Bug #11194  source code input on account creation [dufuz]
 ');
 $a->resetUsesrole();
 $a->addUsesRole('web', 'Role_Web', 'pearified.com');
