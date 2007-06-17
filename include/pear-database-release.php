2c5a09479fa61f812802d9f5deff1669b049f846
Greg Beaver <cellog@php.net>
2007-06-17 17:21:06 +0000

    only lead devs can release
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@237929 c90b9560-bf6c-de11-be94-00142212c4b1

--- include/pear-database-release.php
+++ include/pear-database-release.php
@@ -210,7 +210,7 @@ class release
 
         include_once 'pear-database-user.php';
         $role = user::maintains($auth_user->handle, $package);
-        if ($role != 'lead' && $role != 'developer' && !$auth_user->isAdmin() && !$auth_user->isQA()) {
+        if ($role != 'lead' && !$auth_user->isAdmin() && !$auth_user->isQA()) {
             return PEAR::raiseError('release::validateUpload: insufficient privileges');
         }
         // (2) verify that package exists
