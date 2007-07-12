3aa49ecca38e56a419aeb783c27f2c8c3cbd4c7e
David Coallier <davidc@php.net>
2007-07-12 23:01:16 +0000

    - GoPHP5 Project linking page
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@239650 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ public_html/gophp5.php
@@ -0,0 +1,37 @@
+<?php 
+response_header('GOPHP5!', false, false);
+
+?>
+
+<h2>Joining the GoPHP5.org's movement</h2>
+<p>
+After being the leader and most use PHP applications and libraries
+for many years with over 30 Million downloads, the 
+<a href="http://pear.php.net">PEAR Project</a> is happy to announce
+that the next version of PEAR, named PEAR2, is going to aim at being
+only available for PHP5.2 and newer versions of <a href="http://php.net">PHP</a>
+</p>
+
+<p>
+PEAR2 is currently not released but under development with a new installer (Pyrus)
+coming along the way. Using the benefits of the PHP Archiver (<a href="http://pecl.php.net/phar">PHAR</a>)
+and the features that PHP5.2 offers, we can now produce better applications than
+ever using the PHP language.
+</p>
+
+<p>
+Thanks to all our users and especially our dedicated developers for making this possible. Without them, we would not be able
+to achieve and reach a goal that has been important to us for the few past years, moving to PHP5.
+</p>
+
+<h3>Links</h3>
+- <a href="http://gophp5.org" title="GoPHP5">GoPHP5</a><br />
+- <a href="http://php.net" title="PHP Project">PHP</a><br />
+- <a href="http://php.net/oop5" title="php oop">PHP5 Object Orientation manual</a><br />
+- <a href="http://pecl.php.net/phar" title="phar">Phar</a><br />
+- <a href="http://wiki.pear.php.net" title="PEAR's wiki">PEAR's wiki</a><br />
+
+
+<?php
+response_footer();
+?>
