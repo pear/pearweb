1900759cc96f4f34a8f8d3055aa6d729b0795e14
Helgi Þormar Þorbjörnsson <dufuz@php.net>
2007-09-05 23:25:01 +0000

    Properly finish Bug #10990
    # Please do not add this into the cron cycle until I get the go from Hannes, if we intent to do add it before the next release cycle
    
    
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@242224 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ cron/bug-update-php-version.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ * +----------------------------------------------------------------------+
+ * | PEAR Web site version 1.0                                            |
+ * +----------------------------------------------------------------------+
+ * | Copyright (c) 2003-2007 The PEAR Group                               |
+ * +----------------------------------------------------------------------+
+ * | This source file is subject to version 2.02 of the PHP license,      |
+ * | that is bundled with this package in the file LICENSE, and is        |
+ * | available at through the world-wide-web at                           |
+ * | http://www.php.net/license/2_02.txt.                                 |
+ * | If you did not receive a copy of the PHP license and are unable to   |
+ * | obtain it through the world-wide-web, please send a note to          |
+ * | license@php.net so we can mail you a copy immediately.               |
+ * +----------------------------------------------------------------------+
+ * | Authors: Helgi Þormar Þorbjörnsson <dufuz@php.net>                   |
+ * +----------------------------------------------------------------------+
+ *
+ * $Id$
+ */
+
+// fetch the data
+$new5 = file_get_contents('http://www.php.net/releases/?serialize=1&version=5');
+if ($new5 === false) {
+    die('There was a problem fetching the serialized array from php.net');
+}
+
+$new4 = file_get_contents('http://www.php.net/releases/?serialize=1&version=4');
+if ($new4 === false) {
+    die('There was a problem fetching the serialized array from php.net');
+}
+
+$n5 = unserialize($new5);
+$n4 = unserialize($new4);
+
+$file = '../public_html/bugs/include/php_versions.php';
+include $file;
+
+$php4 = $php = array();
+foreach ($versions as $v) {
+    if ($v[0] != '4') {
+        $php[] = $v;
+    } else {
+        $php4[] = $v;
+    }
+}
+
+$merge  = !in_array($n5['version'], $versions) ? array_merge(array($n5['version']), $php)  : $php;
+$merge4 = !in_array($n4['version'], $versions) ? array_merge(array($n4['version']), $php4) : $php4;
+$final  = array_merge($merge, $merge4);
+$data   = var_export($final, true);
+
+$output = '<?php
+' . $data;
+
+file_put_contents($file, $output);
