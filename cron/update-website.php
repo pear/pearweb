ebd448ee2544b8aa4b4b4b8d88dd07448f30ea67
Greg Beaver <cellog@php.net>
2007-09-26 02:31:04 +0000

    don't allow any pearweb packages to overwrite bin_dir files by mistake or by compromise of the pearweb release mechanism
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@242917 c90b9560-bf6c-de11-be94-00142212c4b1

--- cron/update-website.php
+++ cron/update-website.php
@@ -8,8 +8,13 @@
  * @license   http://www.php.net/license/3_01.txt  PHP License
  * @version   $Id: trackback-cleanup.php,v 1.3 2006/02/07 18:14:38 mj Exp $
  */
+require_once 'PEAR/Config.php';
 require_once 'PEAR/PackageUpdate.php';
 require_once 'PEAR/PackageUpdate/Cli.php';
+$singleton = &PEAR_Config::singleton();
+// even if someone compromises pearweb, there's no way to install anything that would overwrite
+// a critical file
+$singleton->set('bin_dir', '/dev/null');
 $pearweb = &PEAR_PackageUpdate::factory('Cli', 'pearweb', 'pear.php.net');
 if ($pearweb) {
     $pearweb->setMinimumState(PEAR_PACKAGEUPDATE_STATE_STABLE);
