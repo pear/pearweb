ceb35a814c3e36879d56f841f19a33a9e976558b
Helgi Þormar Þorbjörnsson <dufuz@php.net>
2007-10-29 21:13:35 +0000

    Throwing in the updated changelog
    # Hopefully we'll have a new release tonight
    
    
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@245172 c90b9560-bf6c-de11-be94-00142212c4b1

--- package.php
+++ package.php
@@ -37,7 +37,39 @@ $a->setReleaseVersion('1.17.0');
 $a->setReleaseStability('stable');
 $a->setAPIStability('stable');
 $a->setNotes('
- * Feature request #11985: Changed header type to rdf+xml for the map rest interface.
+ * Fix Bug #9476: schema validation error on postinstall script [dufuz]
+ * Fix Bug #11094: users table should include longitude, latitude on creation [dufuz]
+ * Fix Bug #11295: Fixed in CVS does not mark a bug as closed [dufuz]
+ * Fix Bug #11308: Bug tracker: no feedback after (un)subscribing [cellog]
+ * Fix Bug #11382: Tell registering users they need to provide &quot;Firstname Lastname&quot; [dufuz]
+ * Fix Bug #11412: Incorrect Package Statistics for Category [dufuz]
+ * Fix Bug #11421: Developers should be able to read unconfirmed bugcomments [dufuz]
+ * Fix Bug #11422: &quot;Status: Open | Feedback | All&quot; header not displayed when no bugs are
+   found [wiesemann]
+ * Fix Bug #11441: Package releases not available [cellog]
+ * Fix Bug #11456: Admin page account overview [cellog]
+ * Fix Bug #11497: RSS feed description field should contain breaks [dufuz]
+ * Fix Bug #11502: Uploading patch doesn&apos;t work [cellog]
+ * Fix Bug #11638: Link to Bug Tracker on User Note Entry Screen fails [dufuz]
+ * Fix Bug #11759: Manual: Note form loses page URL [cellog]
+ * Fix Bug #11760: Bug tracker: summary is shown twice [wiesemann]
+ * Fix Bug #11804: Forgotten Password Field for mailaddress too short [cellog]
+ * Fix Bug #12004: Roadmap lacks error handling in case of duplicate roadmap versions [dufuz]
+ * Implement Feature #3085: Link to package page from finished proposals [dufuz]
+ * Implement Feature #10800: Copy closed tickets since last release into a new roadmap [dufuz]
+ * Implement Feature #10900: link developer maps from pearweb [cweiske]
+ * Implement Feature #10943: Remove verification screen for developers [dufuz]
+ * Implement Feature #11086: tabs &amp; whitespace mixing in assorted files [dufuz]
+ * Implement Feature #11109: Restyle the warning message css [dufuz]
+ * Implement Feature #11270: Replace all image CAPTCHA with math one [dufuz]
+ * Implement Feature #11362: Get rid of captcha for pear.dev when sending mail to user via webform [dufuz]
+ * Implement Feature #11436: split go-pear in its own package [dufuz]
+ * Implement Feature #11450: When showing bugs by user handle, hide those [quipo]
+ * Implement Feature #11530: bug reports should have a panel/div explaining the colour coding [dufuz]
+ * Implement Feature #11851: Assigned bugs that are not maintained by developer made visable on
+   info page [dufuz]
+ * Implement Feature #11985: Developer map rest link should be application/rdf+xml [davidc]
+ * Implement Feature #12013: package name is shown in bug list even you select only one package [dufuz]
 ');
 $a->resetUsesrole();
 $a->addUsesRole('web', 'Role_Web', 'pearified.com');
@@ -122,4 +154,3 @@ $a->addReplacement('pearweb.php', 'pear-config', '@php-dir@', 'php_dir');
 $a->addReplacement('pearweb.php', 'package-info', '@version@', 'version');
 $a->generateContents();
 $a->writePackageFile();
-?>
