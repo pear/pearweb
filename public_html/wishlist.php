be4c66529491f892ed132a2a0b6cda15bc9b4a70
Helgi Þormar Þorbjörnsson <dufuz@php.net>
2007-12-29 00:06:01 +0000

    lets not cut lines down like that unless we're going over a real length
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@249256 c90b9560-bf6c-de11-be94-00142212c4b1

--- public_html/wishlist.php
+++ public_html/wishlist.php
@@ -25,17 +25,11 @@ if (empty($user)) {
 }
 
 PEAR::setErrorHandling(PEAR_ERROR_RETURN);
-$url = $dbh->getOne('SELECT wishlist FROM users WHERE handle = ?',
-                    array($user));
+$url = $dbh->getOne('SELECT wishlist FROM users WHERE handle = ?', array($user));
 if (empty($url) || PEAR::isError($url)) {
     header("HTTP/1.0 404 Not found");
     die("<h1>User not found</h1>\n");
 }
 
 header("Location: $url");
-
-printf("<a href=\"%s\">click here to go to %s's wishlist</a>",
-       $url,
-       $user
-       );
-?>
+printf("<a href=\"%s\">click here to go to %s's wishlist</a>", $url, $user);
