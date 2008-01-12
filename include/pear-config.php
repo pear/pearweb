945b153876cb10650250b4b3441bd0445540a39e
Helgi Þormar Þorbjörnsson <dufuz@php.net>
2008-01-12 16:14:21 +0000

    Now instead about 50 if else sentences then we have only a handful, this tho means if a person sets the channelname then they'll have to set almost all of the other Env variables
    # This is temp, we'll refactor it more, for now this is mostly to get a proper overview
    
    
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@250482 c90b9560-bf6c-de11-be94-00142212c4b1

--- include/pear-config.php
+++ include/pear-config.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Establishes constants used throughout PEAR's website.
  *
@@ -18,250 +17,117 @@
  */
 
 define ('PEARWEB_TEMPLATEDIR', dirname(dirname(__FILE__)) . '/templates/');
-if (isset($_ENV['PEAR_CHANNELNAME'])) {
-    /**
-     * @ignore
-     */
-    define('PEAR_CHANNELNAME', $_ENV['PEAR_CHANNELNAME']);
+if (isset($_ENV['PEAR_CHANNELNAME']) && $_ENV['PEAR_CHANNELNAME'] != 'pear.php.net') {
+    define('PEAR_CHANNELNAME',           $_ENV['PEAR_CHANNELNAME']);
+    define('PEAR_WEBMASTER_EMAIL',       $_ENV['PEAR_WEBMASTER_EMAIL']);
+    define('PEAR_QA_EMAIL',              $_ENV['PEAR_QA_EMAIL']);
+    define('PEAR_DOC_EMAIL',             $_ENV['PEAR_DOC_EMAIL']);
+    define('PEAR_ANNOUNCE_EMAIL',        $_ENV['PEAR_ANNOUNCE_EMAIL']);
+    define('PEAR_GENERAL_EMAIL',         $_ENV['PEAR_GENERAL_EMAIL']);
+    define('PEAR_CHANNEL_SUMMARY',       $_ENV['PEAR_CHANNEL_SUMMARY']);
+    define('PEAR_AUTH_REALM',            $_ENV['PEAR_AUTH_REALM']);
+    define('PEAR_TARBALL_DIR',           $_ENV['PEAR_TARBALL_DIR']);
+    define('TRACKBACK_AKISMET_KEY_FILE', $_ENV['TRACKBACK_AKISMET_KEY_FILE']);
+    define('PEAR_CHM_DIR',               $_ENV['PEAR_CHM_DIR']);
+    define('PEAR_APIDOC_DIR',            $_ENV['PEAR_APIDOC_DIR']);
+    define('PEAR_PATCHES',               $_ENV['PEAR_PATCHES']);
+    define('PEAR_CVS',                   $_ENV['PEAR_CVS']);
+    define('PROPOSAL_MAIL_PEAR_DEV',     $_ENV['PROPOSAL_MAIL_PEAR_DEV']);
+    define('PROPOSAL_MAIL_PEAR_GROUP',   $_ENV['PROPOSAL_MAIL_PEAR_GROUP']);
+    define('PROPOSAL_MAIL_FROM',         $_ENV['PROPOSAL_MAIL_FROM']);
+    define('PEAR_WIKI_URL',              $_ENV['PEAR_WIKI_URL']);
+    define('PEAR_WIKI_DSN',              $_ENV['PEAR_WIKI_DSN']);
 } else {
-    /**
-     * The channel server name that is used for all info
-     */
+     // The channel server name that is used for all info
     define('PEAR_CHANNELNAME', 'pear.php.net');
-}
-if (isset($_ENV['PEAR_WEBMASTER_EMAIL'])) {
-    /**
-     * @ignore
-     */
-    define('PEAR_WEBMASTER_EMAIL', $_ENV['PEAR_WEBMASTER_EMAIL']);
-} else {
-    /**
-     * The channel webmaster email
-     */
+    // The channel webmaster email
     define('PEAR_WEBMASTER_EMAIL', 'pear-webmaster@lists.php.net');
-}
-if (isset($_ENV['PEAR_QA_EMAIL'])) {
-    /**
-     * @ignore
-     */
-    define('PEAR_QA_EMAIL', $_ENV['PEAR_QA_EMAIL']);
-} else {
-    /**
-     * The channel QA email
-     */
+    // The channel QA email
     define('PEAR_QA_EMAIL', 'pear-qa@lists.php.net');
-}
-if (isset($_ENV['PEAR_DOC_EMAIL'])) {
-    /**
-     * @ignore
-     */
-    define('PEAR_DOC_EMAIL', $_ENV['PEAR_DOC_EMAIL']);
-} else {
-    /**
-     * The channel webmaster email
-     */
+    // The channel webmaster email
     define('PEAR_DOC_EMAIL', 'pear-doc@lists.php.net');
-}
-if (isset($_ENV['PEAR_ANNOUNCE_EMAIL'])) {
-    /**
-     * @ignore
-     */
-    define('PEAR_ANNOUNCE_EMAIL', $_ENV['PEAR_ANNOUNCE_EMAIL']);
-} else {
-    /**
-     * The channel webmaster email
-     */
+    // The channel webmaster email
     define('PEAR_ANNOUNCE_EMAIL', 'pear-dev@lists.php.net');
-}
-if (isset($_ENV['PEAR_GENERAL_EMAIL'])) {
-    /**
-     * @ignore
-     */
-    define('PEAR_GENERAL_EMAIL', $_ENV['PEAR_GENERAL_EMAIL']);
-} else {
-    /**
-     * The channel webmaster email
-     */
+    // The channel webmaster email
     define('PEAR_GENERAL_EMAIL', 'pear-general@lists.php.net');
-}
-if (isset($_ENV['PEAR_CHANNEL_SUMMARY'])) {
-    /**
-     * @ignore
-     */
-    define('PEAR_CHANNEL_SUMMARY', $_ENV['PEAR_CHANNEL_SUMMARY']);
-} else {
-    /**
-     * The channel webmaster email
-     */
+    // The channel webmaster email
     define('PEAR_CHANNEL_SUMMARY', 'PEAR PHP Extension and Application Repository');
-}
-if (isset($_ENV['PEAR_TMPDIR'])) {
-    /**
-     * @ignore
-     */
-    define('PEAR_TMPDIR', $_ENV['PEAR_TMPDIR']);
-    /**
-     * @ignore
-     */
-    define('PEAR_CVS_TMPDIR', $_ENV['PEAR_TMPDIR'].'/cvs');
-    /**
-     * @ignore
-     */
-    define('PEAR_UPLOAD_TMPDIR', $_ENV['PEAR_TMPDIR'].'/uploads');
-    /**
-     * @ignore
-     */
-    define('PEAR_PATCHTRACKER_TMPDIR', $_ENV['PEAR_TMPDIR'].'/patch');
-} else {
-    /**
-     * Where pearweb's temporary files should be stored
-     */
-    define('PEAR_TMPDIR', '/var/tmp/pear');
-    /**
-     * Where pearweb's temporary CVS files should be stored
-     */
-    define('PEAR_CVS_TMPDIR', '/var/tmp/pear/cvs');
-    /**
-     * Where pearweb's temporary uploads should be stored
-     */
-    define('PEAR_UPLOAD_TMPDIR', '/var/tmp/pear/uploads');
-    /**
-     * Where pearweb's temporary uploads should be stored
-     */
-    define('PEAR_PATCHTRACKER_TMPDIR', '/var/tmp/pear/patch');
-}
-
-if (isset($_ENV['PEAR_DATABASE_DSN'])) {
-    /**
-     * @ignore
-     */
-    define('PEAR_DATABASE_DSN', $_ENV['PEAR_DATABASE_DSN']);
-} else {
-    if (function_exists('mysql_connect')) {
-        /**
-         * The PEAR::DB DSN connection string
-         *
-         * To override default, set the value in $_ENV['PEAR_DATABASE_DSN']
-         * before this file is included.
-         */
-        define('PEAR_DATABASE_DSN', 'mysql://pear:pear@localhost/pear');
-    } elseif (function_exists('mysqli_connect')) {
-        /** @ignore */
-        define('PEAR_DATABASE_DSN', 'mysqli://pear:pear@localhost/pear');
-    }
-}
-
-if (isset($_ENV['PEAR_AUTH_REALM'])) {
-    define('PEAR_AUTH_REALM', $_ENV['PEAR_AUTH_REALM']);
-} else {
     /**
      * The authorization realm
      *
      * Gets output to a "WWW-authenticate" header upon authentication failures
-     *
-     * To override default, set the value in $_ENV['PEAR_AUTH_REALM']
-     * before this file is included.
      */
     define('PEAR_AUTH_REALM', 'PEAR');
-}
-
-if (isset($_ENV['PEAR_TARBALL_DIR'])) {
-    /**
-     * @ignore
-     */
-    define('PEAR_TARBALL_DIR', $_ENV['PEAR_TARBALL_DIR']);
-} else {
-    /**
-     * Where package tarballs can be found
-     *
-     * To override default, set the value in $_ENV['PEAR_TARBALL_DIR']
-     * before this file is included.
-     */
+    // Where package tarballs can be found
     define('PEAR_TARBALL_DIR', '/var/lib/pear');
-}
-
-if (isset($_ENV['PEAR_CHM_DIR'])) {
-    /**
-     * @ignore
-     */
-    define('PEAR_CHM_DIR', $_ENV['PEAR_CHM_DIR']);
-} else {
-    /**
-     * Where the CHM builds of the manual are located
-     *
-     * To override default, set the value in $_ENV['PEAR_CHM_DIR']
-     * before this file is included.
-     */
+    // Key file returning key for Akismet.com spam chek.
+    define('TRACKBACK_AKISMET_KEY_FILE', '/usr/local/www/akismet.key');
+    // Where the CHM builds of the manual are located
     define('PEAR_CHM_DIR', '/var/lib/pear/chm/');
-}
-
-if (isset($_ENV['PEAR_APIDOC_DIR'])) {
-    /**
-     * @ignore
-     */
-    define('PEAR_APIDOC_DIR', $_ENV['PEAR_APIDOC_DIR']);
-} else {
     /**
      * Location of the documentation automatically generated for each
      * package by phpDocumentor
-     *
-     * To override default, set the value in $_ENV['PEAR_APIDOC_DIR']
-     * before this file is included.
      */
     define('PEAR_APIDOC_DIR', '/var/lib/pear/apidoc/');
-}
-
-if (isset($_ENV['PEAR_PATCHES'])) {
-    /**
-     * @ignore
-     */
-    define('PEAR_PATCHES', $_ENV['PEAR_PATCHES']);
-} else {
-    /**
-     * Where patches can be found
-     *
-     * To override default, set the value in $_ENV['PEAR_PATCHES']
-     * before this file is included.
-     */
+    // Where patches can be found
     define('PEAR_PATCHES', '/var/lib/pear/patches/');
-}
-
-if (isset($_ENV['PEAR_CVS'])) {
-    /**
-     * @ignore
-     */
-    define('PEAR_CVS', $_ENV['PEAR_CVS']);
-} else {
-    /**
-     * Where proposed patches reside
-     *
-     * To override default, set the value in $_ENV['PEAR_CVS']
-     * before this file is included.
-     */
+    // Where proposed patches reside
     define('PEAR_CVS', '/var/lib/pear/patches/cvs/');
+    /**
+     * PEPr: the address of the PEAR Developer email list
+     *
+     * Notices of changes will be sent to this address.
+     */
+    define('PROPOSAL_MAIL_PEAR_DEV', 'PEAR developer mailinglist <pear-dev@lists.php.net>');
+    /**
+     * PEPr: the address of the PEAR Group email list
+     *
+     * Notices of some changes get sent to this address.
+     */
+    define('PROPOSAL_MAIL_PEAR_GROUP', 'PEAR group <pear-group@php.net>');
+    // PEPr: the email address used as the From header
+    define('PROPOSAL_MAIL_FROM', 'PEPr <bounce-no-user@php.net>');
+    // The full URL to the wiki, no / at the end
+    define('PEAR_WIKI_URL', 'http://wiki.pear.php.net');
+    // The DSN for the wiki database
+    define('PEAR_WIKI_DSN', 'mysql://pear:pear@localhost/pearwiki');
 }
 
-/**
- * A preg regular expression for validating user names
- */
+if (isset($_ENV['PEAR_TMPDIR'])) {
+    define('PEAR_TMPDIR',              $_ENV['PEAR_TMPDIR']);
+    define('PEAR_CVS_TMPDIR',          PEAR_TMPDIR . '/cvs');
+    define('PEAR_UPLOAD_TMPDIR',       PEAR_TMPDIR . '/uploads');
+    define('PEAR_PATCHTRACKER_TMPDIR', PEAR_TMPDIR . '/patch');
+} else {
+    // Where pearweb's temporary files should be stored
+    define('PEAR_TMPDIR', '/var/tmp/pear');
+    // Where pearweb's temporary CVS files should be stored
+    define('PEAR_CVS_TMPDIR', '/var/tmp/pear/cvs');
+    // Where pearweb's temporary uploads should be stored
+    define('PEAR_UPLOAD_TMPDIR', '/var/tmp/pear/uploads');
+    // Where pearweb's temporary uploads should be stored
+    define('PEAR_PATCHTRACKER_TMPDIR', '/var/tmp/pear/patch');
+}
+
+if (isset($_ENV['PEAR_DATABASE_DSN'])) {
+    define('PEAR_DATABASE_DSN', $_ENV['PEAR_DATABASE_DSN']);
+} else {
+    if (function_exists('mysql_connect')) {
+        // The PEAR::DB DSN connection string
+        define('PEAR_DATABASE_DSN', 'mysql://pear:pear@localhost/pear');
+    } elseif (function_exists('mysqli_connect')) {
+        define('PEAR_DATABASE_DSN', 'mysqli://pear:pear@localhost/pear');
+    }
+}
+
+// A preg regular expression for validating user names
 define('PEAR_COMMON_USER_NAME_REGEX', '/^[a-z][a-z0-9]+\z/i');
-
-/**
- * How long the cache should last
- */
+// How long the cache should last
 define('CACHE_LIFETIME', 3600);
-
-/**
- * Where the cached output is stored
- */
+// Where the cached output is stored
 define('DAMBLAN_RSS_CACHE_DIR', PEAR_TMPDIR . '/rss_cache');
-
-/**
- *
- */
+// RSS cache time
 define('DAMBLAN_RSS_CACHE_TIME', 1800);
-
-
 
 /**
  * PEPr: how long a proposal must be in the "proposal" phase before
@@ -269,130 +135,19 @@ define('DAMBLAN_RSS_CACHE_TIME', 1800);
  */
 define('PROPOSAL_STATUS_PROPOSAL_TIMELINE', (60 * 60 * 24 * 7)); // 1 week
 
-/**
- * PEPr: how long the "Call for Votes" lasts
- */
+// PEPr: how long the "Call for Votes" lasts
 define('PROPOSAL_STATUS_VOTE_TIMELINE', (60 * 60 * 24 * 7)); // 1 week
 
-if (isset($_ENV['PROPOSAL_MAIL_PEAR_DEV'])) {
-    /**
-     * @ignore
-     */
-    define('PROPOSAL_MAIL_PEAR_DEV', $_ENV['PROPOSAL_MAIL_PEAR_DEV']);
-} else {
-    /**
-     * PEPr: the address of the PEAR Developer email list
-     *
-     * Notices of changes will be sent to this address.
-     *
-     * To override default, set the value in $_ENV['PROPOSAL_MAIL_PEAR_DEV']
-     * before this file is included.
-     */
-    define('PROPOSAL_MAIL_PEAR_DEV', 'PEAR developer mailinglist <pear-dev@lists.php.net>');
-}
-
-if (isset($_ENV['PROPOSAL_MAIL_PEAR_GROUP'])) {
-    /**
-     * @ignore
-     */
-    define('PROPOSAL_MAIL_PEAR_GROUP', $_ENV['PROPOSAL_MAIL_PEAR_GROUP']);
-} else {
-    /**
-     * PEPr: the address of the PEAR Group email list
-     *
-     * Notices of some changes get sent to this address.
-     *
-     * To override default, set the value in $_ENV['PROPOSAL_MAIL_PEAR_GROUP']
-     * before this file is included.
-     */
-    define('PROPOSAL_MAIL_PEAR_GROUP', 'PEAR group <pear-group@php.net>');
-}
-
-if (isset($_ENV['PROPOSAL_MAIL_FROM'])) {
-    /**
-     * @ignore
-     */
-    define('PROPOSAL_MAIL_FROM', $_ENV['PROPOSAL_MAIL_FROM']);
-} else {
-    /**
-     * PEPr: the email address used as the From header
-     *
-     * To override default, set the value in $_ENV['PROPOSAL_MAIL_FROM']
-     * before this file is included.
-     */
-    define('PROPOSAL_MAIL_FROM', 'PEPr <bounce-no-user@php.net>');
-}
-
-if (isset($_ENV['PEAR_WIKI_URL'])) {
-    /**
-     * @ignore
-     */
-    define('PEAR_WIKI_URL', $_ENV['PEAR_WIKI_URL']);
-} else {
-    /**
-     * The full URL to the wiki, no / at the end
-     *
-     * To override default, set the value in $_ENV['PEAR_WIKI_URL']
-     * before this file is included.
-     */
-    define('PEAR_WIKI_URL', 'http://wiki.pear.php.net');
-}
-
-if (isset($_ENV['PEAR_WIKI_DSN'])) {
-    /**
-     * @ignore
-     */
-    define('PEAR_WIKI_DSN', $_ENV['PEAR_WIKI_DSN']);
-} else {
-    /**
-     * The DSN for the wiki database
-     *
-     * To override default, set the value in $_ENV['PEAR_WIKI_DSN']
-     * before this file is included.
-     */
-    define('PEAR_WIKI_DSN', 'mysql://pear:pear@localhost/pearwiki');
-}
-
-/**
- * PEPr: the string prepended to the subject lines of emails
- */
+// PEPr: the string prepended to the subject lines of emails
 define('PROPOSAL_EMAIL_PREFIX', '[PEPr]');
 
-/**
- * PEPr: the string put on the end of each email
- */
+// PEPr: the string put on the end of each email
 define('PROPOSAL_EMAIL_POSTFIX', "\n\n-- \nSent by PEPr, the automatic proposal system at http://" . PEAR_CHANNELNAME);
-
 define('PROPOSAL_OVERVIEW_FINISHED', 10);
 
-/**
- * Number of trackbacks from 1 IP allowed within given timespan.
- */
+// Number of trackbacks from 1 IP allowed within given timespan.
 define('TRACKBACK_REPOST_COUNT', 3);
-/**
- * Timespan for above defined repost count. 3600 == 30 mins
- */
+// Timespan for above defined repost count. 3600 == 30 mins
 define('TRACKBACK_REPOST_TIMESPAN', 3600);
-/**
- * Auto purging time for trackbacks (14 days).
- */
+// Auto purging time for trackbacks (14 days).
 define('TRACKBACK_PURGE_TIME', 14 * 27 * 60 * 60);
-/**
- * Key file returning key for Akismet.com spam chek.
- */
-if (isset($_ENV['TRACKBACK_AKISMET_KEY_FILE'])) {
-    /**
-     * @ignore
-     */
-    define('TRACKBACK_AKISMET_KEY_FILE', $_ENV['TRACKBACK_AKISMET_KEY_FILE']);
-} else {
-    /**
-     * The full URL to the wiki, no / at the end
-     *
-     * To override default, set the value in $_ENV['PEAR_WIKI_URL']
-     * before this file is included.
-     */
-    define('TRACKBACK_AKISMET_KEY_FILE', '/usr/local/www/akismet.key');
-}
-
-?>
