c00c600907a514582d46a47f67cf539e52f6a187
Helgi Þormar Þorbjörnsson <dufuz@php.net>
2008-01-26 18:30:35 +0000

    Request #12563 	QA, detection of closed bugs that haven't made it into a release in X months
    # With some good support from Chuck B, (ashnazg) when I was making that query, thanks!
    # Note: this hasn't been linked anywhere, will do later on
    
    
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@251380 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ public_html/qa/packages_closed_reports_no_release.php
@@ -0,0 +1,106 @@
+<?php
+/*
+   +----------------------------------------------------------------------+
+   | PEAR Web site version 1.0                                            |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 2001-2008 The PHP Group                                |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available at through the world-wide-web at                           |
+   | http://www.php.net/license/3_01.txt.                                 |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Authors: Helgi Þormar Þorbjörnsson <dufuz@php.net>                   |
+   +----------------------------------------------------------------------+
+   $Id$
+*/
+
+/*
+ * This page give you a list of all packages with closed bug report
+ * but hasn't had a release in X amount of time
+ */
+include_once 'HTML/Table.php';
+
+// Sortable tables http://www.kryogenix.org/code/browser/sorttable/
+$extra_header = '<script src="/javascript/sorttable.js"></script>';
+
+response_header('Quality Assurance Initiative - Packages with closed reports but no release in 3 months',
+    false, $extra_header);
+
+// Just so we don't fetch bugs for packages that recently had a releases
+$min_release_date = strtotime('-6 Months');
+
+$sql = "SELECT
+    packages.package_type,
+    packages.name,
+    bugdb.ts2,
+    bugdb.id AS bug_id,
+    UNIX_TIMESTAMP(r.releasedate) as releasedate
+FROM
+    packages
+    JOIN bugdb ON packages.name = bugdb.package_name AND bugdb.status = 'Closed'
+    LEFT JOIN (
+        SELECT package, MAX(releasedate) as releasedate FROM releases GROUP BY package
+    ) as r ON packages.id = r.package
+WHERE
+    UNIX_TIMESTAMP(r.releasedate) < UNIX_TIMESTAMP(bugdb.ts2)
+  AND
+    UNIX_TIMESTAMP(r.releasedate) < $min_release_date
+GROUP BY
+    packages.id, packages.name, bugdb.package_name, bugdb.id, r.package
+ORDER BY
+    r.releasedate";
+
+$res        = $dbh->getAll($sql, null, DB_FETCHMODE_ASSOC);
+$total_rows = $dbh->getOne('SELECT FOUND_ROWS()');
+
+echo 'Checks <a href="#pear">PEAR</a> and <a href="#pecl">PECL</a><br />';
+echo 'Found ' . $total_rows . ' reports that have been closed but their package has not had a release in 6 months<br /><br />';
+
+foreach ($res as $data) {
+    $bugs[$data['package_type']][$data['name']]['bug_id'][]     = $data['bug_id'];
+    $bugs[$data['package_type']][$data['name']]['last_release'] = $data['releasedate'];
+}
+
+// PEAR
+$table = new HTML_Table(array('class' => 'sortable'));
+$table->setHeaderContents(0, 0, 'Package');
+$table->setHeaderContents(0, 1, '# bugs');
+$table->setHeaderContents(0, 2, 'Last Release Date');
+
+echo '<h2 id="pear">PEAR</h2>';
+$row = 1;
+foreach ($bugs['pear'] as $name => $qa) {
+    $table->addRow(array(
+        make_link('/package/' . $name . '/', $name),
+        make_link('/bugs/search.php?cmd=display&package_name[]=' . $name . '&status=CRSLR', count($qa['bug_id'])),
+        make_utc_date($qa['last_release'])
+    ));
+    $table->setCellAttributes($row, 1, 'style="text-align: center;"');
+    $row++;
+}
+echo $table->toHTML();
+
+// PECL
+$table = new HTML_Table(array('class' => 'sortable'));
+$table->setHeaderContents(0, 0, 'Package');
+$table->setHeaderContents(0, 1, '# bugs');
+$table->setHeaderContents(0, 2, 'Last Release Date');
+
+echo '<h2 id="pecl">PECL</h2>';
+$row = 1;
+foreach ($bugs['pecl'] as $name => $qa) {
+    $table->addRow(array(
+        make_link('/package/' . $name . '/', $name),
+        count($qa['bug_id']),
+        make_utc_date($qa['last_release'])
+    ));
+    $table->setCellAttributes($row, 1, 'style="text-align: center;"');
+    $row++;
+}
+echo $table->toHTML();
+
+response_footer();
