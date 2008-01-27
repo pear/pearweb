d73e429870bf495e232bab2028c3c94e02ee59bc
Helgi Þormar Þorbjörnsson <dufuz@php.net>
2008-01-27 20:48:07 +0000

    Move the election templates into their own dir
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@251469 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ templates/election/new-step1.tpl.php
@@ -0,0 +1,168 @@
+<?php
+response_header('Election :: ' . ucfirst($new));
+?>
+<h1><?php echo ucfirst($new) ?> Election [Step 1]</h1>
+<?php
+if ($error) {
+    foreach ($error as $err) {
+        echo '<div class="errors">' . htmlspecialchars($err) . '</div>';
+    }
+}
+?>
+<form name="newelection" action="/election/<?php echo $new ?>.php" method="post">
+<input type="hidden" name="step" value="2" />
+<?php if ($new == 'edit'): ?>
+<input type="hidden" name="election_id" value="<?php echo $election_id ?>" />
+<?php endif; // if ($new == 'edit'): ?>
+ <table>
+  <tr>
+   <th class="form-label_left" colspan="2">(All entries are required)</th>
+  </tr>
+  <tr>
+   <th class="form-label_left">Eligible Voters</th>
+   <td class="form-input">
+    <select name="eligiblevoters">
+    <?php
+     if ($info['eligiblevoters'] == '1') {
+         $ev1 = ' selected="true"';
+         $ev2 = '';
+     } else {
+         $ev2 = ' selected="true"';
+         $ev1 = '';
+     }
+    ?>
+     <option value="1"<?php echo $ev1 ?>>PEAR Developers</option>
+     <option value="2"<?php echo $ev2 ?>>General PHP Public</option>
+    </select>
+   </td>
+  </tr>
+  <tr>
+   <th class="form-label_left">Election Purpose (summary)</th>
+   <td class="form-input">
+    <input type="text" name="purpose" size="100" maxlength="100" value="<?php
+        echo htmlspecialchars($info['purpose']) ?>" />
+   </td>
+  </tr>
+  <tr>
+   <th class="form-label_left">Number of items to choose from (items created in step 2)</th>
+   <td class="form-input">
+    <select name="choices">
+<?php
+    for ($i = 2; $i <= 20; $i++) {
+        if ($info['choices'] == $i) {
+            $sel = ' selected="selected"';
+        } else {
+            $sel = '';
+        }
+        echo '    <option' . $sel . '>' . $i . '</option>' . "\n";
+    } ?>
+    </select>
+   </td>
+  </tr>
+  <tr>
+   <th class="form-label_left">Election detail (wiki markup)</th>
+   <td class="form-input">
+    <textarea name="detail" rows="20" cols="75"><?php echo $info['detail'] ?></textarea>
+   </td>
+  </tr>
+  <tr>
+   <th class="form-label_left">Election start date <?php if ($new == 'new'): ?>
+   (must be at least 30 days from now)
+   <?php endif; // if ($new == 'new') ?>
+   </th>
+   <td class="form-input">
+    <select name="year">
+    <?php
+    foreach ($years as $year) {
+        if ($info['year'] == $year) {
+            $sel = ' selected="selected"';
+        } else {
+            $sel = '';
+        }
+        echo '    <option' . $sel . '>' . $year . '</option>' . "\n";
+    } ?>
+    </select>-<select name="month">
+    <?php
+    for ($i = 1; $i <= 12; $i++) {
+        if ($info['month'] == $i) {
+            $sel = ' selected="selected"';
+        } else {
+            $sel = '';
+        }
+        echo '    <option' . $sel . '>' . $i . '</option>' . "\n";
+    } ?>
+    </select>-<select name="day">
+    <?php
+    for ($i = 1; $i <= 31; $i++) {
+        if ($info['day'] == $i) {
+            $sel = ' selected="selected"';
+        } else {
+            $sel = '';
+        }
+        echo '    <option' . $sel . '>' . $i . '</option>' . "\n";
+    } ?>
+    </select>
+   </td>
+  </tr>
+  <tr>
+   <th class="form-label_left">Length of election in days</th>
+   <td class="form-input">
+    <select name="length">
+    <?php
+    for ($i = 1; $i <= 14; $i++) {
+        if ($info['length'] == $i) {
+            $sel = ' selected="selected"';
+        } else {
+            $sel = '';
+        }
+        echo '    <option' . $sel . '>' . $i . '</option>' . "\n";
+    } ?>
+    </select>
+   </td>
+  </tr>
+  <tr>
+   <th class="form-label_left" colspan="2">
+    The next two sections relate to the number of items
+    that a single voter can choose.  For most elections, voters
+    should choose only one (minimum=maximum=1).  For some
+    elections, like QA group, the voter should choose up to 5
+    developers to populate the QA group (minimum=1,maximum=5), and
+    so on.
+   </th>
+  </tr>
+  <tr>
+   <th class="form-label_left">Minimum votes needed</th>
+   <td class="form-input">
+    <select name="minimum">
+    <?php
+    for ($i = 1; $i <= 19; $i++) {
+        if ($info['minimum'] == $i) {
+            $sel = ' selected="selected"';
+        } else {
+            $sel = '';
+        }
+        echo '    <option' . $sel . '>' . $i . '</option>' . "\n";
+    } ?>
+    </select>
+   </td>
+  </tr>
+  <tr>
+   <th class="form-label_left">Maximum votes allowed</th>
+   <td class="form-input">
+    <select name="maximum">
+    <?php
+    for ($i = 1; $i <= 19; $i++) {
+        if ($info['maximum'] == $i) {
+            $sel = ' selected="selected"';
+        } else {
+            $sel = '';
+        }
+        echo '    <option' . $sel . '>' . $i . '</option>' . "\n";
+    } ?>
+    </select>
+   </td>
+  </tr>
+ </table>
+ <input type="submit" name="newelection" value="Save Election" />
+</form>
+<?php response_footer();
