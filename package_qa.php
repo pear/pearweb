57e84dd2470067ed33c1fc48f163fe7bd0e92223
Helgi Þormar Þorbjörnsson <dufuz@php.net>
2008-03-08 19:42:00 +0000

    move qa/index.php into pearweb_index for easy editing for peclweb and rest goes into a new package called pearweb_qa
    # pearweb_qa only has those that contributed scripts in there as lead/developers
    
    
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@254622 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ package_qa.php
@@ -0,0 +1,38 @@
+<?php
+require_once 'PEAR/PackageFileManager2.php';
+PEAR::setErrorHandling(PEAR_ERROR_DIE);
+$a = PEAR_PackageFileManager2::importOptions(
+    dirname(__FILE__) . '/package-qa.xml',
+    array(
+        'packagefile' => 'package-qa.xml',
+        'baseinstalldir' => '/',
+        'filelistgenerator' => 'cvs',
+        'roles' => array('*' => 'www'),
+        'simpleoutput' => true,
+        'include' => array(
+            'public_html/qa/',
+        ),
+        'ignore' => array(
+            'public_html/qa/index.php'
+        ),
+    ));
+$a->setReleaseVersion('1.0.0');
+$a->setReleaseStability('stable');
+$a->setAPIStability('stable');
+$a->setNotes('
+- split qa script code from pearweb [dufuz]
+');
+$a->resetUsesrole();
+$a->clearDeps();
+$a->setPhpDep('5.2.3');
+$a->setPearInstallerDep('1.7.1');
+$a->addPackageDepWithChannel('required', 'PEAR', 'pear.php.net', '1.7.1');
+$a->addPackageDepWithChannel('required', 'pearweb', 'pear.php.net', '1.18.0');
+$a->generateContents();
+$a->writePackageFile();
+
+if (isset($_SERVER['argv']) && @$_SERVER['argv'][1] == 'make') {
+    $a->writePackageFile();
+} else {
+    $a->debugPackageFile();
+}
