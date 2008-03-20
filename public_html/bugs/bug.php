2f963cd2dca790863c418c6dd67123acdf23d7c1
Helgi Þormar Þorbjörnsson <dufuz@php.net>
2008-03-20 22:44:56 +0000

    - fixing the delete url to only hide first - now delete_comment can only delete comments that have been hidden first
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@255634 c90b9560-bf6c-de11-be94-00142212c4b1

--- public_html/bugs/bug.php
+++ public_html/bugs/bug.php
@@ -1280,7 +1280,7 @@ function output_note($com_id, $ts, $email, $comment, $showemail = 1, $handle = n
         echo '(' . htmlspecialchars($comment_name) . ')';
     }
     if ($edit === 1 && $com_id !== 0 && auth_check('pear.dev')) {
-        echo "&nbsp<a href=\"".htmlspecialchars($_SERVER['PHP_SELF'])."?id=$id&amp;edit=1&amp;delete_comment=$com_id\">[delete]</a>\n";
+        echo "&nbsp<a href=\"".htmlspecialchars($_SERVER['PHP_SELF'])."?id=$id&amp;edit=1&amp;hide_comment=$com_id\">[delete]</a>\n";
     }
     echo '<pre class="note">'. "\n";
 
@@ -1302,21 +1302,21 @@ function output_note($com_id, $ts, $email, $comment, $showemail = 1, $handle = n
 function hide_comment($id, $com_id)
 {
     global $dbh;
-    $query ='UPDATE bugdb_comments SET active = 0 WHERE bug='.(int)$id.' AND id='.(int)$com_id;
+    $query ='UPDATE bugdb_comments SET active = 0 WHERE bug = '.(int)$id.' AND id = '.(int)$com_id;
     $res =& $dbh->query($query);
 }
 
 function show_comment($id, $com_id)
 {
     global $dbh;
-    $query ='UPDATE bugdb_comments SET active = 1 WHERE bug='.(int)$id.' AND id='.(int)$com_id;
+    $query ='UPDATE bugdb_comments SET active = 1 WHERE bug = '.(int)$id.' AND id = '.(int)$com_id;
     $res =& $dbh->query($query);
 }
 
 function delete_comment($id, $com_id)
 {
     global $dbh;
-    $query = 'DELETE FROM bugdb_comments WHERE bug='.(int)$id.' AND id='.(int)$com_id;
+    $query = 'DELETE FROM bugdb_comments WHERE active = 0 bug = '.(int)$id.' AND id = '.(int)$com_id;
     $res =& $dbh->query($query);
 }
 
