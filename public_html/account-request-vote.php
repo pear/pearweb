85ff27148d9bb3cb4d27279b3be6a3d117e9f8ed
Helgi Þormar Þorbjörnsson <dufuz@php.net>
2008-05-06 20:18:32 +0000

    Add Services_HoneyPot to prevent spammers and unwanted people from requesting an account
    Work in progress
    
    # Adding to other account pages later on
    
    
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@259186 c90b9560-bf6c-de11-be94-00142212c4b1

--- public_html/account-request-vote.php
+++ public_html/account-request-vote.php
@@ -22,12 +22,12 @@ require_once 'HTML/QuickForm.php';
 require_once 'Damblan/Mailer.php';
 require_once 'election/pear-election-accountrequest.php';
 require_once 'Text/CAPTCHA/Numeral.php';
+require_once 'Services/ProjectHoneyPot.php';
 
 $numeralCaptcha = new Text_CAPTCHA_Numeral();
 session_start();
 
 $display_form = true;
-$width        = 60;
 $errors       = array();
 $jumpto       = 'handle';
 
@@ -109,6 +109,30 @@ do {
     }
 } while (0);
 
+try {
+    // Uncomment this if you need to test on Windows
+    $resolver = null;
+    //$resolver = new Net_DNS_Resolver;
+    //$resolver->nameservers = array('66.114.197.251');
+
+    $sphp = Services_ProjectHoneyPot::factory(HONEYPOT_API_KEY, $resolver);
+    $sphp->setResponseFormat('object');
+    $ip = $_SERVER['REMOTE_ADDR'];
+    // Uncomment for testing or get one from http://www.projecthoneypot.org/top_harvesters.php
+    // $ip = '209.85.138.136';
+    $status = $sphp->query($ip);
+} catch (Services_ProjectHoneyPot_Exception $e) {
+   report_error($e);
+   $display_form = false;
+}
+
+if ($status && $status->suspicious) {
+    $errors = 'We can not allow you to continue since your IP has been marked suspecious by the
+               http://projecthoneypot.org/, if that was done in error then please contact ' .
+               PEAR_DEV_EMAIL . ' as well as the projecthoneypot people to resolve the issue.';
+    report_error($errors);
+    $display_form = false;
+}
 
 if ($display_form) {
 $mailto = '<a href="mailto:' . PEAR_DEV_EMAIL . '">PEAR developers mailing list</a>';
