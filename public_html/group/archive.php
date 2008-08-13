f83cad312d1a663513bd679d915f220a77eed232
Helgi Þormar Þorbjörnsson <dufuz@php.net>
2008-08-13 09:05:14 +0000

    Update the group, add a archive page to hold older groups to historic reference.
    # this change has already gone live on the server
    
    
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@264741 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ public_html/group/archive.php
@@ -0,0 +1,56 @@
+<?php
+/*
+   +----------------------------------------------------------------------+
+   | PEAR Web site version 1.0                                            |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 2008 The PEAR Group                                    |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available at through the world-wide-web at                           |
+   | http://www.php.net/license/3_01.txt.                                 |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Authors:                                                             |
+   +----------------------------------------------------------------------+
+   $Id$
+*/
+response_header('The PEAR Group - Archive');
+?>
+
+<h1>The PEAR Group - Archive</h1>
+
+<p>This is an archive of older groups that have governed PEAR (names are in no particular order)</p>
+
+<h2>Group of May 1st, 2007 until May 1st, 2008</h2>
+<ul>
+  <li><a href="/user/cellog" title="Greg Beaver">Greg Beaver</a> [President]</li>
+  <li><a href="/user/mj" title="Martin Jansen">Martin Jansen</a></li>
+  <li><a href="/user/davidc" title="David Coallier">David Coallier</a> </li>
+  <li><a href="/user/arnaud" title="Arnaud Limbourg">Arnaud Limbourg</a></li>
+  <li><a href="/user/jeichorn" title="Joshua Eichorn">Joshua Eichorn</a> [Vice President]</li>
+  <li><a href="/user/cweiske" title="Christian Weiske">Christian Weiske</a></li>
+  <li><a href="/user/dufuz" title="Helgi &THORN;ormar &THORN;orbjoernsson">Helgi &THORN;ormar &THORN;orbjoernsson</a></li>
+  <li><a href="/user/pmjones" title="Paul M. Jones">Paul M. Jones</a></li>
+</ul>
+
+<h2>Group of August 12th, 2003 until May 1st, 2007</h2>
+<p>
+ This is the first PEAR group that Stig <a href="http://marc.info/?l=pear-dev&m=106073080219083&w=2">announced</a>
+ back in 2003 but did not follow the same governing pattern as other groups after it did.
+</p>
+<ul>
+  <li><a href="/user/lsmith" title="Lukas Smith">Lukas Smith</a> </li>
+  <li><a href="/user/mj" title="Martin Jansen">Martin Jansen</a></li>
+  <li><a href="/user/ssb" title="Stig S. Bakken">Stig S. Bakken</a> </li>
+  <li><a href="/user/jon" title="Jon Parise">Jon Parise</a></li>
+  <li><a href="/user/cox" title="Tomas V.V.Cox">Tomas V.V.Cox</a></li>
+  <li><a href="/user/alank" title="Alan Knowles">Alan Knowles</a></li>
+  <li><a href="/user/pajoye" title="Pierre-Alain Joye">Pierre-Alain Joye</a></li>
+  <li><a href="/user/jmcastagnetto" title="Jesus M. Castagnetto">Jesus M. Castagnetto</a></li>
+</ul>
+
+<?php
+response_footer();
--- public_html/group/index.php
+++ public_html/group/index.php
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
    $Id$
 */
-response_header("The PEAR Group");
+response_header('The PEAR Group');
 ?>
 
 <h1>The PEAR Group</h1>
@@ -26,21 +26,24 @@ response_header("The PEAR Group");
 of the following individuals (in no particular order):</p>
 
 <ul>
-  <li><a href="/user/mj" title="Martin Jansen">Martin Jansen</a></li>
-  <li><a href="/user/davidc" title="David Coallier">David Coallier</a> </li>
-  <li><a href="/user/arnaud" title="Arnaud Limbourg">Arnaud Limbourg</a></li>
-  <li><a href="/user/jeichorn" title="Joshua Eichorn">Joshua Eichorn</a> [Vice President]</li>
-  <li><a href="/user/cweiske" title="Christian Weiske">Christian Weiske</a></li>
-  <li><a href="/user/dufuz" title="Helgi &THORN;ormar">Helgi &THORN;ormar</a></li>
-  <li><a href="/user/pmjones" title="Paul M. Jones">Paul M. Jones</a></li>
+  <li><a href="/user/ashnazg" title="Chuck Burges">Chuck Burges</a></li>
+  <li><a href="/user/davidc" title="David Coallier">David Coallier</a> [President]</li>
+  <li><a href="/user/jstump" title="Joe Stump">Joe Stump</a></li>
+  <li><a href="/user/jeichorn" title="Joshua Eichorn">Joshua Eichorn</a></li>
+  <li><a href="/user/cweiske" title="Christian Weiske">Christian Weiske</a> [Vice President]</li>
+  <li><a href="/user/dufuz" title="Helgi &THORN;ormar &THORN;orbjoernsson">Helgi &THORN;ormar &THORN;orbjoernsson</a></li>
+  <li><a href="/user/bbieber" title="Brett Bieber">Brett Bieber</a></li>
 </ul>
 
-<p>The current PEAR Group membership lasts from May 1, 2007 until May 1, 2008, and
+<p>
+The current PEAR Group membership lasts from June 15th, 2008 until June 15th, 2009, and
 is made up of PEAR developers duly elected by the developers of PEAR to serve.
 </p>
 
+<p>An archive of older groups can be found <a href="/group/archive.php">here</a></p>
+
 <p>The Group was
-<?php echo make_link("http://marc.theaimsgroup.com/?l=pear-dev&m=106073080219083&w=2", "first announced"); ?> 
+<?php echo make_link("http://marc.theaimsgroup.com/?l=pear-dev&m=106073080219083&w=2", "first announced"); ?>
 on 12th August 2003 by Stig S. Bakken, and was made into an elected body by the constitution
 adopted on 18th March 2007. If you would like to get in
 contact with the members, you can write to
@@ -49,17 +52,16 @@ contact with the members, you can write to
 <h2>&raquo; Administrative Documents</h2>
 
 <ul>
-  <li>04th November 2005: <?php echo make_link("docs/20051104-sa.php", "Security Vulnerability Announcement"); ?></li>
-  <li>02nd April 2004:  <?php echo make_link("docs/20040402-la.php", "License Announcement"); ?></li>
-  <li>19th March 2004:  <?php echo make_link("docs/20040322-vm.php", "Handling Votings and Membership (II)"); ?></li>
+  <li>04th November 2005:  <?php echo make_link("docs/20051104-sa.php", "Security Vulnerability Announcement"); ?></li>
+  <li>02nd April 2004:     <?php echo make_link("docs/20040402-la.php", "License Announcement"); ?></li>
+  <li>19th March 2004:     <?php echo make_link("docs/20040322-vm.php", "Handling Votings and Membership (II)"); ?></li>
   <li>26th February 2004:  <?php echo make_link("docs/20040226-vn.php", "Version Naming"); ?></li>
   <li>14th November 2003:  <?php echo make_link("docs/20031114-pds.php", "Package Directory Structure"); ?></li>
   <li>14th November 2003:  <?php echo make_link("docs/20031114-pcl.php", "Forming of the PEAR core list"); ?></li>
   <li>14th November 2003:  <?php echo make_link("docs/20031114-bbr.php", "New guidelines for BC breaking releases"); ?></li>
   <li>04th September 2003: <?php echo make_link("docs/20030904-pph.php", "Handling Package Proposals"); ?></li>
-  <li>20th August 2003: <?php echo make_link("docs/20030820-vm.php", "Handling Votings and Membership"); ?></li>
+  <li>20th August 2003:    <?php echo make_link("docs/20030820-vm.php", "Handling Votings and Membership"); ?></li>
 </ul>
 
 <?php
-
 response_footer();
