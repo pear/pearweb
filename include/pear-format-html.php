3681e9cc5bf6f0c1a9516092f92e17e9ffa6983d
Brett Bieber <saltybeagle@php.net>
2009-03-13 14:11:04 +0000

    Revert to the HTML 5 doctype. Thanks Helgi.
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@277105 c90b9560-bf6c-de11-be94-00142212c4b1

--- include/pear-format-html.php
+++ include/pear-format-html.php
@@ -99,8 +99,7 @@ function response_header($title = 'The PHP Extension and Application Repository'
         header('Content-Type: text/html; charset=ISO-8859-15');
     }
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html>
 <head <?php echo $head ?>>
 <?php echo $extraHeaders; ?>
