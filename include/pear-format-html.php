ea6916518e443ad7326ee31e5ded3a2fd553a6f1
Daniel O'Connor <clockwerx@php.net>
2009-06-16 13:27:13 +0000

    Removes google analytics to pearweb
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@282246 c90b9560-bf6c-de11-be94-00142212c4b1

--- include/pear-format-html.php
+++ include/pear-format-html.php
@@ -262,17 +262,6 @@ echo make_link('/about/credits.php', 'CREDITS');
      echo "\n";
     ?>
   </div>
-  <!-- Google analytics -->
-  <script type="text/javascript">
-    var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-    document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-  </script>
-  <script type="text/javascript">
-    try {
-      var pageTracker = _gat._getTracker("<?php print PEAR_GOOGLE_ANALYTICS_ID; ?>");
-      pageTracker._trackPageview();
-    } catch(err) {}
-  </script>
 
 
 <!-- Onload focus to pear -->
