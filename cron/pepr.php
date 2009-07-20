cecac4f621df9cb00e64c499ab9fe90aec6c1f89
Brett Bieber <saltybeagle@php.net>
2009-07-20 18:29:18 +0000

    Remove format_date function, already declared in pear-format-html.php - this may fix pepr
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@284454 c90b9560-bf6c-de11-be94-00142212c4b1

--- cron/pepr.php
+++ cron/pepr.php
@@ -66,19 +66,3 @@ foreach ($proposals AS $id => $proposal) {
         }
     }
 }
-
-/**
- * Turns a unix timestamp into a uniformly formatted date
- * If the date is during the current year, the year is omitted.
- *
- * @param int $date  the unix timestamp to be formatted
- * @param string $format date function formatted string
- * @return string  the formatted date
- */
-function format_date($ts = null, $format = 'Y-m-d H:i e')
-{
-    if (!$ts) {
-        $ts = time();
-    }
-    return gmdate($format, $ts - date('Z', $ts));
-}
