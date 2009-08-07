1ffa885af8f62d7c687e6e2a1d67bb2145c04af4
Daniel O'Connor <clockwerx@php.net>
2009-08-07 01:34:15 +0000

    Request #16496  * News page should point you at the pear blog
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@286896 c90b9560-bf6c-de11-be94-00142212c4b1

--- public_html/news/index.php
+++ public_html/news/index.php
@@ -18,30 +18,36 @@
    $Id$
 */
 
-response_header('News');
+
+$extraHeaders = '<link rel="alternate" href="http://blog.pear.php.net/feed/" type="application/rss+xml" title="PEAR News" />';
+
+response_header("News", false, $extraHeaders);
 ?>
 
 <h1>PEAR News</h1>
+<p>Hey you! You probably want to look at the <a href="http://blog.pear.php.net/">PEAR blog</a> to find the current news.</p>
 
-<h2>&raquo; <a name="yr2007" id="yr2007">Year 2007</a></h2>
+<h2>Historical news</h2>
+
+<h3>&raquo; <a name="yr2007" id="yr2007">Year 2007</a></h3>
 <ul>
  <li><?php echo make_link('newpresident-2007.php', 'PEAR has a new President!'); ?> (April) </li>
  <li><?php echo make_link('newgroup-2007.php', 'A new PEAR Group has been elected'); ?> (April) </li>
  <li><?php echo make_link('package.xml.1.0.php', 'Innovating the future: Package.xml 1.0 and PEAR 1.3.6 are officially deprecated'); ?> (January)</li>
 </ul>
-<h2>&raquo; <a name="yr2005" id="yr2005">Year 2005</a></h2>
+<h3>&raquo; <a name="yr2005" id="yr2005">Year 2005</a></h3>
 <ul>
  <li><?php echo make_link('vulnerability.php', 'Serious vulnerability in the PEAR installer'); ?> (November)</li>
 </ul>
 
-<h2>&raquo; <a name="yr2004" id="yr2004">Year 2004</a></h2>
+<h3>&raquo; <a name="yr2004" id="yr2004">Year 2004</a></h3>
 <ul>
  <li><?php echo make_link('nm-guide.php', 'New Maintainer&#39;s Guide'); ?> (August)</li>
  <li><?php echo make_link('weekly-summaries.php', 'Weekly Summaries'); ?> (April)</li>
  <li><?php echo make_link('pepr.php', 'Announcing PEPr'); ?> (January)</li>
 </ul>
 
-<h2>&raquo; <a name="yr2003" id="yr2003">Year 2003</a></h2>
+<h3>&raquo; <a name="yr2003" id="yr2003">Year 2003</a></h3>
 <ul>
  <li><?php echo make_link('pecl-split.php', 'Own infrastructure for PECL'); ?> (October)</li>
  <li><?php echo make_link('group-announce.php', 'Announcing the PEAR Group'); ?> (August)</li>
