f1aa66e68f0e9d483ecf8a4bc6ad4d99c1be973e
Daniel O'Connor <clockwerx@php.net>
2011-03-27 02:19:04 +0000

    Swap doctype to xhtml5; as per http://news.php.net/php.pear.webmaster/6819
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@309727 c90b9560-bf6c-de11-be94-00142212c4b1

--- include/pear-format-html.php
+++ include/pear-format-html.php
@@ -105,7 +105,7 @@ function response_header(
         header('Content-Type: text/html; charset=UTF-8');
     }
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head <?php echo $head ?>>
 <?php echo $extraHeaders; ?>
--- tests/site/release-upload.php/test_notloggedin.phpt
+++ tests/site/release-upload.php/test_notloggedin.phpt
@@ -22,7 +22,7 @@ __halt_compiler();
 ===DONE===
 --EXPECTF--
 <?xml version="1.0" encoding="ISO-8859-15" ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
  <title>PEAR :: Login</title>
