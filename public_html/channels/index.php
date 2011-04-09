080594503692f5761e36a0d683b8f1b4e45ab005
Daniel O'Connor <clockwerx@php.net>
2011-04-09 15:31:43 +0000

    Shifted package listing to database
    git-svn-id: http://svn.php.net/repository/pear/pearweb/trunk@310102 c90b9560-bf6c-de11-be94-00142212c4b1

--- public_html/channels/index.php
+++ public_html/channels/index.php
@@ -55,38 +55,8 @@ provided on the individual pages.</p>
   <dl><kbd>$ pear channel-discover <?php print $channel['name']; ?></kbd></dl>
 <?php } ?>
 </dl>
-<ul>
-  <li><a href="http://pear.11abacus.com/">11abacus</a></li>
-  <li><a href="http://pear.agavi.org/">Agavi</a></li>
-  <li><a href="http://pear.crisscott.com/">Crisscott</a></li>
-  <li><a href="http://pear.domain51.com/">Domain51</a></li>
-  <li><a href="http://components.ez.no/">eZ components</a></li>
-  <li><a href="http://pear.horde.org/">Horde</a></li>
-  <li><a href="http://pear.midcom-project.org/">Midgard Project</a></li>
-  <li><a href="http://pear.phing.info/">Phing</a></li>
-  <li><a href="http://pear.php-tools.net/">PHP Application Tools</a></li>
-  <li><a href="http://pear.phpunit.de/">PHPUnit</a></li>
-  <li><a href="http://pear.phpspec.org/">PHPSpec</a></li>
-  <li><a href="http://pear.piece-framework.com/">Piece Framework</a></li>
-  <li><a href="http://pear-smarty.googlecode.com/">Inofficial Smarty channel</a></li>
-  <li><a href="http://pear.si.kz">si.kz</a></li>
-  <li><a href="http://pear.symfony-project.com/">Symfony</a></li>
-  <li><a href="http://solarphp.com/home/index.php?area=Main&amp;page=DownloadInstall#toc1">Solar</a></li>
-  <li><a href="http://pear.funkatron.com/">Edward Finkler</a></li>
-  <li><a href="http://zend.googlecode.com/">Unofficial Zend Framework channel</a></li>
-  <li><a href="http://pear.firephp.org/">FirePHP</a></li>
-  <li><a href="http://pear.timj.co.uk/">Tim Jackson's PHP tools</a></li>
-  <li><a href="http://pear.phpundercontrol.org/">phpUnderControl</a></li>
-  <li><a href="http://pear.pdepend.org/">PHP Depend</a></li>
-  <li><a href="http://pear.phpmd.org/">PHP Mess Detector</a></li>
-  <li><a href="http://pear.pearfarm.org/">PEARFarm</a></li>
-  <li><a href="http://pear.pirum-project.org/">Pirum</a></li>
-  <li><a href="http://pearhub.org/">PEARHub</a></li>
-  <li><a href="http://pear.fluentdom.org/">FluentDOM</a></li>
-  <li><a href="http://www.faett.net/">Faett</a></li>
-  <li><a href="http://phpseclib.sourceforge.net/pear.htm">phpseclib</a></li>
-  <li><a href="http://pear.indeyets.pp.ru">Alexey Zakhlestin's PEAR channel</a></li>
-</ul>
+
+<p><a href="/channels/add.php">Add your site</a></p>
 
 <?php if (auth_check('pear.admin')) { ?>
     <h2>Sites to be Approved</h2>
@@ -101,12 +71,10 @@ provided on the individual pages.</p>
     </dl>
 <?php } ?>
 
-<p><a href="/channels/add.php">Add your site</a></p>
-
 <h2>Channel server software</h2>
 <p>Want to host your own channel? </p>
 <ul>
-    <li><a href="http://pear.chiaraquartet.net">Chiara_PEAR_Server</a> (<a href="http://greg.chiaraquartet.net/archives/123-Setting-up-your-own-PEAR-channel-with-Chiara_PEAR_Server-the-official-way.html">Documentation</a>)</li>
+    <li><a href="http://pear.chiaraquartet.net">Chiara_PEAR_Server</a> (<a href="http://greg.chiaraquartet.net/archives/123-Setting-up-your-own-PEAR-channel-with-Chiara_PEAR_Server-the-official-way.html">documentation</a>)</li>
     <li><a href="http://svn.php.net/viewvc/pear2/sandbox/SimpleChannelServer/trunk/">SimpleChannelServer</a> (<a href="http://saltybeagle.com/2008/12/using-simplechannelserver-to-manage-a-pear-channel-on-google-code/">documentation</a>)</li>
     <li><a href="http://www.pirum-project.org/">Pirum</a> (<a href="http://blog.stuartherbert.com/php/2011/03/30/setting-up-your-own-pear-channel/">documentation</a>)</li>
     <li><a href="http://pearfarm.org/">Pearfarm</a></li>
--- sql/channels.sql
+++ sql/channels.sql
@@ -12,3 +12,34 @@ INSERT INTO channels VALUES ('pear.php.net', 1, 'PEAR', 'http://pear.php.net/', 
 
 INSERT INTO channels VALUES ('pecl.php.net', 1, 'PECL', 'http://pecl.php.net/', 'PEAR Webmaster', 'pear-webmaster@lists.php.net');
 
+
+INSERT INTO channels VALUES ('pear.11abacus.com', 1, '11abacus', 'http://pear.11abacus.com/', '', '');
+INSERT INTO channels VALUES ('pear.agavi.org', 1, 'Agavi', 'http://pear.agavi.org/', '', '');
+INSERT INTO channels VALUES ('pear.crisscott.com', 1, 'Crisscott', 'http://pear.crisscott.com/', '', '');
+INSERT INTO channels VALUES ('pear.domain51.com', 1, 'Domain51', 'http://pear.domain51.com/', '', '');
+INSERT INTO channels VALUES ('components.ez.no', 1, 'eZ components', 'http://components.ez.no/', '', '');
+INSERT INTO channels VALUES ('pear.horde.org', 1, 'Horde', 'http://pear.horde.org/', '', '');
+INSERT INTO channels VALUES ('pear.midcom-project.org', 1, 'Midgard Project', 'http://pear.midcom-project.org/', '', '');
+INSERT INTO channels VALUES ('pear.phing.info', 1, 'Phing', 'http://pear.phing.info/', '', '');
+INSERT INTO channels VALUES ('pear.php-tools.net', 1, 'PHP Application Tools', 'http://pear.php-tools.net/', '', '');
+INSERT INTO channels VALUES ('pear.phpunit.de', 1, 'PHPUnit', 'http://pear.phpunit.de/', '', '');
+INSERT INTO channels VALUES ('pear.phpspec.org', 1, 'PHPSpec', 'http://pear.phpspec.org/', '', '');
+INSERT INTO channels VALUES ('pear.piece-framework.com', 1, 'Piece Framework', 'http://pear.piece-framework.com/', '', '');
+INSERT INTO channels VALUES ('pear-smarty.googlecode.com', 1, 'Inofficial Smarty channel', 'http://pear-smarty.googlecode.com/', '', '');
+INSERT INTO channels VALUES ('pear.si.kz', 1, 'si.kz', 'http://pear.si.kz', '', '');
+INSERT INTO channels VALUES ('pear.symfony-project.com', 1, 'Symfony', 'http://pear.symfony-project.com/', '', '');
+INSERT INTO channels VALUES ('solarphp.com', 1, 'Solar', 'http://solarphp.com/home/index.php?area=Main&amp;page=DownloadInstall#toc1', '', '');
+INSERT INTO channels VALUES ('pear.funkatron.com', 1, 'Edward Finkler', 'http://pear.funkatron.com/', '', '');
+INSERT INTO channels VALUES ('zend.googlecode.com', 1, 'Unofficial Zend Framework channel', 'http://zend.googlecode.com/', '', '');
+INSERT INTO channels VALUES ('pear.firephp.org', 1, 'FirePHP', 'http://pear.firephp.org/', '', '');
+INSERT INTO channels VALUES ('pear.timj.co.uk', 1, 'Tim Jackson\'s PHP tools', 'http://pear.timj.co.uk/', '', '');
+INSERT INTO channels VALUES ('pear.phpundercontrol.org', 1, 'phpUnderControl', 'http://pear.phpundercontrol.org/', '', '');
+INSERT INTO channels VALUES ('pear.pdepend.org', 1, 'PHP Depend', 'http://pear.pdepend.org/', '', '');
+INSERT INTO channels VALUES ('pear.phpmd.org', 1, 'PHP Mess Detector','http://pear.phpmd.org/', '', '');
+INSERT INTO channels VALUES ('pear.pearfarm.org', 1, 'PEARFarm', 'http://pear.pearfarm.org/', '', '');
+INSERT INTO channels VALUES ('pear.pirum-project.org', 1, 'Pirum', 'http://pear.pirum-project.org/', '', '');
+INSERT INTO channels VALUES ('pearhub.org', 1, 'PEARHub', 'http://pearhub.org/', '', '');
+INSERT INTO channels VALUES ('pear.fluentdom.org', 1, 'FluentDOM', 'http://pear.fluentdom.org/', '', '');
+INSERT INTO channels VALUES ('www.faett.net', 1, 'Faett', 'http://www.faett.net/', '', '');
+INSERT INTO channels VALUES ('phpseclib.sourceforge.net', 1, 'phpseclib', 'http://phpseclib.sourceforge.net/pear.htm', '', '');
+INSERT INTO channels VALUES ('pear.indeyets.pp.ru', 1, 'Alexey Zakhlestin\'s PEAR channel', 'http://pear.indeyets.pp.ru', '', '');
