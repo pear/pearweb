0487a2f8e8727442a29e0097322948a290a8049b
Daniel Convissor <danielc@analysisandsolutions.com>
2011-11-13 15:59:13 -0500

    Adjust script to be runnable anytime. Remove PHP 4 versions.

--- cron/bug-update-php-version.php
+++ cron/bug-update-php-version.php
@@ -19,54 +19,46 @@
  * $Id$
  */
 
-// fetch the data
-$new5 = file_get_contents('http://www.php.net/releases/?serialize=1&version=5');
+// NOTE: this data does not contain alpha, beta or RC releases.
+$new5 = file_get_contents('http://www.php.net/releases/?serialize=1&version=5&max=40');
 if ($new5 === false) {
     die('There was a problem fetching the serialized array from php.net');
 }
+$n5 = array_keys(unserialize($new5));
 
-$new4 = file_get_contents('http://www.php.net/releases/?serialize=1&version=4');
-if ($new4 === false) {
-    die('There was a problem fetching the serialized array from php.net');
+usort($n5, 'sort_versions');
+function sort_versions($a, $b) {
+    // Reverse order.
+    return version_compare($b, $a);
 }
-
-$n5 = unserialize($new5);
-$n4 = unserialize($new4);
 
 $file = '@www-dir@/pear.php.net/public_html/bugs/include/php_versions.php';
 if (!file_exists($file)) {
     $file = dirname(__FILE__) . '/../public_html/bugs/include/php_versions.php';
 }
-require $file;
-
-$php4 = $php = array();
-foreach ($versions as $v) {
-    if ($v[0] != '4') {
-        $php[] = $v;
-    } else {
-        $php4[] = $v;
-    }
-}
-
-if (!in_array($n5['version'], $versions) && strpos($n5['version'], 'RC') === false) {
-    $merge = array_merge(array($n5['version']), $php);
-} else {
-    $merge = $php;
-}
-
-if (!in_array($n4['version'], $versions) && strpos($n4['version'], 'RC') === false) {
-    $merge4 = array_merge(array($n4['version']), $php4);
-} else {
-    $merge4 = $php4;
-}
-
-$final  = array_merge($merge, $merge4);
-$data   = var_export($final, true);
-
-// var_export parses date() and just uses its output, fixing that
-$data = str_replace(date('Y-m-d') . "',", '\' . date(\'Y-m-d\'),', $data);
 
 $output = '<?php
-$versions = ' . $data . ';';
+// Update this file using pearweb/cron/bug-update-php-version.
+$versions = array(
+    "HEAD SVN-" . date("Y-m-d"),
+    "5_4 SVN-" . date("Y-m-d"),
+';
+
+$output_53_start = '
+    "5.4 RC (specify # in Description)",
+    "5.4.0 beta",
+    "5_3 SVN-" . date("Y-m-d"),
+';
+
+$found_53 = false;
+foreach ($n5 as $version) {
+    if (!$found_53 && strpos($version, '5.3.') !== false) {
+        $found_53 = true;
+        $output .= "$output_53_start\n";
+    }
+    $output .= '    "' . $version . "\",\n";
+}
+
+$output .= ");\n";
 
 file_put_contents($file, $output);
