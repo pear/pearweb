665a1c03d87392c9f3f95f8f7dd54e687239abf2
Daniel O'Connor <daniel.oconnor@gmail.com>
2011-12-11 21:08:52 +1030

    Improve default sort order

--- public_html/qa/packages_closed_reports_no_release.php
+++ public_html/qa/packages_closed_reports_no_release.php
@@ -64,7 +64,7 @@ $sql .= "
 GROUP BY
     packages.id, packages.name, bugdb.package_name, bugdb.id, r.package
 ORDER BY
-    r.releasedate";
+    unmaintained, r.releasedate";
 
 $res        = $dbh->getAll($sql, null, DB_FETCHMODE_ASSOC);
 $total_rows = $dbh->getOne('SELECT FOUND_ROWS()');
