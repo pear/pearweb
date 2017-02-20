1b1dc2da38c09534c43f4c6178d3c5bb7fe88277
Christian Weiske <cweiske@cweiske.de>
2017-02-20 17:13:29 +0100

    Example SQL how to change a nickname

---
+++ scripts/change-user-nickname.sql
@@ -0,0 +1,15 @@
+UPDATE users SET handle="newnick" WHERE handle="oldnick";
+UPDATE bugdb SET handle="newnick" WHERE handle="oldnick";
+UPDATE bugdb_comments SET handle="newnick" WHERE handle="oldnick";
+UPDATE bug_account_request SET handle="newnick" WHERE handle="oldnick";
+UPDATE cvs_acl SET username="newnick" WHERE username="oldnick";
+UPDATE election_handle_votes SET handle="newnick" WHERE handle="oldnick";
+UPDATE election_account_request SET handle="newnick" WHERE handle="oldnick";
+UPDATE maintains SET handle="newnick" WHERE handle="oldnick";
+UPDATE manual_notes SET user_handle="newnick" WHERE user_handle="oldnick";
+UPDATE notes SET uid="newnick" WHERE uid="oldnick";
+UPDATE package_proposal_changelog SET user_handle="newnick" WHERE user_handle="oldnick";
+UPDATE package_proposal_votes SET user_handle="newnick" WHERE user_handle="oldnick";
+UPDATE package_proposals SET user_handle="newnick" WHERE user_handle="oldnick";
+UPDATE releases SET doneby="newnick" WHERE doneby="oldnick";
+
