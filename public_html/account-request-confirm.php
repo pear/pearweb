6ed62327bb4dc9db2b0cb285dece091cac65a14f
Ken Guest <kguest@php.net>
2020-09-10 22:13:32 +0100

    Apply filter_var for sanitising some inputs

--- public_html/account-request-confirm.php
+++ public_html/account-request-confirm.php
@@ -10,34 +10,46 @@ response_header('Account confirmation');
 if (isset($_POST['confirmdetails'])) {
     $request = new PEAR_Bug_Accountrequest;
     if ($request->find($_POST['salt']) && $request->pending()) {
-        $salt = $_POST['salt'];
+        $salt = filter_var($_POST['salt'], FILTER_SANITISE_STRING);
         if (empty($_POST['isMD5'])) {
             $_POST['PEAR_PW']  = md5($_POST['PEAR_PW']);
             $_POST['PEAR_PW2'] = md5($_POST['PEAR_PW2']);
         }
 
-        if (count($errors = $request->validateRequest($_POST['PEAR_USER'], $_POST['PEAR_PW'],
-                                       $_POST['PEAR_PW2'], $_POST['name']))) {
+        if (count(
+            $errors = $request->validateRequest(
+                filter_var($_POST['PEAR_USER'], FILTER_SANITISE_STRING),
+                filter_var($_POST['PEAR_PW'], FILTER_SANITISE_STRING),
+                filter_var($_POST['PEAR_PW2'], FILTER_SANITISE_STRING),
+                filter_var($_POST['name'], FILTER_SANITISE_STRING)
+            )
+        )
+        ) {
             $email = $request->email;
-            $name = $_POST['name'];
-            $user = $_POST['PEAR_USER'];
+            $name = filter_var($_POST['name'], FILTER_SANITISE_STRING);
+            $user = filter_var($_POST['PEAR_USER'], FILTER_SANITISE_STRING);
             include PEARWEB_TEMPLATEDIR . '/bugs/registernewaccount.php';
             response_footer();
             exit;
         }
-        $errors = $request->confirmRequest($_POST['PEAR_USER'],
-            $_POST['PEAR_PW'], $_POST['name']);
+        $errors = $request->confirmRequest(
+            filter_var($_POST['PEAR_USER'], FILTER_SANITISE_STRING),
+            filter_var($_POST['PEAR_PW'], FILTER_SANITISE_STRING),
+            filter_var($_POST['name'], FILTER_SANITISE_STRING)
+        );
         if ($errors === true) {
-            report_success('Your account has been activated, bugs you have opened and comments
-        you have made are now available to the public for viewing');
+            report_success(
+                'Your account has been activated, bugs you have opened and ' .
+                'comments you have made are now available to the public for viewing'
+            );
             echo '<a href="login.php">Log In</a> to continue.';
             response_footer();
             exit;
         }
 
         $email = $request->email;
-        $name = $_POST['name'];
-        $user = $_POST['PEAR_USER'];
+        $name = filter_var($_POST['name'], FILTER_SANITISE_STRING);
+        $user = filter_var($_POST['PEAR_USER'], FILTER_SANITISE_STRING);
         include PEARWEB_TEMPLATEDIR . '/bugs/registernewaccount.php';
         response_footer();
         exit;
@@ -48,8 +60,11 @@ if (isset($_POST['confirmdetails'])) {
 
 if (isset($_GET['type']) && $_GET['type'] == 'bug') {
     echo '<h1>Confirm Bug Tracker Email Address</h1>';
-    echo '<p>Please choose a username for opening future bugs/adding comments to existing bugs</p>';
-    if (!empty($stripped['salt']) && strlen($salt = htmlspecialchars($stripped['salt'])) == 32) {
+    echo '<p>Please choose a username for opening future bugs/adding ';
+    echo 'comments to existing bugs</p>';
+    if (!empty($stripped['salt'])
+        && strlen($salt = htmlspecialchars($stripped['salt'])) == 32
+    ) {
         $request = new PEAR_Bug_Accountrequest;
         if ($request->find($salt) && $request->pending()) {
             $email = $request->email;
@@ -66,7 +81,9 @@ if (isset($_GET['type']) && $_GET['type'] == 'bug') {
     }
 } else {
     echo '<h1>Confirm Account</h1>';
-    if (empty($stripped['salt']) || strlen($salt = htmlspecialchars($stripped['salt'])) != 32) {
+    if (empty($stripped['salt'])
+        || strlen($salt = htmlspecialchars($stripped['salt'])) != 32
+    ) {
         report_error('Unknown salt');
     } else {
         $request = new PEAR_Election_Accountrequest();
@@ -74,10 +91,16 @@ if (isset($_GET['type']) && $_GET['type'] == 'bug') {
         if (PEAR::isError($result)) {
             report_error($result->getMessage());
         } elseif ($result) {
-            report_success('Your account has been activated, you can now vote in
-        PEAR elections that are for the general PHP public as well as open bugs in the bug tracker');
+            report_success(
+                'Your account has been activated, you can now vote in PEAR ' .
+                'elections that are for the general PHP public as well as ' .
+                'open bugs in the bug tracker'
+            );
         } else {
-            report_error('There was a problem activating your account, please contact ' . PEAR_WEBMASTER_EMAIL);
+            report_error(
+                'There was a problem activating your account, please contact ' .
+                PEAR_WEBMASTER_EMAIL
+            );
         }
     }
 }
